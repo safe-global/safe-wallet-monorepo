35c5230be562c9158ecba8ba30c5c19b9c2ebaee
j75689 <j75689@gmail.com>
2023-10-24 15:08:03 +0800

    dep: update safe-deployments

--- src/config/custom_contract.ts
+++ src/config/custom_contract.ts
@@ -1,12 +1,30 @@
 export const contractNetworks = {
+  '204': {
+    safeMasterCopyAddress: '0xE2CF742b554F466d5E7a37C371FD47C786d2FBc0',
+    safeProxyFactoryAddress: '0x9fea7F7C69f14aa1a7d62cC9D468fEB2F9371CB3',
+    multiSendAddress: '0xDeB0467cCfAda493902C8D279A2F41f26b813AC9',
+    multiSendCallOnlyAddress: '0xC33224E130C702808e12299eCaBC16148a5B3d0B',
+    fallbackHandlerAddress: '0x40B30946045a876ffD68CaF008f94eeAAD50F855',
+    signMessageLibAddress: '0x6ACe153bF757b4999c7D6f0F3dFb1043dC67d61a',
+    createCallAddress: '0x392e2F66c3BBF0046c861e0065fB7C7917b18078',
+  },
   '5611': {
-    safeMasterCopyAddress: '0x3E5c63644E683549055b9Be8653de26E0B4CD36E',
-    safeProxyFactoryAddress: '0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2',
-    multiSendAddress: '0xA238CBeb142c10Ef7Ad8442C6D1f9E89e07e7761',
-    multiSendCallOnlyAddress: '0x40A2aCCbd92BCA938b02010E17A5b8929b49130D',
-    fallbackHandlerAddress: '0xf48f2B2d2a534e402487b3ee7C18c33Aec0Fe5e4',
-    signMessageLibAddress: '0xA65387F16B013cf2Af4605Ad8aA5ec25a2cbA3a2',
-    createCallAddress: '0x7cbB62EaA69F79e6873cD1ecB2392971036cFAa4',
+    safeMasterCopyAddress: '0xE2CF742b554F466d5E7a37C371FD47C786d2FBc0',
+    safeProxyFactoryAddress: '0x9fea7F7C69f14aa1a7d62cC9D468fEB2F9371CB3',
+    multiSendAddress: '0xDeB0467cCfAda493902C8D279A2F41f26b813AC9',
+    multiSendCallOnlyAddress: '0xC33224E130C702808e12299eCaBC16148a5B3d0B',
+    fallbackHandlerAddress: '0x40B30946045a876ffD68CaF008f94eeAAD50F855',
+    signMessageLibAddress: '0x6ACe153bF757b4999c7D6f0F3dFb1043dC67d61a',
+    createCallAddress: '0x392e2F66c3BBF0046c861e0065fB7C7917b18078',
+  },
+  '97': {
+    safeMasterCopyAddress: '0xE2CF742b554F466d5E7a37C371FD47C786d2FBc0',
+    safeProxyFactoryAddress: '0x9fea7F7C69f14aa1a7d62cC9D468fEB2F9371CB3',
+    multiSendAddress: '0xDeB0467cCfAda493902C8D279A2F41f26b813AC9',
+    multiSendCallOnlyAddress: '0xC33224E130C702808e12299eCaBC16148a5B3d0B',
+    fallbackHandlerAddress: '0x40B30946045a876ffD68CaF008f94eeAAD50F855',
+    signMessageLibAddress: '0x6ACe153bF757b4999c7D6f0F3dFb1043dC67d61a',
+    createCallAddress: '0x392e2F66c3BBF0046c861e0065fB7C7917b18078',
   },
 }
 
