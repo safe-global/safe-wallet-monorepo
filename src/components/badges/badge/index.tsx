6347a2b0af471c00ae3e0b4db5916017db64ffd5
AnotherDev <joseluismanco37@gmail.com>
2024-05-08 16:25:43 -0500

    feat: make dynamic badge component

--- src/components/badges/badge/index.tsx
+++ src/components/badges/badge/index.tsx
@@ -1,11 +1,25 @@
 import { Box, Card, CardActions, CardContent, IconButton, Stack, SvgIcon, Typography } from '@mui/material'
 import React from 'react'
-import Concept from '@/public/images/concept/base.png'
-import Image from 'next/image'
+import type { StaticImageData } from 'next/image'
 import SuperChainPoints from '@/public/images/common/superChain.svg'
 import Hearth from '@/public/images/common/hearth.svg'
 import css from './styles.module.css'
-function Badge() {
+import Image from 'next/image'
+function Badge({
+  image,
+  title,
+  description,
+  networkOrProtocol,
+  points,
+  tiers,
+}: {
+  image: StaticImageData
+  title: string
+  description: string
+  networkOrProtocol: string
+  points: number
+  tiers: number[]
+}) {
   return (
     <Card className={css.badgeContainer}>
       <CardContent>
@@ -13,26 +27,26 @@ function Badge() {
           <IconButton className={css.hearth}>
             <SvgIcon component={Hearth} inheritViewBox fontSize="small" />
           </IconButton>
-          <Image src={Concept} alt="base" />
+          <Image src={image} alt={networkOrProtocol} />
           <Typography margin={0} fontWeight={600} fontSize={16} textAlign="center" variant="h4">
-            Base User
+            {title}
           </Typography>
           <Typography margin={0} fontSize={14} fontWeight={400} textAlign="center" color="text.secondary">
-            Number of transactions made on Base
+            {description}
           </Typography>
           <Box border={2} borderRadius={1} padding="12px" borderColor="secondary.main">
             <Typography margin={0} textAlign="center" color="secondary.main">
               Unlock Next Tier:
             </Typography>
             <Typography textAlign="center" margin={0}>
-              400 transactions on Base
+              400 transactions on {networkOrProtocol}
             </Typography>
           </Box>
         </Stack>
       </CardContent>
       <CardActions>
         <Box width="100%" display="flex" pt={3} justifyContent="center" alignItems="center">
-          20 <SvgIcon component={SuperChainPoints} inheritViewBox fontSize="medium" />
+          {points} <SvgIcon component={SuperChainPoints} inheritViewBox fontSize="medium" />
         </Box>
       </CardActions>
     </Card>
--- src/components/badges/content/index.tsx
+++ src/components/badges/content/index.tsx
@@ -1,6 +1,7 @@
 import { Grid, Stack, Typography } from '@mui/material'
 import React from 'react'
 import Badge from '../badge'
+import Concept from '@/public/images/concept/base.png'
 
 function BadgesContent() {
   return (
@@ -12,14 +13,46 @@ function BadgesContent() {
       </Grid>
       <Grid xs={12} item>
         <Stack direction="row" spacing={2} justifyContent="space-between" useFlexGap flexWrap="wrap">
-          <Badge />
-          <Badge />
-          <Badge />
-          <Badge />
-          <Badge />
-          <Badge />
-          <Badge />
-          <Badge />
+          <Badge
+            image={Concept}
+            title="Base User"
+            description="Number of transactions made on Base"
+            networkOrProtocol="Base"
+            points={20}
+            tiers={[1, 2, 3]}
+          />
+          <Badge
+            image={Concept}
+            title="Base User"
+            description="Number of transactions made on Base"
+            networkOrProtocol="Base"
+            points={20}
+            tiers={[1, 2, 3]}
+          />
+          <Badge
+            image={Concept}
+            title="Base User"
+            description="Number of transactions made on Base"
+            networkOrProtocol="Base"
+            points={20}
+            tiers={[1, 2, 3]}
+          />
+          <Badge
+            image={Concept}
+            title="Base User"
+            description="Number of transactions made on Base"
+            networkOrProtocol="Base"
+            points={20}
+            tiers={[1, 2, 3]}
+          />
+          <Badge
+            image={Concept}
+            title="Base User"
+            description="Number of transactions made on Base"
+            networkOrProtocol="Base"
+            points={20}
+            tiers={[1, 2, 3]}
+          />
         </Stack>
       </Grid>
     </Grid>
