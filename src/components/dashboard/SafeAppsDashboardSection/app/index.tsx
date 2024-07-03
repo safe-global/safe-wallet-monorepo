5b4e70b5eddaefcd83acec1017f3edd5a5bba023
AnotherDev <joseluismanco37@gmail.com>
2024-07-03 04:44:35 -0500

    feat: add redirect to SuperChainRaffle

--- src/components/dashboard/SafeAppsDashboardSection/app/index.tsx
+++ src/components/dashboard/SafeAppsDashboardSection/app/index.tsx
@@ -6,9 +6,10 @@ import SuperChainEcoStamp from '@/public/images/common/superchain-eco-stamp.svg'
 import Hearth from '@/public/images/common/hearth.svg'
 
 function SuperChainApp() {
+
     return (
         <Grid item xs={12} sm={6} md={4} xl={4}>
-            <Paper className={css.container}>
+            <Paper onClick={() => window.open('https://super-chain-raffle.vercel.app/', '_blank')} className={css.container}>
                 <Stack padding='24px' gap='12px' justifyContent='space-between' display='flex' height='100%' direction='column' >
                     <Box display="flex" justifyContent='space-between' alignItems="center">
                         <Stack direction='row' gap='12px' justifyContent='flex-start' alignItems='center' fontSize='42px'>
