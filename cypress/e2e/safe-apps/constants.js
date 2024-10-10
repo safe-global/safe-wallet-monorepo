0e083e5d4b043366efd648af743cbdabc2322188
anotherdev <joseluismanco37@gmail.com>
2024-10-09 22:12:04 -0500

    chore: remove unused cypress

--- cypress/e2e/safe-apps/constants.js
+++
@@ -1,5 +0,0 @@
-import { LS_NAMESPACE } from '../../../src/config/constants'
-
-export const BROWSER_PERMISSIONS_KEY = `${LS_NAMESPACE}SafeApps__browserPermissions`
-export const SAFE_PERMISSIONS_KEY = `${LS_NAMESPACE}SafeApps__safePermissions`
-export const INFO_MODAL_KEY = `${LS_NAMESPACE}SafeApps__infoModal`
