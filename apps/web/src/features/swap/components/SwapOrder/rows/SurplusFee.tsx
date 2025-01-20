e7247d5f4361cf3879fafd6b1c2aea80ef008a31
Aaron Cook <aaron@safe.global>
2025-01-20 17:08:59 +0100

    Update SDK

--- apps/web/src/features/swap/components/SwapOrder/rows/SurplusFee.tsx
+++ apps/web/src/features/swap/components/SwapOrder/rows/SurplusFee.tsx
@@ -1,22 +1,14 @@
-import type { Order } from '@safe-global/safe-gateway-typescript-sdk'
+import type { BaseOrder } from '@safe-global/safe-gateway-typescript-sdk'
 import { getOrderFeeBps } from '@/features/swap/helpers/utils'
 import { DataRow } from '@/components/common/Table/DataRow'
 import { formatVisualAmount } from '@/utils/formatters'
 import { HelpIconTooltip } from '@/features/swap/components/HelpIconTooltip'
 
-export const SurplusFee = ({
-  order,
-}: {
-  order: Pick<Order, 'fullAppData' | 'sellToken' | 'buyToken' | 'status' | 'executedSurplusFee' | 'kind'> & {
-    // TODO: Remove when migrating to new SDK
-    executedFee: string
-    executedFeeToken: Order['sellToken']
-  }
-}) => {
+export const SurplusFee = ({ order }: { order: BaseOrder }) => {
   const bps = getOrderFeeBps(order)
   const { executedFee, executedFeeToken } = order
 
-  if (executedFee === null || typeof executedFee === 'undefined' || executedFee === '0') {
+  if (executedFee === null || executedFee === '0') {
     return null
   }
 
