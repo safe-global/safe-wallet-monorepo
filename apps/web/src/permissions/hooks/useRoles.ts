bb0cfa7e17beaf2b29cc292fe4aca89fd357fca2
Tim Schwarz <4171783+tmjssz@users.noreply.github.com>
2025-01-20 23:55:26 +0100

    feat: add NestedOwner role and update useRoles hook to include nested safe ownership check

--- apps/web/src/permissions/hooks/useRoles.ts
+++ apps/web/src/permissions/hooks/useRoles.ts
@@ -5,6 +5,7 @@ import { useIsRecoverer } from '@/features/recovery/hooks/useIsRecoverer'
 import { useIsSpendingLimitBeneficiary } from '@/hooks/useIsOnlySpendingLimitBeneficiary'
 import useWallet from '@/hooks/wallets/useWallet'
 import { Role } from '../types'
+import { useIsNestedSafeOwner } from '@/hooks/useIsNestedSafeOwner'
 
 /**
  * Hook to get the roles that the current user has based on the Safe and the connected wallet.
@@ -13,6 +14,7 @@ import { Role } from '../types'
 export const useRoles = (): Role[] => {
   const wallet = useWallet()
   const isOwner = useIsSafeOwner()
+  const isNestedSafeOwner = useIsNestedSafeOwner()
   const isProposer = useIsWalletProposer()
   const isRecoverer = useIsRecoverer()
   const isSpendingLimitBeneficiary = useIsSpendingLimitBeneficiary()
@@ -21,6 +23,7 @@ export const useRoles = (): Role[] => {
   const roleApplicableMap: Record<Role, boolean> = useMemo(
     () => ({
       [Role.Owner]: isOwner,
+      [Role.NestedOwner]: !!isNestedSafeOwner,
       [Role.Proposer]: !!isProposer,
       [Role.Recoverer]: isRecoverer,
       [Role.SpendingLimitBeneficiary]: isSpendingLimitBeneficiary,
--- apps/web/src/permissions/types/Role.ts
+++ apps/web/src/permissions/types/Role.ts
@@ -1,5 +1,6 @@
 export enum Role {
   Owner = 'Owner',
+  NestedOwner = 'NestedOwner',
   Proposer = 'Proposer',
   Executioner = 'Executioner',
   ModuleRole = 'ModuleRole',
