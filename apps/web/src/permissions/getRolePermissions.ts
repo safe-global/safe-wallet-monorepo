e665916f1b0b9ba112d8950444491b3f9dd4c9bb
Tim Schwarz <4171783+tmjssz@users.noreply.github.com>
2025-01-21 17:54:09 +0100

    refactor: simplify getRolePermissions by removing redundant getRolePermissionSet function

--- apps/web/src/permissions/getRolePermissions.ts
+++ apps/web/src/permissions/getRolePermissions.ts
@@ -2,22 +2,6 @@ import rolePermissionConfig from './config'
 import type { PermissionSet, Role, RoleProps } from './types'
 
 /**
- * Get the PermissionSet for a specific role with the given props.
- * @param role Role to get permissions for
- * @param props Specific parameters for the role
- * @returns PermissionSet for the role or undefined if no permissions are defined for the role
- */
-const getRolePermissionSet = <R extends Role>(role: R, props: RoleProps<R>) => {
-  const rolePermissionsFn = rolePermissionConfig[role]
-
-  if (!rolePermissionsFn) {
-    return undefined
-  }
-
-  return rolePermissionsFn(props)
-}
-
-/**
  * Get the PermissionSet for multiple roles with the given role props object.
  * @param roles Roles to get permissions for
  * @param props Object with specific parameters for the roles
@@ -25,11 +9,11 @@ const getRolePermissionSet = <R extends Role>(role: R, props: RoleProps<R>) => {
  */
 export const getRolePermissions = <R extends Role>(roles: R[], props: { [K in R]?: RoleProps<K> }) =>
   roles.reduce<{ [_K in R]?: PermissionSet }>((acc, role) => {
-    const rolePermissionSet = getRolePermissionSet(role, props[role] as RoleProps<R>)
+    const rolePermissionsFn = rolePermissionConfig[role]
 
-    if (!rolePermissionSet) {
+    if (!rolePermissionsFn) {
       return acc
     }
 
-    return { ...acc, [role]: rolePermissionSet }
+    return { ...acc, [role]: rolePermissionsFn(props[role] as RoleProps<R>) }
   }, {})
