60b4b4f02572c6b085c16a791570ed78f80bdadb
katspaugh <381895+katspaugh@users.noreply.github.com>
2025-01-22 14:49:39 +0100

    Fix(Gas estimation): gas price from oracles can be a float

--- apps/web/src/hooks/useGasPrice.ts
+++ apps/web/src/hooks/useGasPrice.ts
@@ -46,6 +46,10 @@ const isEtherscanResult = (data: any): data is EtherscanResult => {
   return 'FastGasPrice' in data && 'suggestBaseFee' in data
 }
 
+const stringToBigInt = (value: string): bigint => {
+  return BigInt(value.split('.')[0])
+}
+
 /**
  * Parses result from etherscan oracle.
  * Since EIP 1559 it returns the `maxFeePerGas` as gas price and the current network baseFee as `suggestedBaseFee`.
@@ -55,8 +59,8 @@ const isEtherscanResult = (data: any): data is EtherscanResult => {
  * @see https://docs.etherscan.io/api-endpoints/gas-tracker
  */
 const parseEtherscanOracleResult = (result: EtherscanResult, gweiFactor: string): EstimatedGasPrice => {
-  const maxFeePerGas = BigInt(Number(result.FastGasPrice) * Number(gweiFactor))
-  const baseFee = BigInt(Number(result.suggestBaseFee) * Number(gweiFactor))
+  const maxFeePerGas = stringToBigInt(result.FastGasPrice) * stringToBigInt(gweiFactor)
+  const baseFee = stringToBigInt(result.suggestBaseFee) * stringToBigInt(gweiFactor)
 
   return {
     maxFeePerGas,
