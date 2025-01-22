490e9b606f583c7c16d4909d4e97f60514e0341b
Jonathansoufer <jonathan@safe.global>
2025-01-22 14:49:05 +0000

    add (temp) local trigger

--- apps/mobile/src/features/Assets/Assets.container.tsx
+++ apps/mobile/src/features/Assets/Assets.container.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 import { SafeTab } from '@/src/components/SafeTab'
 
 import { TokensContainer } from '@/src/features/Assets/components/Tokens'
 import { NFTsContainer } from '@/src/features/Assets/components/NFTs'
 import { AssetsHeaderContainer } from '@/src/features/Assets/components/AssetsHeader'
+
+import NotificationsService from '@/src/services/notifications/NotificationService'
+import { ChannelId } from '@/src/utils/notifications'
 
 const tabItems = [
   {
@@ -16,6 +19,14 @@ const tabItems = [
     Component: NFTsContainer,
   },
 ]
+
+useEffect(() => {
+  NotificationsService.displayNotification({
+    channelId: ChannelId.DEFAULT_NOTIFICATION_CHANNEL_ID,
+    title: 'Welcome to the Assets tab',
+    body: 'Here you can see your tokens and NFTs',
+  })
+}, [])
 
 export function AssetsContainer() {
   return <SafeTab items={tabItems} headerHeight={200} renderHeader={AssetsHeaderContainer} />
