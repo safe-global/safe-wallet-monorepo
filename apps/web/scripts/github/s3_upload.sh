e423ced5b8fe5deb70a7de46f8f9387bc9d9c3d3
louis-md <louis@margot-duclot.fr>
2025-01-28 17:11:24 +0100

    Remove thread batches

--- apps/web/scripts/github/s3_upload.sh
+++ apps/web/scripts/github/s3_upload.sh
@@ -12,27 +12,10 @@ cd out
 aws s3 sync . $BUCKET --delete
 
 # Upload all HTML files again but w/o an extention so that URLs like /welcome open the right page
-# Max number of parallel processes
-MAX_PARALLEL=20
-
-# Counter to track the number of running processes
-counter=0
-
 for file in $(find . -name '*.html' | sed 's|^\./||'); do
-    # Launch the process
-    aws s3 cp "${file%}" "$BUCKET/${file%.*}" --content-type 'text/html' &
-
-    # Increment the counter
-    ((counter++))
-
-    # If the counter reaches the max limit, wait for all processes to complete
-    if ((counter >= MAX_PARALLEL)); then
-        wait
-        counter=0
-    fi
+    aws s3 cp ${file%} $BUCKET/${file%.*} --content-type 'text/html' &
 done
 
-# Wait for any remaining processes to complete
 wait
 
 cd -
