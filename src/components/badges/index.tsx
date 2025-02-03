b589b3253815918799aa9de8608bc44680854a7e
sebaxor <sebasor@gmail.com>
2025-02-03 04:00:27 -0500

    feat(badges): backend cache refetching interval

--- src/components/badges/index.tsx
+++ src/components/badges/index.tsx
@@ -21,6 +21,7 @@ function Badges() {
   }>({
     queryKey: ['badges', safeAddress, safeLoaded],
     queryFn: async () => await badgesService.getBadges(safeAddress as `0x${string}`),
+    refetchInterval: 5000,
     enabled: !!safeLoaded,
   })
   const isClaimable = useMemo(() => data?.currentBadges.some((badge) => badge.claimable), [data?.currentBadges])
