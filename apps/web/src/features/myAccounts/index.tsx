14ce90dd9f911524e31ec590751085fa22a6f91c
Germán Martínez <germaga92@gmail.com>
2025-02-04 08:15:40 +0000

    Add Safenet banner

--- apps/web/src/features/myAccounts/index.tsx
+++ apps/web/src/features/myAccounts/index.tsx
@@ -1,15 +1,16 @@
-import AccountListFilters from 'src/features/myAccounts/components/AccountListFilters'
 import AccountsHeader from '@/features/myAccounts/components/AccountsHeader'
 import AccountsList from '@/features/myAccounts/components/AccountsList'
-import { useState } from 'react'
-import { Box, Divider, Paper } from '@mui/material'
-import madProps from '@/utils/mad-props'
-import css from '@/features/myAccounts/styles.module.css'
-import useWallet from '@/hooks/wallets/useWallet'
-import { type AllSafeItemsGrouped, useAllSafesGrouped } from '@/features/myAccounts/hooks/useAllSafesGrouped'
-import classNames from 'classnames'
-import useTrackSafesCount from '@/features/myAccounts/hooks/useTrackedSafesCount'
 import { DataWidget } from '@/features/myAccounts/components/DataWidget'
+import { useAllSafesGrouped, type AllSafeItemsGrouped } from '@/features/myAccounts/hooks/useAllSafesGrouped'
+import useTrackSafesCount from '@/features/myAccounts/hooks/useTrackedSafesCount'
+import css from '@/features/myAccounts/styles.module.css'
+import SafenetBanner from '@/features/safenet/components/new-safe/SafenetBanner'
+import useWallet from '@/hooks/wallets/useWallet'
+import madProps from '@/utils/mad-props'
+import { Box, Divider, Paper } from '@mui/material'
+import classNames from 'classnames'
+import { useState } from 'react'
+import AccountListFilters from 'src/features/myAccounts/components/AccountListFilters'
 
 type MyAccountsProps = {
   safes: AllSafeItemsGrouped
@@ -26,6 +27,8 @@ const MyAccounts = ({ safes, onLinkClick, isSidebar = false }: MyAccountsProps) 
     <Box data-testid="sidebar-safe-container" className={css.container}>
       <Box className={classNames(css.myAccounts, { [css.sidebarAccounts]: isSidebar })}>
         <AccountsHeader isSidebar={isSidebar} onLinkClick={onLinkClick} />
+
+        {!isSidebar && <SafenetBanner />}
 
         <Paper sx={{ padding: 0 }}>
           <AccountListFilters setSearchQuery={setSearchQuery} />
