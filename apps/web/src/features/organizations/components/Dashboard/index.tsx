d2fa04a128fa85be6f2455cfbbee43318c7f4354
James Mealy <james@safe.global>
2025-02-05 16:40:42 +0000

    feat: add org dashboard signed out state

--- apps/web/src/features/organizations/components/Dashboard/index.tsx
+++ apps/web/src/features/organizations/components/Dashboard/index.tsx
@@ -1,5 +1,30 @@
+import { Box, Typography, Button } from '@mui/material'
+import css from './styles.module.css'
+
 const OrganizationsDashboard = () => {
-  return null
+  return (
+    <Box>
+      <Typography variant="h1" className={css.title}>
+        Dashboard
+      </Typography>
+
+      <Box className={css.content}>
+        <Box className={css.loggedOut}>
+          <Typography variant="h2" className={css.heading}>
+            Sign in to see your organisations
+          </Typography>
+
+          <Typography variant="body2" className={css.description}>
+            Description
+          </Typography>
+
+          <Button variant="contained" className={css.signInButton}>
+            Sign in
+          </Button>
+        </Box>
+      </Box>
+    </Box>
+  )
 }
 
 export default OrganizationsDashboard
