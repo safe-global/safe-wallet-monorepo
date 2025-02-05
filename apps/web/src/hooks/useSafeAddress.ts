a00d6b02569b62938075fad003a87f3e53e4295b
katspaugh <381895+katspaugh@users.noreply.github.com>
2025-02-05 11:34:11 +0100

    Safe address from URL

--- apps/web/src/hooks/useSafeAddress.ts
+++ apps/web/src/hooks/useSafeAddress.ts
@@ -1,11 +1,24 @@
 import { useMemo } from 'react'
-import { useRouter } from 'next/router'
+import { useParams } from 'next/navigation'
+import { parse, type ParsedUrlQuery } from 'querystring'
 import { parsePrefixedAddress } from '@/utils/addresses'
 
+// Use the location object directly because Next.js's router.query is available only on mount
+const getLocationQuery = (): ParsedUrlQuery => {
+  if (typeof location === 'undefined') return {}
+  const query = parse(location.search.slice(1))
+  return query
+}
+
+export const useUrlSafeAddress = (): string | undefined => {
+  const queryParams = useParams()
+  const query = queryParams && queryParams.safe ? queryParams : getLocationQuery()
+  const safe = query.safe?.toString() || ''
+  return safe
+}
+
 const useSafeAddress = (): string => {
-  const router = useRouter()
-  const { safe = '' } = router.query
-  const fullAddress = Array.isArray(safe) ? safe[0] : safe
+  const fullAddress = useUrlSafeAddress()
 
   const checksummedAddress = useMemo(() => {
     if (!fullAddress) return ''
