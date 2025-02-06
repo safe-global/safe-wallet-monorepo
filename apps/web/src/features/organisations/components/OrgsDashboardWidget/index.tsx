b9a273ca3735821d8ae307194bc2658eec97b9bc
katspaugh <381895+katspaugh@users.noreply.github.com>
2025-02-06 15:46:51 +0100

    Feat(Orgs): dashboard banner (#4900)

---
+++ apps/web/src/features/organisations/components/OrgsDashboardWidget/index.tsx
@@ -0,0 +1,42 @@
+import { Box, Button, Stack, Typography } from '@mui/material'
+import Link from 'next/link'
+import Track from '@/components/common/Track'
+
+const gradientBg = {
+  background: 'linear-gradient(225deg, rgba(95, 221, 255, 0.15) 12.5%, rgba(18, 255, 128, 0.15) 88.07%)',
+}
+
+const OrgsDashboardWidget = () => {
+  return (
+    <Stack direction="row" flexWrap="wrap" gap={2} py={2} px={3} sx={gradientBg}>
+      <Box flex={1} minWidth="60%">
+        <Typography variant="h6" fontWeight="700" mb={2}>
+          Organizations are here!
+        </Typography>
+
+        <Typography variant="body2">
+          Organize your Safe Accounts, all in one place. Collaborate efficiently with your team members and simplify
+          treasury management.
+          <br />
+          Available now in beta.
+        </Typography>
+      </Box>
+
+      <Stack direction="row" gap={2} alignItems="center">
+        <Track action="" category="" label="dashboard">
+          <Link href="" passHref>
+            <Button variant="outlined">Learn more</Button>
+          </Link>
+        </Track>
+
+        <Track action="" category="" label="dashboard">
+          <Link href="" passHref>
+            <Button variant="contained">Try now</Button>
+          </Link>
+        </Track>
+      </Stack>
+    </Stack>
+  )
+}
+
+export default OrgsDashboardWidget
