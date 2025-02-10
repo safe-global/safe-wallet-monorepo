64b30ba692e31df3515af0c69be8f4e957fb5619
katspaugh <381895+katspaugh@users.noreply.github.com>
2025-02-10 15:18:51 +0000

    Code style

--- apps/web/src/features/myAccounts/components/CurrentSafe/index.tsx
+++ apps/web/src/features/myAccounts/components/CurrentSafe/index.tsx
@@ -4,36 +4,38 @@ import { sameAddress } from '@/utils/addresses'
 import type { AllSafeItems } from '../../hooks/useAllSafesGrouped'
 import { useMemo } from 'react'
 import useAddressBook from '@/hooks/useAddressBook'
-import SafesList from '../SafesList'
-import type { SafeInfo } from '@safe-global/safe-gateway-typescript-sdk'
+import SingleAccountItem from '../AccountItems/SingleAccountItem'
 
-function CurrentSafeList({ safe, onLinkClick }: { safe: SafeInfo; onLinkClick?: () => void }) {
-  const {
-    chainId,
-    address: { value: safeAddress },
-  } = safe
+function CurrentSafeList({
+  safeAddress,
+  chainId,
+  onLinkClick,
+}: {
+  safeAddress: string
+  chainId: string
+  onLinkClick?: () => void
+}) {
   const addressBook = useAddressBook()
   const safeName = addressBook[safeAddress]
 
-  const safeItems = useMemo(
-    () => [
-      {
-        chainId,
-        address: safeAddress,
-        isReadOnly: true,
-        isPinned: false,
-        lastVisited: -1,
-        name: safeName,
-      },
-    ],
+  const safeItem = useMemo(
+    () => ({
+      chainId,
+      address: safeAddress,
+      isReadOnly: true,
+      isPinned: false,
+      lastVisited: -1,
+      name: safeName,
+    }),
     [chainId, safeAddress, safeName],
   )
 
-  return <SafesList safes={safeItems} onLinkClick={onLinkClick} />
+  return <SingleAccountItem onLinkClick={onLinkClick} safeItem={safeItem} />
 }
 
 function CurrentSafe({ allSafes, onLinkClick }: { allSafes: AllSafeItems; onLinkClick?: () => void }) {
   const { safe, safeAddress } = useSafeInfo()
+
   const isPinned = useMemo(
     () => safeAddress && allSafes?.some((s) => s.isPinned && sameAddress(s.address, safeAddress)),
     [allSafes, safeAddress],
@@ -41,12 +43,12 @@ function CurrentSafe({ allSafes, onLinkClick }: { allSafes: AllSafeItems; onLink
   if (!safeAddress || isPinned) return null
 
   return (
-    <Box mb={2}>
+    <Box mb={3}>
       <Typography variant="h5" fontWeight={700} mb={2}>
         Current Safe Account
       </Typography>
 
-      <CurrentSafeList onLinkClick={onLinkClick} safe={safe} />
+      <CurrentSafeList onLinkClick={onLinkClick} safeAddress={safeAddress} chainId={safe.chainId} />
     </Box>
   )
 }
