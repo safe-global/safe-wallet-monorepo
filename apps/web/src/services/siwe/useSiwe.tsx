9c81073ccb2cd39b3035af764174e0af8f795a03
James Mealy <james@safe.global>
2025-02-10 16:46:02 +0000

    catch failed SIWE attempts and log error

--- apps/web/src/services/siwe/useSiwe.tsx
+++ apps/web/src/services/siwe/useSiwe.tsx
@@ -2,6 +2,8 @@ import { useWeb3 } from '@/hooks/wallets/web3'
 import { useAuthGetNonceV1Query, useAuthVerifyV1Mutation } from '@safe-global/store/gateway/AUTO_GENERATED/auth'
 import { useCallback } from 'react'
 import { getSignableMessage } from './utils'
+import { logError } from '../exceptions'
+import ErrorCodes from '../exceptions/ErrorCodes'
 
 export const useSiwe = () => {
   const provider = useWeb3()
@@ -11,15 +13,19 @@ export const useSiwe = () => {
   const signIn = useCallback(async () => {
     if (!provider) return
 
-    const { data } = await fetchNonce()
-    if (!data) return
+    try {
+      const { data } = await fetchNonce()
+      if (!data) return
 
-    const [network, signer] = await Promise.all([provider.getNetwork(), provider.getSigner()])
-    const signableMessage = getSignableMessage(signer.address, network.chainId, data.nonce)
+      const [network, signer] = await Promise.all([provider.getNetwork(), provider.getSigner()])
+      const signableMessage = getSignableMessage(signer.address, network.chainId, data.nonce)
 
-    const signature = await signer.signMessage(signableMessage)
+      const signature = await signer.signMessage(signableMessage)
 
-    return verifyAuthMutation({ siweDto: { message: signableMessage, signature } })
+      return verifyAuthMutation({ siweDto: { message: signableMessage, signature } })
+    } catch (error) {
+      logError(ErrorCodes._640)
+    }
   }, [fetchNonce, provider, verifyAuthMutation])
 
   return {
