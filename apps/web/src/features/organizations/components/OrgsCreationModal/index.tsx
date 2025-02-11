ea9acd3bcce8d3fb92eddcd13ccfbc176eed0f04
James Mealy <james@safe.global>
2025-02-11 11:36:56 +0000

    feat: add org creation modal

---
+++ apps/web/src/features/organizations/components/OrgsCreationModal/index.tsx
@@ -0,0 +1,54 @@
+import type { ReactElement, BaseSyntheticEvent } from 'react'
+import { Box, Button, DialogActions, DialogContent, Typography } from '@mui/material'
+import { FormProvider, useForm } from 'react-hook-form'
+import MUILink from '@mui/material/Link'
+import Link from 'next/link'
+
+import ModalDialog from '@/components/common/ModalDialog'
+import NameInput from '@/components/common/NameInput'
+import { AppRoutes } from '@/config/routes'
+
+function OrgsCreationModal({ handleClose }: { handleClose: () => void }): ReactElement {
+  const methods = useForm<{ name: string }>({ mode: 'onChange' })
+  const { handleSubmit, formState } = methods
+
+  const onSubmit = (e: BaseSyntheticEvent) => {
+    e.stopPropagation()
+    handleSubmit((data) => {
+      console.log(data)
+      // TODO: create the organization
+      handleClose()
+    })
+  }
+
+  return (
+    <ModalDialog open onClose={handleClose} dialogTitle="Create organization" hideChainIndicator>
+      <FormProvider {...methods}>
+        <form onSubmit={onSubmit}>
+          <DialogContent sx={{ py: 2 }}>
+            <Box mb={2}>
+              <NameInput data-testid="name-input" label="Name" autoFocus name="name" required />
+            </Box>
+            <Typography variant="body2" color="text.secondary">
+              How is my data processed? Read our{' '}
+              <Link href={AppRoutes.privacy} passHref legacyBehavior>
+                <MUILink>privacy policy</MUILink>
+              </Link>
+            </Typography>
+          </DialogContent>
+
+          <DialogActions>
+            <Button data-testid="cancel-btn" onClick={handleClose}>
+              Cancel
+            </Button>
+            <Button type="submit" variant="contained" disabled={!formState.isValid} disableElevation>
+              Create organization
+            </Button>
+          </DialogActions>
+        </form>
+      </FormProvider>
+    </ModalDialog>
+  )
+}
+
+export default OrgsCreationModal
