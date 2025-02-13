7401afab028889827d99a09d54a8c064a194d8f7
Germán Martínez <germaga92@gmail.com>
2025-02-13 07:48:02 +0000

    Adjust SafeTag in dark mode

--- apps/web/src/features/safenet/components/SafenetTag/index.tsx
+++ apps/web/src/features/safenet/components/SafenetTag/index.tsx
@@ -1,12 +1,14 @@
+import { useDarkMode } from '@/hooks/useDarkMode'
+import SafenetDarkLogo from '@/public/images/safenet/logo-safenet-dark-gradient.svg'
 import SafenetLogo from '@/public/images/safenet/logo-safenet.svg'
 import { Box } from '@mui/material'
 import type { ReactElement } from 'react'
 import css from './styles.module.css'
 
-const SafenetTag = (): ReactElement => (
-  <Box className={css.tag}>
-    <SafenetLogo height="12" />
-  </Box>
-)
+const SafenetTag = (): ReactElement => {
+  const isDarkMode = useDarkMode()
+
+  return <Box className={css.tag}>{isDarkMode ? <SafenetLogo height="12" /> : <SafenetDarkLogo height="12" />}</Box>
+}
 
 export default SafenetTag
