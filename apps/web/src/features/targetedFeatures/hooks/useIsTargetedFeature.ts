927d53384247b970800270b9d19992306ea79470
Aaron Cook <aaron@safe.global>
2025-02-14 15:22:03 +0100

    fix: rename/relocate hooks and feature flags

--- apps/web/src/features/targetedFeatures/hooks/useIsTargetedFeature.ts
+++ apps/web/src/features/targetedFeatures/hooks/useIsTargetedFeature.ts
@@ -1,27 +1,11 @@
 import { useEffect } from 'react'
-import { useTargetedMessagingGetTargetedSafeV1Query } from '@safe-global/store/gateway/AUTO_GENERATED/targeted-messages'
 
+import { useIsOutreachSafe } from '@/features/targetedOutreach/hooks/useIsOutreachSafe'
 import { useHasFeature } from '@/hooks/useChains'
-import useSafeInfo from '@/hooks/useSafeInfo'
 import useLocalStorage from '@/services/local-storage/useLocalStorage'
-import { sameAddress } from '@/utils/addresses'
 import { TARGETED_FEATURES } from '../constants'
 
 const UNLOCKED_FEATURES_LS_KEY = 'unlockedFeatures'
-
-export function useIsTargetedSafe(outreachId: (typeof TARGETED_FEATURES)[number]['id']): boolean {
-  const { safe } = useSafeInfo()
-  const { data } = useTargetedMessagingGetTargetedSafeV1Query(
-    {
-      outreachId,
-      chainId: safe.chainId,
-      safeAddress: safe.address.value,
-    },
-    { skip: !safe.address.value },
-  )
-
-  return data?.outreachId === outreachId && sameAddress(data.address, safe.address.value)
-}
 
 type TargetedFeatures = (typeof TARGETED_FEATURES)[number]['feature']
 
@@ -29,7 +13,7 @@ export function useIsTargetedFeature(feature: TargetedFeatures): boolean {
   const hasFeature = useHasFeature(feature)
 
   const outreachId = TARGETED_FEATURES.find((f) => f.feature === feature)!.id
-  const isTargeted = useIsTargetedSafe(outreachId)
+  const isTargeted = useIsOutreachSafe(outreachId)
 
   // Should a targeted Safe have been opened, we "unlock" the feature across the app
   const [unlockedFeatures = [], setUnlockedFeatures] =
--- apps/web/src/features/targetedOutreach/constants.ts
+++ apps/web/src/features/targetedOutreach/constants.ts
@@ -1,11 +1,4 @@
-import { FEATURES } from '@/utils/chains'
-
 export const ACTIVE_OUTREACH = { id: 2, url: 'https://wn2n6ocviur.typeform.com/to/nlQlP7lU', targetAll: true }
-
-export const TARGETED_FEATURES = [
-  { id: 3, feature: FEATURES.NESTED_SAFES },
-  { id: 4, feature: FEATURES.MASS_PAYOUTS },
-] as const
 
 export const OUTREACH_LS_KEY = 'outreachPopup'
 export const OUTREACH_SS_KEY = 'outreachPopup_session'
