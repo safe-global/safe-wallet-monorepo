cde0fdb5bb0c033296976f438e4d712f5dbf2c9c
Usame Algan <usame@safe.global>
2025-02-18 12:25:11 +0100

    fix: Add org deletion modal

--- apps/web/src/features/organizations/components/OrgsSettings/index.tsx
+++ apps/web/src/features/organizations/components/OrgsSettings/index.tsx
@@ -1,9 +1,11 @@
-import { Button, Card, Grid2, TextField, Typography } from '@mui/material'
+import ModalDialog from '@/components/common/ModalDialog'
+import { Button, Card, DialogActions, DialogContent, Grid2, TextField, Typography } from '@mui/material'
 import {
   useOrganizationsGetOneV1Query,
   useOrganizationsUpdateV1Mutation,
 } from '@safe-global/store/gateway/AUTO_GENERATED/organizations'
 import { useRouter } from 'next/router'
+import { useState } from 'react'
 import { FormProvider, useForm } from 'react-hook-form'
 
 type OrganizationFormData = {
@@ -11,6 +13,7 @@ type OrganizationFormData = {
 }
 
 const OrgsSettings = () => {
+  const [deleteOrgOpen, setDeleteOrgOpen] = useState(false)
   const router = useRouter()
   const orgId = Array.isArray(router.query.orgId) ? router.query.orgId[0] : router.query.orgId
   const { data: org } = useOrganizationsGetOneV1Query({ id: Number(orgId) })
@@ -61,7 +64,37 @@ const OrgsSettings = () => {
             </FormProvider>
           </Grid2>
         </Grid2>
+
+        <Grid2 container p={4}>
+          <Grid2 size={4}>
+            <Typography fontWeight="bold">Danger Zone</Typography>
+          </Grid2>
+          <Grid2 size={8}>
+            <Typography mb={1}>This action cannot be undone.</Typography>
+
+            <Button
+              variant="danger"
+              onClick={() => {
+                setDeleteOrgOpen(true)
+              }}
+            >
+              Delete organization
+            </Button>
+          </Grid2>
+        </Grid2>
       </Card>
+      <ModalDialog dialogTitle="Delete organization" hideChainIndicator open={deleteOrgOpen}>
+        <DialogContent sx={{ mt: 2 }}>
+          <Typography>
+            Are you sure you want to delete <b>{org?.name}</b>?
+          </Typography>
+        </DialogContent>
+
+        <DialogActions>
+          <Button>No, keep it</Button>
+          <Button variant="danger">Permanently delete it</Button>
+        </DialogActions>
+      </ModalDialog>
     </div>
   )
 }
