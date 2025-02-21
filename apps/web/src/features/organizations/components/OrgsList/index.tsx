ed7f514ae3dd0974ab92c1539f5a08bf978be497
James Mealy <james@safe.global>
2025-02-21 14:44:45 +0100

    Feat(Orgs): show list of pending invites on the orgs list screen  (#5058)
    * feat: handle accepting and rejecting pending invites
    
    * fix: make accept and invite buttons smaller

--- apps/web/src/features/organizations/components/OrgsList/index.tsx
+++ apps/web/src/features/organizations/components/OrgsList/index.tsx
@@ -8,9 +8,12 @@ import { isAuthenticated } from '@/store/authSlice'
 import { Box, Button, Card, Grid2, Link, Typography } from '@mui/material'
 import type { GetOrganizationResponse } from '@safe-global/store/gateway/AUTO_GENERATED/organizations'
 import { useOrganizationsGetV1Query } from '@safe-global/store/gateway/AUTO_GENERATED/organizations'
+import type { UserWithWallets } from '@safe-global/store/gateway/AUTO_GENERATED/users'
+import { useUsersGetWithWalletsV1Query } from '@safe-global/store/gateway/AUTO_GENERATED/users'
+import OrgListInvite from '../Dashboard/DashboardInvite'
+import { MemberStatus } from '../Members'
 import { useState } from 'react'
 import css from './styles.module.css'
-import OrgListInvite from '../Dashboard/DashboardInvite'
 
 const AddOrgButton = ({ disabled }: { disabled: boolean }) => {
   const [open, setOpen] = useState(false)
@@ -74,19 +77,25 @@ const NoOrgsState = () => {
   )
 }
 
-// todo: replace with real data
-const invites: GetOrganizationResponse[] = [
-  {
-    id: 123,
-    name: 'Bidget',
-    status: 'ACTIVE',
-    userOrganizations: [],
-  },
-]
+const filterOrgsByStatus = (
+  currentUser: UserWithWallets | undefined,
+  orgs: GetOrganizationResponse[],
+  status: MemberStatus,
+) => {
+  return orgs?.filter((org) => {
+    // @ts-ignore TODO: fix incorrect type from CGW
+    return org.userOrganizations.some((userOrg) => userOrg.user.id === currentUser?.id && userOrg.status === status)
+  })
+}
 
+// todo: replace with real data
 const OrgsList = () => {
   const isUserSignedIn = useAppSelector(isAuthenticated)
+  const { data: currentUser } = useUsersGetWithWalletsV1Query()
   const { data: organizations } = useOrganizationsGetV1Query(undefined, { skip: !isUserSignedIn })
+
+  const pendingInvites = filterOrgsByStatus(currentUser, organizations || [], MemberStatus.INVITED)
+  const activeOrganizations = filterOrgsByStatus(currentUser, organizations || [], MemberStatus.ACTIVE)
 
   return (
     <Box className={css.container}>
@@ -97,15 +106,15 @@ const OrgsList = () => {
         </Box>
 
         {isUserSignedIn &&
-          invites.length > 0 &&
-          invites.map((invitingOrg: GetOrganizationResponse) => (
+          pendingInvites.length > 0 &&
+          pendingInvites.map((invitingOrg: GetOrganizationResponse) => (
             <OrgListInvite key={invitingOrg.id} org={invitingOrg} />
           ))}
 
         {isUserSignedIn && organizations ? (
           <Grid2 container spacing={2} flexWrap="wrap">
-            {organizations.length > 0 ? (
-              organizations.map((org) => (
+            {activeOrganizations.length > 0 ? (
+              activeOrganizations.map((org) => (
                 <Grid2 size={6} key={org.name}>
                   <OrgsCard org={org} />
                 </Grid2>
