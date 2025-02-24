0d8013463d3bba5a283cdba9f5886393beff8241
Germán Martínez <germaga92@gmail.com>
2025-02-24 12:13:49 +0000

    Display only USDC balances in Safenet balance overview

--- apps/web/src/features/safenet/components/SafenetBalanceOverview/index.tsx
+++ apps/web/src/features/safenet/components/SafenetBalanceOverview/index.tsx
@@ -1,5 +1,5 @@
 import ChainIndicator from '@/components/common/ChainIndicator'
-import FiatValue from '@/components/common/FiatValue'
+import TokenAmount from '@/components/common/TokenAmount'
 import useBalances from '@/hooks/useBalances'
 import { useDarkMode } from '@/hooks/useDarkMode'
 import InfoIcon from '@/public/images/notifications/info.svg'
@@ -7,16 +7,23 @@ import SafenetDarkIcon from '@/public/images/safenet/safenet-icon-dark.svg'
 import SafenetIcon from '@/public/images/safenet/safenet-icon.svg'
 import type { SafenetBalance } from '@/utils/safenet'
 import { Box, Divider, SvgIcon, Tooltip, Typography } from '@mui/material'
+import classnames from 'classnames'
 import { useMemo } from 'react'
 import css from './styles.module.css'
 
-function sumBalancesByChainId(balances: SafenetBalance[]): { [chainId: string]: number } {
+// Once there are fiat conversions for Safenet tokens, replace `balance` with `fiatBalance`
+function sumBalancesByChainId(balances: SafenetBalance[]): {
+  [chainId: string]: { balance: number }
+} {
   return balances.reduce(
-    (acc, { chainId, fiatBalance }) => {
-      acc[chainId] = (acc[chainId] || 0) + parseFloat(fiatBalance)
+    (acc, { chainId, balance }) => {
+      if (!acc[chainId]) {
+        acc[chainId] = { balance: 0 }
+      }
+      acc[chainId].balance += parseFloat(balance)
       return acc
     },
-    {} as { [chainId: string]: number },
+    {} as { [chainId: string]: { balance: number } },
   )
 }
 
@@ -24,10 +31,17 @@ const SafenetBalanceOverview = () => {
   const { balances } = useBalances()
   const isDarkMode = useDarkMode()
 
-  const totalSafenetValueFiat = useMemo(() => {
+  // Once there are fiat conversions for Safenet tokens, replace `balance` with `fiatBalance`
+  const totalSafenetBalance = useMemo(() => {
     const safenetTokens = balances.items.filter((token) => !!token.safenetBalance)
     const safenetTokenBalances = safenetTokens.map((token) => token.safenetBalance!).flat()
-    return safenetTokenBalances.reduce((acc, value) => acc + parseFloat(value.fiatBalance), 0)
+    return safenetTokenBalances.reduce(
+      (acc, { balance }) => {
+        acc.balance += parseFloat(balance)
+        return acc
+      },
+      { balance: 0 },
+    )
   }, [balances])
 
   const safenetBalancesByChain = useMemo(() => {
@@ -41,7 +55,8 @@ const SafenetBalanceOverview = () => {
       <SafenetIcon height="20" />
       <Typography color="text.secondary">Available Cross-Chain</Typography>
       <b>
-        <FiatValue value={totalSafenetValueFiat} precise />
+        {/*<FiatValue value={totalSafenetBalance.fiatBalance} precise />*/}
+        <TokenAmount value={totalSafenetBalance.balance.toString()} decimals={6} tokenSymbol="USDC" />
       </b>
       <Tooltip
         title={
@@ -53,15 +68,22 @@ const SafenetBalanceOverview = () => {
               </Typography>
             </Box>
             <Divider className={css.divider} />
-            <Box className={css.row}>
+            <Box className={classnames(css.row, isDarkMode ? css.darkTotal : css.lightTotal)}>
               <Typography fontSize={14}>Total</Typography>
-              <FiatValue value={totalSafenetValueFiat} precise />
+              {/*<FiatValue value={totalSafenetBalance.fiatBalance} precise />*/}
+              <TokenAmount
+                value={totalSafenetBalance.balance.toString()}
+                decimals={6}
+                tokenSymbol="USDC"
+                hasTooltip={false}
+              />
             </Box>
             <Divider className={css.divider} />
-            {Object.entries(safenetBalancesByChain).map(([chainId, fiatValue]) => (
+            {Object.entries(safenetBalancesByChain).map(([chainId, { balance }]) => (
               <Box className={css.row} key={chainId}>
                 <ChainIndicator chainId={chainId} />
-                <FiatValue value={fiatValue} precise />
+                {/*<FiatValue value={fiatBalance} precise />*/}
+                <TokenAmount value={balance.toString()} decimals={6} tokenSymbol="USDC" hasTooltip={false} />
               </Box>
             ))}
           </Box>
