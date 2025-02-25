b932ab0c704ade2b0b95757e9ac40833b7ddb685
Jonathansoufer <jonathan@safe.global>
2025-02-25 19:59:07 +0000

    feat: add address book list and itens

---
+++ apps/mobile/src/features/AddressBook/components/AddressBookView.tsx
@@ -0,0 +1,43 @@
+import { AddressInfo } from '@safe-global/store/gateway/AUTO_GENERATED/transactions'
+import React, { useCallback, useState } from 'react'
+
+import { NoContacts } from './List/NoContacts'
+import { AddressBookListHeader } from './List/AddressBookListHeader'
+import { View } from 'tamagui'
+import SafeSearchBar from '@/src/components/SafeSearchBar/SafeSearchBar'
+import ContactList from './List/AddressBookList'
+
+type Props = {
+  contacts: AddressInfo[]
+}
+
+export const AddressBookView = ({ contacts }: Props) => {
+  const [filteredContacts, setFilteredContacts] = useState<AddressInfo[]>(contacts)
+
+  const handleSearch = useCallback(
+    (query: string) => {
+      if (!query.trim()) {
+        setFilteredContacts(contacts)
+        return
+      }
+
+      const lowercaseQuery = query.toLowerCase()
+      const filtered = contacts.filter((contact) => {
+        const matchesName = contact.name?.toLowerCase().includes(lowercaseQuery)
+        const matchesAddress = contact.value.toLowerCase().includes(lowercaseQuery)
+        return matchesName || matchesAddress
+      })
+
+      setFilteredContacts(filtered)
+    },
+    [contacts],
+  )
+  return (
+    <View paddingHorizontal="$4" marginTop="$2" style={{ flex: 1 }} testID={'address-book-screen'}>
+      <AddressBookListHeader />
+      <SafeSearchBar placeholder="Name, address" onSearch={handleSearch} throttleTime={300} />
+      {contacts.length === 0 && <NoContacts />}
+      <ContactList contacts={filteredContacts} onSelectContact={() => console.log('search')} />
+    </View>
+  )
+}
