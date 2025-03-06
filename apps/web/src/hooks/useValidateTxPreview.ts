2cab9e5501b209d6471ff95a440b65c882658c7a
schmanu <manu@safe.global>
2025-03-06 10:27:54 +0100

    fix: catch unexpected errors while validating data

--- apps/web/src/hooks/useValidateTxPreview.ts
+++ apps/web/src/hooks/useValidateTxPreview.ts
@@ -1,3 +1,6 @@
+import { logError } from '@/services/exceptions'
+import ErrorCodes from '@/services/exceptions/ErrorCodes'
+import { asError } from '@/services/exceptions/utils'
 import { sameAddress } from '@/utils/addresses'
 import { type SafeTransaction } from '@safe-global/safe-core-sdk-types'
 import { type TransactionPreview } from '@safe-global/safe-gateway-typescript-sdk'
@@ -15,39 +18,46 @@ class InvalidPreviewError extends Error {
 export const useValidateTxPreview = (
   txPreview: TransactionPreview | undefined,
   safeTxData: SafeTransaction['data'] | undefined,
-) => {
+): Error | undefined => {
   if (!txPreview || !safeTxData) {
     return
   }
-  // Validate that the tx preview matches the txData
-  // First of all the top level data needs to match:
-  const matchesSafeTxData =
-    sameAddress(txPreview.txData.to.value, safeTxData.to) &&
-    Number(txPreview.txData.operation) === Number(safeTxData.operation) &&
-    txPreview.txData.value === safeTxData.value &&
-    (txPreview.txData.hexData ?? '0x') === safeTxData.data
 
-  if (!matchesSafeTxData) {
-    return new InvalidPreviewError("SafeTx data does not match the preview result's transaction data")
-  }
+  try {
+    // Validate that the tx preview matches the txData
+    // First of all the top level data needs to match:
+    const matchesSafeTxData =
+      sameAddress(txPreview.txData.to.value, safeTxData.to) &&
+      Number(txPreview.txData.operation) === Number(safeTxData.operation) &&
+      txPreview.txData.value === safeTxData.value &&
+      (txPreview.txData.hexData ?? '0x') === safeTxData.data
 
-  // validate the decodedData
-  const dataDecoded = txPreview.txData.dataDecoded
-
-  if (dataDecoded) {
-    // Fallback functions are special as they are called for all data that does not match any other function
-    if (dataDecoded.method === 'fallback') {
-      return
+    if (!matchesSafeTxData) {
+      return new InvalidPreviewError("SafeTx data does not match the preview result's transaction data")
     }
-    const abiString = `function ${dataDecoded.method}(${dataDecoded.parameters?.map((param) => param.type).join(',')})`
-    const abiInterface = new Interface([abiString])
-    const rawDataFromDecodedData = abiInterface.encodeFunctionData(
-      dataDecoded.method,
-      dataDecoded.parameters?.map((param) => param.value),
-    )
 
-    if (rawDataFromDecodedData !== safeTxData.data) {
-      return new InvalidPreviewError('Decoded data does not match raw data')
+    // validate the decodedData
+    const dataDecoded = txPreview.txData.dataDecoded
+
+    if (dataDecoded) {
+      // Fallback functions are special as they are called for all data that does not match any other function
+      if (dataDecoded.method === 'fallback') {
+        return
+      }
+      const abiString = `function ${dataDecoded.method}(${dataDecoded.parameters?.map((param) => param.type).join(',')})`
+      const abiInterface = new Interface([abiString])
+      const rawDataFromDecodedData = abiInterface.encodeFunctionData(
+        dataDecoded.method,
+        dataDecoded.parameters?.map((param) => param.value),
+      )
+
+      if (rawDataFromDecodedData !== safeTxData.data) {
+        return new InvalidPreviewError('Decoded data does not match raw data')
+      }
     }
+  } catch (err) {
+    const error = asError(err)
+    logError(ErrorCodes._818, error)
+    return error
   }
 }
