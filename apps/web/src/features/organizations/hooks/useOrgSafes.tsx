27346e2c177eff8a8326eae9ad6871fa373dd019
James Mealy <james@safe.global>
2025-03-07 15:24:58 +0000

    Feat(Orgs): Add search to accounts page (#5215)
    * Feat(Orgs): add search to accounts page
    
    * Fix: restore previous type

--- apps/web/src/features/organizations/hooks/useOrgSafes.tsx
+++ apps/web/src/features/organizations/hooks/useOrgSafes.tsx
@@ -7,21 +7,24 @@ import { selectOrderByPreference } from '@/store/orderByPreferenceSlice'
 import { getComparator } from '@/features/myAccounts/utils/utils'
 import { useMemo } from 'react'
 import type { SafeItem } from '@/features/myAccounts/hooks/useAllSafes'
+import { selectAllAddressBooks, type AddressBookState } from '@/store/addressBookSlice'
 
-function _buildSafeItems(safes: Record<string, string[]>): SafeItem[] {
+function _buildSafeItems(safes: Record<string, string[]>, allSafeNames: AddressBookState): SafeItem[] {
   const result: SafeItem[] = []
 
   for (const chainId in safes) {
     const addresses = safes[chainId]
 
     addresses.forEach((address) => {
+      const name = allSafeNames[chainId]?.[address]
+
       result.push({
         chainId,
         address,
         isReadOnly: false,
         isPinned: false,
         lastVisited: 0,
-        name: undefined,
+        name,
       })
     })
   }
@@ -32,8 +35,9 @@ function _buildSafeItems(safes: Record<string, string[]>): SafeItem[] {
 export const useOrgSafes = () => {
   const orgId = useCurrentOrgId()
   const { data } = useOrganizationSafesGetV1Query({ organizationId: Number(orgId) })
+  const allSafeNames = useAppSelector(selectAllAddressBooks)
   // @ts-ignore TODO: Fix type issue
-  const safeItems = data ? _buildSafeItems(data.safes) : undefined
+  const safeItems = data ? _buildSafeItems(data.safes, allSafeNames) : undefined
   const safes = useAllSafesGrouped(safeItems)
   const { orderBy } = useAppSelector(selectOrderByPreference)
   const sortComparator = getComparator(orderBy)
