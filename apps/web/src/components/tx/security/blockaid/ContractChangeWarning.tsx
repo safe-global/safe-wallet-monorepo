fd5a1a4293680177db3386fb183c8a94a2fc02f7
James Mealy <james@safe.global>
2025-03-07 10:43:07 +0000

    change mastercopy upgrade warning message

--- apps/web/src/components/tx/security/blockaid/ContractChangeWarning.tsx
+++ apps/web/src/components/tx/security/blockaid/ContractChangeWarning.tsx
@@ -21,9 +21,10 @@ const CONTRACT_CHANGE_TITLES_MAPPING: Record<
 const ProxyUpgradeSummary = ({ beforeAddress, afterAddress }: { beforeAddress: string; afterAddress: string }) => {
   return (
     <Stack direction="column" spacing={0.5}>
-      <Typography variant="overline" mt={1}>
-        Current mastercopy:
+      <Typography variant="body2" sx={{ marginBottom: 'var(--space-2) !important' }}>
+        Please verify that this change is intended and correct as it may overwrite the ownership of your account
       </Typography>
+      <Typography variant="overline">Current mastercopy:</Typography>
       <Box sx={{ padding: '6px 12px', borderRadius: '6px', backgroundColor: 'background.paper' }}>
         <EthHashInfo address={beforeAddress} showCopyButton hasExplorer shortAddress={false} showAvatar={false} />
       </Box>
@@ -48,11 +49,11 @@ export const ContractChangeWarning = ({
 
   const warningContent = (
     <>
-      <Typography variant="body2" mb={2}>
-        {isProxyUpgrade && 'This could allow someone else to take ownership of your account. '}Please verify that this
-        change is correct before proceeding.
-      </Typography>
-      {isProxyUpgrade && <ProxyUpgradeSummary beforeAddress={before.address} afterAddress={after.address} />}
+      {isProxyUpgrade ? (
+        <ProxyUpgradeSummary beforeAddress={before.address} afterAddress={after.address} />
+      ) : (
+        <Typography variant="body2">Please verify that this change is intended and correct.</Typography>
+      )}
     </>
   )
 
