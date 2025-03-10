81ba5d27e9a4e9da128aee2f8082a5edc66ce9a3
Germán Martínez <6764315+germartinez@users.noreply.github.com>
2025-03-10 11:39:18 +0100

    Feat(Safenet): Restrict the creation flow to allowlisted signers only (#5167)
    * Check Processor API allowlist of signers
    
    * Add hook to check Safenet allowlist

--- apps/web/src/store/safenet.ts
+++ apps/web/src/store/safenet.ts
@@ -170,14 +170,17 @@ export const safenetApi = createApi({
           threshold: number
         }
         saltNonce: string
+        dryRun?: boolean
       }
     >({
-      query: ({ account: { owners, threshold }, saltNonce }) => ({
-        url: `/account/deploy`,
-        method: 'POST',
-        body: { account: { owners, threshold }, saltNonce },
-      }),
-      providesTags: ['DeploySafenetAccount'],
+      query: ({ account: { owners, threshold }, saltNonce, dryRun }) => {
+        const url = `/account/deploy${dryRun ? `?dryRun=true` : ''}`
+        return {
+          url,
+          method: 'POST',
+          body: { account: { owners, threshold }, saltNonce },
+        }
+      },
     }),
   }),
 })
@@ -190,4 +193,5 @@ export const {
   useGetSafenetTransactionDetailsBySettlementQuery,
   useGetSafenetAccountQuery,
   useLazyDeploySafenetAccountQuery,
+  useDeploySafenetAccountQuery,
 } = safenetApi
