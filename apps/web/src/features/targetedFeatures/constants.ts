a5296eb361ec314b93d294f42e6db4248676e7e9
Tim Schwarz <4171783+tmjssz@users.noreply.github.com>
2025-03-12 10:29:51 +0100

    fix(Mass payouts): make feature flag non-targeted

--- apps/web/src/features/targetedFeatures/constants.ts
+++ apps/web/src/features/targetedFeatures/constants.ts
@@ -2,5 +2,5 @@ import { FEATURES } from '@/utils/chains'
 
 export const TARGETED_FEATURES = [
   { id: 3, feature: FEATURES.TARGETED_NESTED_SAFES },
-  { id: 4, feature: FEATURES.TARGETED_MASS_PAYOUTS },
+  { id: 4, feature: FEATURES.MASS_PAYOUTS },
 ] as const
