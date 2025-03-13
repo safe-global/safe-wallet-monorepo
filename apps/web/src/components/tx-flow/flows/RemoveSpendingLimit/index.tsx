a87498e9fa69b00bdb168d8505f2b391a0062d91
Tim Schwarz <4171783+tmjssz@users.noreply.github.com>
2025-03-13 17:01:25 +0100

    feat(RemoveSpendingLimitFlow): integrate transaction review step

--- apps/web/src/components/tx-flow/flows/RemoveSpendingLimit/index.tsx
+++ apps/web/src/components/tx-flow/flows/RemoveSpendingLimit/index.tsx
@@ -1,12 +1,38 @@
 import TxLayout from '@/components/tx-flow/common/TxLayout'
+import type { TxStep } from '../../common/TxLayout'
 import { RemoveSpendingLimit } from './RemoveSpendingLimit'
 import type { SpendingLimitState } from '@/store/spendingLimitsSlice'
 import SaveAddressIcon from '@/public/images/common/save-address.svg'
+import { useMemo } from 'react'
+import useTxStepper from '../../useTxStepper'
+import { ConfirmTxDetails } from '@/components/tx/ConfirmTxDetails'
 
 const RemoveSpendingLimitFlow = ({ spendingLimit }: { spendingLimit: SpendingLimitState }) => {
+  const { data, step, nextStep, prevStep } = useTxStepper(null)
+
+  const steps = useMemo<TxStep[]>(
+    () => [
+      {
+        txLayoutProps: { title: 'Confirm transaction' },
+        content: <RemoveSpendingLimit params={spendingLimit} key={0} onSubmit={() => nextStep(data)} />,
+      },
+      {
+        txLayoutProps: { title: 'Confirm transaction details', fixedNonce: true },
+        content: <ConfirmTxDetails key={1} onSubmit={() => {}} />,
+      },
+    ],
+    [nextStep, data, spendingLimit],
+  )
+
   return (
-    <TxLayout title="Confirm transaction" subtitle="Remove spending limit" icon={SaveAddressIcon}>
-      <RemoveSpendingLimit params={spendingLimit} />
+    <TxLayout
+      subtitle="Remove spending limit"
+      icon={SaveAddressIcon}
+      step={step}
+      onBack={prevStep}
+      {...(steps?.[step]?.txLayoutProps || {})}
+    >
+      {steps.map(({ content }) => content)}
     </TxLayout>
   )
 }
