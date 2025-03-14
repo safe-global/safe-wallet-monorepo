f63c5221d11561cc85a5630e826b609523e0065e
katspaugh <katspaugh@gmail.com>
2025-03-14 09:10:02 +0100

    Feat(Tx details): show "Unverified contract" for unknown contracts

--- apps/web/src/components/common/NamedAddressInfo/index.tsx
+++ apps/web/src/components/common/NamedAddressInfo/index.tsx
@@ -3,16 +3,31 @@ import useChainId from '@/hooks/useChainId'
 import { getContract } from '@safe-global/safe-gateway-typescript-sdk'
 import EthHashInfo from '../EthHashInfo'
 import type { EthHashInfoProps } from '../EthHashInfo/SrcEthHashInfo'
+import { useWeb3ReadOnly } from '@/hooks/wallets/web3'
 
-const NamedAddressInfo = ({ address, name, customAvatar, ...props }: EthHashInfoProps) => {
+function useAddressName(address: string, name?: string | null, customAvatar?: string) {
   const chainId = useChainId()
-  const [contract] = useAsync(
+  const web3 = useWeb3ReadOnly()
+
+  const [contract, error] = useAsync(
     () => (!name && !customAvatar ? getContract(chainId, address) : undefined),
     [address, chainId, name, customAvatar],
   )
 
-  const finalName = name || contract?.displayName || contract?.name
-  const finalAvatar = customAvatar || contract?.logoUri
+  const [isUnverifiedContract] = useAsync<boolean>(async () => {
+    if (!error) return false // Only check via RPC if getContract returned an error
+    const code = await web3?.getCode(address)
+    return code !== '0x'
+  }, [address, web3, error])
+
+  return {
+    name: name || contract?.displayName || contract?.name || (isUnverifiedContract ? 'Unverified contract' : undefined),
+    avatar: customAvatar || contract?.logoUri,
+  }
+}
+
+const NamedAddressInfo = ({ address, name, customAvatar, ...props }: EthHashInfoProps) => {
+  const { name: finalName, avatar: finalAvatar } = useAddressName(address, name, customAvatar)
 
   return <EthHashInfo address={address} name={finalName} customAvatar={finalAvatar} {...props} />
 }
