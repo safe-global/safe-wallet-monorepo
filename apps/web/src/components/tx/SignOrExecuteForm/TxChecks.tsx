b36f1ee528f130e3a73f4068169fcf22b5266e16
katspaugh <katspaugh@gmail.com>
2025-03-18 09:25:36 +0100

    Feat(Signing UX): show hashes for ledger

--- apps/web/src/components/tx/SignOrExecuteForm/TxChecks.tsx
+++ apps/web/src/components/tx/SignOrExecuteForm/TxChecks.tsx
@@ -4,10 +4,8 @@ import { FEATURES } from '@/utils/chains'
 import { type ReactElement } from 'react'
 import { TxSimulation, TxSimulationMessage } from '@/components/tx/security/tenderly'
 import TxCard from '@/components/tx-flow/common/TxCard'
-import { Alert, Box, Typography } from '@mui/material'
-import ExternalLink from '@/components/common/ExternalLink'
-import Track from '@/components/common/Track'
-import { MODALS_EVENTS } from '@/services/analytics'
+import { Box, Typography } from '@mui/material'
+
 import type { SafeTransaction, MetaTransactionData } from '@safe-global/safe-core-sdk-types'
 
 import css from './styles.module.css'
@@ -30,14 +28,6 @@ const TxChecks = ({
   return (
     <TxCard>
       <Typography variant="h5">Transaction checks</Typography>
-
-      <Alert severity="info">
-        We recommend analyzing your transaction with a third-party tool like{' '}
-        <Track {...MODALS_EVENTS.OPEN_SAFE_UTILS}>
-          <ExternalLink href={SAFE_UTILS_URL}>Safe Utils</ExternalLink>
-        </Track>
-        . Third-party terms & disclaimers may apply.
-      </Alert>
 
       {(isTxSimulationFeatureEnabled || isRiskMitigationFeatureEnabled) && (
         <>
