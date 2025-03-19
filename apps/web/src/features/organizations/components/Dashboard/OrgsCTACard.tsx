b7c28f5c40e684c97bef369ddb1d03cf74733b44
James Mealy <james@safe.global>
2025-03-19 15:02:17 +0000

    Feat(Orgs): open info modal when clicking the learn more button on the org dashboard

--- apps/web/src/features/organizations/components/Dashboard/OrgsCTACard.tsx
+++ apps/web/src/features/organizations/components/Dashboard/OrgsCTACard.tsx
@@ -1,43 +1,51 @@
 import css from '@/features/organizations/components/Dashboard/styles.module.css'
 import LightbulbIcon from '@/public/images/common/lightbulb.svg'
 import { Typography, Paper, Box, Button, SvgIcon } from '@mui/material'
+import OrgsInfoModal from '../OrgsInfoModal'
+import { useState } from 'react'
 
 const OrgsCTACard = () => {
+  const [isInfoOpen, setIsInfoOpen] = useState<boolean>(false)
+
   const handleLearnMore = () => {
+    setIsInfoOpen(true)
     // TODO: Implement invite functionality
     console.log('Learn more clicked')
   }
 
   return (
-    <Paper sx={{ p: 3, borderRadius: '12px', height: '100%' }}>
-      <Box position="relative" width={1}>
-        <Box className={css.iconBG}>
-          <SvgIcon component={LightbulbIcon} inheritViewBox />
-        </Box>
+    <>
+      <Paper sx={{ p: 3, borderRadius: '12px', height: '100%' }}>
+        <Box position="relative" width={1}>
+          <Box className={css.iconBG}>
+            <SvgIcon component={LightbulbIcon} inheritViewBox />
+          </Box>
 
-        <Button
-          onClick={handleLearnMore}
-          variant="outlined"
-          size="compact"
-          sx={{
-            position: 'absolute',
-            top: 0,
-            right: 0,
-          }}
-          aria-label="Invite team members"
-        >
-          Learn more
-        </Button>
-      </Box>
-      <Box>
-        <Typography variant="body1" color="text.primary" fontWeight={700} mb={1}>
-          Explore organizations
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          Seamlessly use your Safe Accounts from one place and collaborate with your team members.
-        </Typography>
-      </Box>
-    </Paper>
+          <Button
+            onClick={handleLearnMore}
+            variant="outlined"
+            size="compact"
+            sx={{
+              position: 'absolute',
+              top: 0,
+              right: 0,
+            }}
+            aria-label="Invite team members"
+          >
+            Learn more
+          </Button>
+        </Box>
+        <Box>
+          <Typography variant="body1" color="text.primary" fontWeight={700} mb={1}>
+            Explore organizations
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Seamlessly use your Safe Accounts from one place and collaborate with your team members.
+          </Typography>
+        </Box>
+      </Paper>
+      {isInfoOpen && <OrgsInfoModal showButtons={false} onClose={() => setIsInfoOpen(false)} />}
+    </>
   )
 }
 
