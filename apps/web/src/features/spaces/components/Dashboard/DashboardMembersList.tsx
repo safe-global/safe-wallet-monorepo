a893b7a5552b96ae927e9efb1b38727248ad3184
James Mealy <james@safe.global>
2025-03-20 14:24:23 +0000

    Fix(Orgs): hide add members button on dashboard members list for non admins

--- apps/web/src/features/spaces/components/Dashboard/DashboardMembersList.tsx
+++ apps/web/src/features/spaces/components/Dashboard/DashboardMembersList.tsx
@@ -1,4 +1,4 @@
-import { Paper, Button, Box } from '@mui/material'
+import { Paper, Button, Box, Stack } from '@mui/material'
 import type { UserOrganization } from '@safe-global/store/gateway/AUTO_GENERATED/organizations'
 import PlusIcon from '@/public/images/common/plus.svg'
 import { useState } from 'react'
@@ -6,25 +6,31 @@ import AddMembersModal from '../AddMembersModal'
 import MemberName from '../MembersList/MemberName'
 import { SPACE_EVENTS, SPACE_LABELS } from '@/services/analytics/events/spaces'
 import Track from '@/components/common/Track'
+import { useIsAdmin } from '@/features/spaces/hooks/useSpaceMembers'
 
 const DashboardMembersList = ({ members }: { members: UserOrganization[] }) => {
   const [openAddMembersModal, setOpenAddMembersModal] = useState(false)
+  const isAdmin = useIsAdmin()
 
   return (
     <>
       <Paper sx={{ p: 2, borderRadius: '8px' }}>
-        {members.map((member) => (
-          <Box mb={2} key={member.id}>
-            <MemberName key={member.id} member={member} />
+        <Stack spacing={2}>
+          {members.map((member) => (
+            <Box key={member.id}>
+              <MemberName key={member.id} member={member} />
+            </Box>
+          ))}
+        </Stack>
+        {isAdmin && (
+          <Box display="flex" justifyContent="center" mt={2}>
+            <Track {...SPACE_EVENTS.ADD_MEMBER_MODAL} label={SPACE_LABELS.space_dashboard}>
+              <Button size="small" variant="text" startIcon={<PlusIcon />} onClick={() => setOpenAddMembersModal(true)}>
+                Add member
+              </Button>
+            </Track>
           </Box>
-        ))}
-        <Box display="flex" justifyContent="center">
-          <Track {...SPACE_EVENTS.ADD_MEMBER_MODAL} label={SPACE_LABELS.space_dashboard}>
-            <Button size="small" variant="text" startIcon={<PlusIcon />} onClick={() => setOpenAddMembersModal(true)}>
-              Add member
-            </Button>
-          </Track>
-        </Box>
+        )}
       </Paper>
       {openAddMembersModal && <AddMembersModal onClose={() => setOpenAddMembersModal(false)} />}
     </>
