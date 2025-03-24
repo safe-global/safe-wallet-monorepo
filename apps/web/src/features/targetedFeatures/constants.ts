9b6fd077086bd9166fac37314a7ff90d214b2ace
Tim Schwarz <4171783+tmjssz@users.noreply.github.com>
2025-03-24 13:08:40 +0100

    fix: revert changes on TARGETED_FEATURES

--- apps/web/src/features/targetedFeatures/constants.ts
+++ apps/web/src/features/targetedFeatures/constants.ts
@@ -1,3 +1,3 @@
-import type { FEATURES } from '@/utils/chains'
+import { FEATURES } from '@/utils/chains'
 
-export const TARGETED_FEATURES: { id: number; feature: FEATURES }[] = []
+export const TARGETED_FEATURES = [{ id: 3, feature: FEATURES.NESTED_SAFES }] as const
