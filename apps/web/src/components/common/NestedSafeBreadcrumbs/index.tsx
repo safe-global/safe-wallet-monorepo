563859780fcdb389313607c6d76db6b0e30260b8
Usame Algan <usame@safe.global>
2025-03-25 15:05:41 +0100

    feat: Show breadcrumbs if navigating to a safe from a space

--- apps/web/src/components/common/NestedSafeBreadcrumbs/index.tsx
+++ apps/web/src/components/common/NestedSafeBreadcrumbs/index.tsx
@@ -1,16 +1,10 @@
 import { useRouter } from 'next/router'
-import Link from 'next/link'
-import { Tooltip, Typography, useMediaQuery, useTheme } from '@mui/material'
+import { Typography } from '@mui/material'
 import type { ReactElement } from 'react'
-import type { UrlObject } from 'url'
 
 import useSafeInfo from '@/hooks/useSafeInfo'
-import useAddressBook from '@/hooks/useAddressBook'
-import Identicon from '../Identicon'
-import { shortenAddress } from '@/utils/formatters'
-
-import css from './styles.module.css'
 import { useParentSafe } from '@/hooks/useParentSafe'
+import { BreadcrumbItem } from '@/components/common/Breadcrumbs/BreadcrumbItem'
 
 export function NestedSafeBreadcrumbs(): ReactElement | null {
   const { pathname } = useRouter()
@@ -22,7 +16,7 @@ export function NestedSafeBreadcrumbs(): ReactElement | null {
   }
 
   return (
-    <div className={css.container}>
+    <>
       <BreadcrumbItem
         title="Parent Safe"
         address={parentSafe.address.value}
@@ -30,30 +24,6 @@ export function NestedSafeBreadcrumbs(): ReactElement | null {
       />
       <Typography variant="body2">/</Typography>
       <BreadcrumbItem title="Nested Safe" address={safeAddress} />
-    </div>
-  )
-}
-
-const BreadcrumbItem = ({ title, address, href }: { title: string; address: string; href?: UrlObject }) => {
-  const theme = useTheme()
-  const isMobile = useMediaQuery(theme.breakpoints.down('sm'))
-  const addressBook = useAddressBook()
-  const name = addressBook[address] ?? (isMobile ? shortenAddress(address) : address)
-
-  return (
-    <Tooltip title={title}>
-      <div className={css.breadcrumb}>
-        <Identicon address={address} size={20} />
-        {href ? (
-          <Link href={href}>
-            <Typography variant="body2" color="text.secondary">
-              {name}
-            </Typography>
-          </Link>
-        ) : (
-          <Typography variant="body2">{name}</Typography>
-        )}
-      </div>
-    </Tooltip>
+    </>
   )
 }
