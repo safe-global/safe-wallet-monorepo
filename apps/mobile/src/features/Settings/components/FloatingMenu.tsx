f2fd6e4cb32a6bd6102e48866ee0d8d62ea00e4e
Jonathansoufer <jonathan@safe.global>
2025-03-26 20:56:29 +0000

    chore(theme): add theme awareness to component

--- apps/mobile/src/features/Settings/components/FloatingMenu.tsx
+++ apps/mobile/src/features/Settings/components/FloatingMenu.tsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import { Pressable } from 'react-native'
 import { MenuAction, MenuView, NativeActionEvent } from '@react-native-menu/menu'
+import { ThemePreference } from '@/src/theme/provider/safeTheme'
 
 type FloatingMenuProps = {
   onPressAction: (event: NativeActionEvent) => void
   actions: MenuAction[]
   children: React.ReactNode
+  themeVariant?: ThemePreference
 }
-export const FloatingMenu = ({ onPressAction, actions, children }: FloatingMenuProps) => {
+export const FloatingMenu = ({ onPressAction, actions, children, themeVariant }: FloatingMenuProps) => {
   return (
-    <MenuView onPressAction={onPressAction} actions={actions} shouldOpenOnLongPress={false}>
+    <MenuView themeVariant={themeVariant} onPressAction={onPressAction} actions={actions} shouldOpenOnLongPress={false}>
       <Pressable testID={'settings-screen-header-more-settings-button'}>{children}</Pressable>
     </MenuView>
   )
