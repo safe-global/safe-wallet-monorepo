76af58945fdc8ff9193fb69a0cc387601dd9afa1
Daniel Dimitrov <daniel.d@safe.global>
2025-03-27 22:20:33 +0100

    fix(mobile): history wrong amount for transfers

--- apps/mobile/src/components/TokenAmount/TokenAmount.tsx
+++ apps/mobile/src/components/TokenAmount/TokenAmount.tsx
@@ -1,10 +1,20 @@
 import { type ReactElement } from 'react'
 import { formatVisualAmount } from '@safe-global/utils/utils/formatters'
 import { TransferDirection } from '@safe-global/store/gateway/types'
-import { Text } from 'tamagui'
+import { Text, TextProps } from 'tamagui'
 import { TransferTransactionInfo } from '@safe-global/store/gateway/AUTO_GENERATED/transactions'
 
 const PRECISION = 20
+
+interface TokenAmountProps {
+  value: string
+  decimals?: number | null
+  tokenSymbol?: string
+  direction?: TransferTransactionInfo['direction']
+  preciseAmount?: boolean
+  textProps?: TextProps
+  displayPositiveSign?: boolean
+}
 
 export const TokenAmount = ({
   value,
@@ -12,23 +22,27 @@ export const TokenAmount = ({
   tokenSymbol,
   direction,
   preciseAmount,
-}: {
-  value: string
-  decimals?: number | null
-  tokenSymbol?: string
-  direction?: TransferTransactionInfo['direction']
-  preciseAmount?: boolean
-}): ReactElement => {
-  const sign = direction === TransferDirection.OUTGOING ? '-' : ''
-  const amount =
-    decimals !== undefined && decimals !== null
-      ? formatVisualAmount(value, decimals, preciseAmount ? PRECISION : undefined)
-      : value
+  textProps,
+  displayPositiveSign,
+}: TokenAmountProps): ReactElement => {
+  const getSign = (): string => {
+    if (direction === TransferDirection.OUTGOING) {
+      return '-'
+    }
+    return displayPositiveSign ? '+' : ''
+  }
+
+  const formatAmount = (): string => {
+    if (decimals === undefined || decimals === null) {
+      return value
+    }
+    return formatVisualAmount(value, decimals, preciseAmount ? PRECISION : undefined)
+  }
 
   return (
-    <Text fontWeight={700}>
-      {sign}
-      {amount} {tokenSymbol}
+    <Text fontWeight={700} {...textProps}>
+      {getSign()}
+      {formatAmount()} {tokenSymbol}
     </Text>
   )
 }
--- apps/mobile/src/components/transactions-list/Card/TxTokenCard/TxTokenCard.tsx
+++ apps/mobile/src/components/transactions-list/Card/TxTokenCard/TxTokenCard.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { Text, View } from 'tamagui'
 import { SafeListItem } from '@/src/components/SafeListItem'
 import { isERC721Transfer, isOutgoingTransfer, isTxQueued } from '@/src/utils/transaction-guards'
-import { ellipsis } from '@/src/utils/formatters'
 import { TransferDirection } from '@safe-global/store/gateway/types'
 import { TransferTransactionInfo, Transaction } from '@safe-global/store/gateway/AUTO_GENERATED/transactions'
 import { Logo } from '@/src/components/Logo'
@@ -25,7 +24,6 @@ export function TxTokenCard({ bordered, inQueue, txStatus, executionInfo, txInfo
   const isERC721 = isERC721Transfer(txInfo.transferInfo)
   const isOutgoing = txInfo.direction === TransferDirection.OUTGOING
 
-  console.log('is send tx', isSendTx, value, decimals)
   return (
     <SafeListItem
       inQueue={inQueue}
@@ -38,8 +36,16 @@ export function TxTokenCard({ bordered, inQueue, txStatus, executionInfo, txInfo
       leftNode={<Logo logoUri={logoUri} accessibilityLabel={name} />}
       rightNode={
         <View maxWidth="34%">
-          <Text color={isOutgoing ? '$color' : '$primary'} textAlign="right">
-            {isOutgoing ? '-' : '+'} {ellipsis(value, 8)} {!isERC721 && tokenSymbol}
+          <Text>
+            <TokenAmount
+              value={value}
+              decimals={decimals}
+              tokenSymbol={!isERC721 ? tokenSymbol : ''}
+              direction={txInfo.direction}
+              preciseAmount
+              displayPositiveSign
+              textProps={{ color: isOutgoing ? '$color' : '$primary', textAlign: 'right', fontWeight: 400 }}
+            />
           </Text>
         </View>
       }
