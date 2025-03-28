478c4731e01d1c42ec9cee743ad969ecddc2e97e
Usame Algan <5880855+usame-algan@users.noreply.github.com>
2025-03-28 10:50:22 +0100

    fix: Redirect user to accounts page if spaces feature is turned off (#5531)

--- apps/web/src/pages/welcome/spaces.tsx
+++ apps/web/src/pages/welcome/spaces.tsx
@@ -2,15 +2,21 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import SpacesList from '@/features/spaces/components/SpacesList'
 import { BRAND_NAME } from '@/config/constants'
+import useFeatureFlagRedirect from '@/features/spaces/hooks/useFeatureFlagRedirect'
+import { useHasFeature } from '@/hooks/useChains'
+import { FEATURES } from '@/utils/chains'
 
 const Spaces: NextPage = () => {
+  const isSpacesFeatureEnabled = useHasFeature(FEATURES.SPACES)
+  useFeatureFlagRedirect()
+
   return (
     <>
       <Head>
         <title>{`${BRAND_NAME} – Spaces`}</title>
       </Head>
 
-      <SpacesList />
+      {isSpacesFeatureEnabled && <SpacesList />}
     </>
   )
 }
