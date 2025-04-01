401529a5d5622896f1b18123068c229f3d091abb
Tim Schwarz <4171783+tmjssz@users.noreply.github.com>
2025-04-01 12:09:29 +0200

    feat(tx-flow): rename ReviewTransactionContent to TxChecks and add isCounterfactualSafe prop

--- apps/web/src/components/tx-flow-2/features/TxChecks.tsx
+++ apps/web/src/components/tx-flow-2/features/TxChecks.tsx
@@ -4,28 +4,29 @@ import madProps from '@/utils/mad-props'
 import { SafeTxContext } from '@/components/tx-flow/SafeTxProvider'
 import { TxFlowContext } from '@/components/tx-flow-2/TxFlowProvider'
 import useIsCounterfactualSafe from '@/features/counterfactual/hooks/useIsCounterfactualSafe'
-import TxChecks from '@/components/tx/SignOrExecuteForm/TxChecks'
+import TxChecksComponent from '@/components/tx/SignOrExecuteForm/TxChecks'
 
-export const ReviewTransactionContent = ({
+export const TxChecks = ({
   safeTx,
   isRejection,
+  isCounterfactualSafe,
 }: {
   safeTx: ReturnType<typeof useSafeTx>
   isRejection: ReturnType<typeof useIsRejection>
+  isCounterfactualSafe: ReturnType<typeof useIsCounterfactualSafe>
 }): ReactElement | null => {
-  const isCounterfactualSafe = useIsCounterfactualSafe()
-
   if (isCounterfactualSafe || isRejection || !safeTx) {
     return null
   }
 
-  return <TxChecks transaction={safeTx} />
+  return <TxChecksComponent transaction={safeTx} />
 }
 
 const useSafeTx = () => useContext(SafeTxContext).safeTx
 const useIsRejection = () => useContext(TxFlowContext).isRejection
 
-export default madProps(ReviewTransactionContent, {
+export default madProps(TxChecks, {
   safeTx: useSafeTx,
   isRejection: useIsRejection,
+  isCounterfactualSafe: useIsCounterfactualSafe,
 })
