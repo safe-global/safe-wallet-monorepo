d0026a32d115fa9bc3fdf57db4f283d458cd44a4
Daniel Dimitrov <daniel.d@safe.global>
2025-04-01 10:42:27 +0200

    chore(store): update types

--- packages/store/src/gateway/AUTO_GENERATED/transactions.ts
+++ packages/store/src/gateway/AUTO_GENERATED/transactions.ts
@@ -353,15 +353,27 @@ export type CustomTransactionInfo = {
   methodName?: string | null
   actionCount?: number | null
 }
+export type BaseDataDecoded = {
+  method: string
+  parameters?: DataDecodedParameter[]
+}
+export type MultiSend = {
+  operation: 0 | 1
+  value: string
+  dataDecoded?: BaseDataDecoded
+  to: string
+  data?: object
+}
 export type DataDecodedParameter = {
   name: string
   type: string
   value: object
-  valueDecoded?: ((object | null) | (object[] | null)) | null
+  valueDecoded?: BaseDataDecoded | MultiSend[] | null
 }
 export type DataDecoded = {
   method: string
   parameters?: DataDecodedParameter[] | null
+  accuracy?: 'FULL_MATCH' | 'PARTIAL_MATCH' | 'ONLY_FUNCTION_MATCH' | 'NO_MATCH' | 'UNKNOWN'
 }
 export type AddOwner = {
   type: 'ADD_OWNER'
