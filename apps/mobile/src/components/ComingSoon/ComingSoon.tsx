4993da9e5e0cc0a792ef80b57cbc5e16100a3588
Jonathansoufer <jonathan@safe.global>
2025-04-03 10:23:34 +0100

    fix(merge): alignments

--- apps/mobile/src/components/ComingSoon/ComingSoon.tsx
+++ apps/mobile/src/components/ComingSoon/ComingSoon.tsx
@@ -5,8 +5,8 @@ export const ComingSoon = () => {
   return (
     <View testID="coming-soon" alignItems="center" gap="$4" marginTop={'$4'}>
       <H5 fontWeight={600}>Coming soon</H5>
-      <Text textAlign="center" color="$colorSecondary" width="80%">
-        We’re working on this message view. Details aren’t available just yet—but they will be soon.
+      <Text textAlign="center" color="$colorSecondary">
+        We’re working on this message view. Details aren’t available just yet—but they will be soon
       </Text>
     </View>
   )
