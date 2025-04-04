7af53ff1103f1e1d4496d084fb9c2040d9b4e733
Tim Schwarz <4171783+tmjssz@users.noreply.github.com>
2025-04-04 13:56:35 +0200

    feat(tx-flow): add error handling for transaction batching and display error message

--- apps/web/src/components/tx-flow/actions/Batching.tsx
+++ apps/web/src/components/tx-flow/actions/Batching.tsx
@@ -1,4 +1,4 @@
-import { useContext, type SyntheticEvent } from 'react'
+import { useContext, useState, type SyntheticEvent } from 'react'
 import { SafeTxContext } from '@/components/tx-flow/SafeTxProvider'
 import BatchButton from '@/components/tx/SignOrExecuteForm/BatchButton'
 import { useTxActions } from '@/components/tx/SignOrExecuteForm/hooks'
@@ -8,12 +8,16 @@ import { TxModalContext } from '@/components/tx-flow'
 import { TxFlowContext } from '../TxFlowProvider'
 import useIsCounterfactualSafe from '@/features/counterfactual/hooks/useIsCounterfactualSafe'
 import { SlotName, withSlot } from '../slots'
+import { asError } from '@safe-global/utils/services/exceptions/utils'
+import ErrorMessage from '@/components/tx/ErrorMessage'
+import { Errors, logError } from '@/services/exceptions'
 
 const Batching = () => {
   const { setTxFlow } = useContext(TxModalContext)
   const { addToBatch } = useTxActions()
   const { safeTx } = useContext(SafeTxContext)
   const { isSubmittable, setIsSubmittable } = useContext(TxFlowContext)
+  const [submitError, setSubmitError] = useState<Error | undefined>()
 
   const isBatchable = !!safeTx && !isDelegateCall(safeTx)
 
@@ -24,7 +28,16 @@ const Batching = () => {
 
     setIsSubmittable(false)
 
-    await addToBatch(safeTx, origin)
+    try {
+      await addToBatch(safeTx, origin)
+    } catch (_err) {
+      const err = asError(_err)
+      logError(Errors._819, err)
+      setSubmitError(err)
+
+      setIsSubmittable(true)
+      return
+    }
 
     setIsSubmittable(true)
 
@@ -32,11 +45,17 @@ const Batching = () => {
   }
 
   return (
-    <BatchButton
-      onClick={onBatchClick}
-      disabled={!isSubmittable || !isBatchable}
-      tooltip={!isBatchable ? `Cannot batch this type of transaction` : undefined}
-    />
+    <>
+      {submitError && (
+        <ErrorMessage error={submitError}>Error batching the transaction. Please try again.</ErrorMessage>
+      )}
+
+      <BatchButton
+        onClick={onBatchClick}
+        disabled={!isSubmittable || !isBatchable}
+        tooltip={!isBatchable ? `Cannot batch this type of transaction` : undefined}
+      />
+    </>
   )
 }
 
