a9d7f2a7a3162a71e20a257f1f26ee07b8df46a4
katspaugh <katspaugh@gmail.com>
2025-04-09 15:55:31 +0200

    Update tx decoder link

--- apps/web/src/components/transactions/TxDetails/Summary/DecoderLinks.tsx
+++ apps/web/src/components/transactions/TxDetails/Summary/DecoderLinks.tsx
@@ -1,15 +1,15 @@
 import ExternalLink from '@/components/common/ExternalLink'
 import { Paper, Typography } from '@mui/material'
 
-const TX_DECODER_URL = 'https://transaction-decoder.vercel.app'
-const SAFE_UTILS_URL = 'https://safeutils.openzeppelin.com/'
+const TX_DECODER_URL = 'https://transaction-decoder.pages.dev'
+const SAFE_UTILS_URL = 'https://safeutils.openzeppelin.com'
 
 const DecoderLinsk = () => (
   <Paper sx={{ backgroundColor: 'var(--color-background-main) !important', p: 2, my: 3 }}>
     <Typography variant="body2" color="primary.light">
       Cross-verify your transaction data with external tools like{' '}
       <ExternalLink href={SAFE_UTILS_URL}>Safe Utils</ExternalLink> and{' '}
-      <ExternalLink href={TX_DECODER_URL}>Decoder Tool</ExternalLink>.
+      <ExternalLink href={TX_DECODER_URL}>Transaction Decoder</ExternalLink>.
     </Typography>
   </Paper>
 )
