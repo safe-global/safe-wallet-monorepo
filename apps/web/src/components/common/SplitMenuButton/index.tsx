286d2ba589bb00fa10955527bdf62cfe5cfbe03a
Tim Schwarz <4171783+tmjssz@users.noreply.github.com>
2025-04-09 17:15:58 +0200

    fix(SplitMenuButton): replace Popper with Popover and fix styling

--- apps/web/src/components/common/SplitMenuButton/index.tsx
+++ apps/web/src/components/common/SplitMenuButton/index.tsx
@@ -2,13 +2,9 @@ import { useEffect, useRef, useState, type SyntheticEvent } from 'react'
 import Button from '@mui/material/Button'
 import ButtonGroup from '@mui/material/ButtonGroup'
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown'
-import ClickAwayListener from '@mui/material/ClickAwayListener'
-import Grow from '@mui/material/Grow'
-import Paper from '@mui/material/Paper'
-import Popper from '@mui/material/Popper'
 import MenuItem from '@mui/material/MenuItem'
 import MenuList from '@mui/material/MenuList'
-import { CircularProgress, Tooltip } from '@mui/material'
+import { Box, CircularProgress, Popover, Tooltip } from '@mui/material'
 
 export default function SplitMenuButton({
   options,
@@ -46,7 +42,8 @@ export default function SplitMenuButton({
     onClick?.(options[selectedIndex], e)
   }
 
-  const handleMenuItemClick = (event: React.MouseEvent<HTMLLIElement, MouseEvent>, index: number) => {
+  const handleMenuItemClick = (e: React.MouseEvent<HTMLLIElement, MouseEvent>, index: number) => {
+    e.preventDefault()
     setSelectedIndex(index)
     setOpen(false)
     onChange?.(options[index])
@@ -64,22 +61,15 @@ export default function SplitMenuButton({
     setOpen(false)
   }
 
-  const maxCharLen = Math.max(...options.map((option) => option.length)) + 2
-
   return (
     <>
-      <ButtonGroup variant="contained" ref={anchorRef} aria-label="Button group with a nested menu">
+      <ButtonGroup variant="contained" ref={anchorRef} aria-label="Button group with a nested menu" fullWidth>
         <Tooltip title={tooltip} placement="top">
-          <span>
-            <Button
-              onClick={handleClick}
-              type="submit"
-              disabled={disabled}
-              sx={{ minWidth: `${maxCharLen}ch !important` }}
-            >
+          <Box flex={1}>
+            <Button onClick={handleClick} type="submit" disabled={disabled}>
               {loading ? <CircularProgress size={20} /> : options[selectedIndex]}
             </Button>
-          </span>
+          </Box>
         </Tooltip>
 
         {options.length > 1 && (
@@ -89,47 +79,39 @@ export default function SplitMenuButton({
             aria-label="select action"
             aria-haspopup="menu"
             onClick={handleToggle}
-            sx={{ minWidth: '0 !important', px: 1.5 }}
+            sx={{ minWidth: '0 !important', maxWidth: 48, px: 1.5 }}
           >
             <ArrowDropDownIcon />
           </Button>
         )}
       </ButtonGroup>
 
-      <Popper
-        sx={{ zIndex: 100 }}
+      <Popover
         open={open}
         anchorEl={anchorRef.current}
-        role={undefined}
-        transition
-        placement="bottom-end"
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'right',
+        }}
+        transformOrigin={{ horizontal: 'right', vertical: -2 }}
+        slotProps={{
+          root: { slotProps: { backdrop: { sx: { backgroundColor: 'transparent' } } } },
+        }}
       >
-        {({ TransitionProps }) => (
-          <Grow
-            {...TransitionProps}
-            style={{
-              transformOrigin: 'center right',
-            }}
-          >
-            <Paper elevation={1}>
-              <ClickAwayListener onClickAway={handleClose}>
-                <MenuList autoFocusItem>
-                  {options.map((option, index) => (
-                    <MenuItem
-                      key={option}
-                      selected={index === selectedIndex}
-                      disabled={disabledIndex === index}
-                      onClick={(event) => handleMenuItemClick(event, index)}
-                    >
-                      {option}
-                    </MenuItem>
-                  ))}
-                </MenuList>
-              </ClickAwayListener>
-            </Paper>
-          </Grow>
-        )}
-      </Popper>
+        <MenuList autoFocusItem>
+          {options.map((option, index) => (
+            <MenuItem
+              key={option}
+              selected={index === selectedIndex}
+              disabled={disabledIndex === index}
+              onClick={(event) => handleMenuItemClick(event, index)}
+            >
+              {option}
+            </MenuItem>
+          ))}
+        </MenuList>
+      </Popover>
     </>
   )
 }
