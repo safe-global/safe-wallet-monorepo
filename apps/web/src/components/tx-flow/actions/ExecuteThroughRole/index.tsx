fab6423e467ca946d4313f9e66e193bc86863ee7
Tim Schwarz <4171783+tmjssz@users.noreply.github.com>
2025-04-11 17:18:26 +0200

    feat(tx-flow): show submit action component also on receipt step
    refactor submit handling to use onSubmitSuccess callback

--- apps/web/src/components/tx-flow/actions/ExecuteThroughRole/index.tsx
+++ apps/web/src/components/tx-flow/actions/ExecuteThroughRole/index.tsx
@@ -5,16 +5,16 @@ import ExecuteThroughRoleForm from './ExecuteThroughRoleForm'
 import useIsCounterfactualSafe from '@/features/counterfactual/hooks/useIsCounterfactualSafe'
 import { type SlotComponentProps, SlotName, withSlot } from '../../slots'
 
-const ExecuteThroughRole = ({ onSubmit }: SlotComponentProps<SlotName.Submit>) => {
+const ExecuteThroughRole = ({ onSubmitSuccess }: SlotComponentProps<SlotName.Submit>) => {
   const { safeTx } = useContext(SafeTxContext)
   const { trackTxEvent, role, isSubmittable } = useContext(TxFlowContext)
 
   const handleSubmit = useCallback(
     async (txId: string, isExecuted = false) => {
-      onSubmit({ txId, isExecuted })
+      onSubmitSuccess?.({ txId, isExecuted })
       trackTxEvent(txId, isExecuted, true)
     },
-    [onSubmit, trackTxEvent],
+    [onSubmitSuccess, trackTxEvent],
   )
 
   return <ExecuteThroughRoleForm safeTx={safeTx} disableSubmit={!isSubmittable} role={role!} onSubmit={handleSubmit} />
