9abadbb82f18337e3a9b31d48ad6bda8ec4930e4
pinkBoss <iotas.echoes-6o@icloud.com>
2025-04-22 09:20:50 +0200

    Update e2e.js

--- apps/web/cypress/support/e2e.js
+++ apps/web/cypress/support/e2e.js
@@ -10,7 +10,7 @@
 // 'supportFile' configuration option.
 //
 // You can read more here:
-// https://on.cypress.io/configuration
+// https://docs.cypress.io/app/references/configuration.html
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
