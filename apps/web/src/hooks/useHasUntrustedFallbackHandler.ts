10678ce93a75286d103adc35165c1bb2b2a9d164
Tim Schwarz <4171783+tmjssz@users.noreply.github.com>
2025-04-25 18:59:21 +0200

    feat: untrusted fallback handler warnings for multiSend transactions

---
+++ apps/web/src/hooks/useHasUntrustedFallbackHandler.ts
@@ -0,0 +1,42 @@
+import { useMemo } from 'react'
+import useSafeInfo from '@/hooks/useSafeInfo'
+import { useCompatibilityFallbackHandlerDeployments } from '@/hooks/useCompatibilityFallbackHandlerDeployments'
+import { sameAddress } from '@safe-global/utils/utils/addresses'
+import { useTWAPFallbackHandlerAddress } from '@/features/swap/hooks/useIsTWAPFallbackHandler'
+
+/**
+ * Hook to check if the Safe's fallback handler (or optionally provided addresses) contain a non-official one.
+ * @param fallbackHandler Optional fallback handler address(es) (if not provided, it will be taken from the Safe info)
+ * @returns Boolean indicating if an untrusted fallback handler is set or if the provided address(es) contain an untrusted one
+ */
+export const useHasUntrustedFallbackHandler = (fallbackHandler?: string | string[]) => {
+  const fallbackHandlerDeployments = useCompatibilityFallbackHandlerDeployments()
+  const { safe } = useSafeInfo()
+  const twapFallbackHandler = useTWAPFallbackHandlerAddress()
+
+  const fallbackHandlerAddresses = useMemo(
+    () => (Array.isArray(fallbackHandler) ? fallbackHandler : [fallbackHandler || safe.fallbackHandler?.value]),
+    [fallbackHandler, safe.fallbackHandler?.value],
+  )
+
+  const officialFallbackHandlerAddresses = useMemo(() => {
+    const addresses = !!twapFallbackHandler ? [twapFallbackHandler] : []
+    const officialAddresses = fallbackHandlerDeployments?.networkAddresses[safe.chainId]
+
+    if (!officialAddresses) {
+      return addresses
+    }
+
+    return [...addresses, ...(Array.isArray(officialAddresses) ? officialAddresses : [officialAddresses])]
+  }, [fallbackHandlerDeployments, safe.chainId, twapFallbackHandler])
+
+  return useMemo(
+    () =>
+      fallbackHandlerAddresses.some(
+        (fallbackHandlerAddress) =>
+          !!fallbackHandlerAddress &&
+          !officialFallbackHandlerAddresses.some((address) => sameAddress(address, fallbackHandlerAddress)),
+      ),
+    [fallbackHandlerAddresses, officialFallbackHandlerAddresses],
+  )
+}
