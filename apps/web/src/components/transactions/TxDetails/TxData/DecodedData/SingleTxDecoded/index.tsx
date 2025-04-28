7d139edd5a810f9e974a1d0237392a945a1965b6
schmanu <manu@safe.global>
2025-04-28 12:45:33 +0200

    feat(web): show decimal token amounts in multiSend summary

--- apps/web/src/components/transactions/TxDetails/TxData/DecodedData/SingleTxDecoded/index.tsx
+++ apps/web/src/components/transactions/TxDetails/TxData/DecodedData/SingleTxDecoded/index.tsx
@@ -10,6 +10,15 @@ import DecodedData from '@/components/transactions/TxDetails/TxData/DecodedData'
 import { sameAddress } from '@safe-global/utils/utils/addresses'
 import { getSafeToL2MigrationDeployment } from '@safe-global/safe-deployments'
 import { useCurrentChain } from '@/hooks/useChains'
+import {
+  type Erc20Token,
+  type NativeToken,
+  type TransactionDetails,
+} from '@safe-global/store/gateway/AUTO_GENERATED/transactions'
+import { ERC20__factory } from '@safe-global/utils/types/contracts'
+import { InlineTransferTxInfo } from '../../Transfer'
+import { useMemo } from 'react'
+import { ZERO_ADDRESS } from '@safe-global/protocol-kit/dist/src/utils/constants'
 
 type SingleTxDecodedProps = {
   tx: InternalTransaction
@@ -19,6 +28,7 @@ type SingleTxDecodedProps = {
   expanded?: boolean
   onChange?: AccordionProps['onChange']
 }
+const ERC20_INTERFACE = ERC20__factory.createInterface()
 
 export const SingleTxDecoded = ({ tx, txData, actionTitle, variant, expanded, onChange }: SingleTxDecodedProps) => {
   const chain = useCurrentChain()
@@ -30,6 +40,54 @@ export const SingleTxDecoded = ({ tx, txData, actionTitle, variant, expanded, on
 
   const safeToL2MigrationDeployment = getSafeToL2MigrationDeployment()
   const safeToL2MigrationAddress = chain && safeToL2MigrationDeployment?.networkAddresses[chain.chainId]
+  const tokenInfoIndex = (txData as TransactionDetails['txData'])?.tokenInfoIndex
+
+  const txDataHex = tx.data ?? txData.hexData
+
+  const isTransfer = Boolean(txDataHex?.startsWith(ERC20_INTERFACE.getFunction('transfer').selector))
+
+  const transferInfos:
+    | {
+        recipient: string
+        transferValue: string
+        tokenInfo: Erc20Token | NativeToken
+      }
+    | undefined = useMemo(() => {
+    if ((!isTransfer && !isNativeTransfer) || !txDataHex || !chain) {
+      return
+    }
+    try {
+      if (isTransfer) {
+        const [recipient, transferValue] = ERC20_INTERFACE.decodeFunctionData('transfer', txDataHex)
+        const tokenInfo = isTransfer ? tokenInfoIndex?.[tx.to] : undefined
+
+        if (tokenInfo?.type !== 'ERC20') {
+          return
+        }
+
+        return { recipient, transferValue, tokenInfo }
+      }
+
+      if (!tx.value || tx.value === '0') {
+        return
+      }
+
+      return {
+        recipient: tx.to,
+        transferValue: tx.value,
+        tokenInfo: {
+          type: 'NATIVE_TOKEN',
+          address: ZERO_ADDRESS,
+          decimals: chain.nativeCurrency.decimals,
+          symbol: chain.nativeCurrency.symbol,
+          logoUri: chain.nativeCurrency.logoUri,
+          name: chain.nativeCurrency.name,
+        },
+      }
+    } catch (error) {
+      return
+    }
+  }, [isTransfer, isNativeTransfer, txDataHex, chain, tx.value, tx.to, tokenInfoIndex])
 
   const singleTxData = {
     to: { value: tx.to },
@@ -47,10 +105,18 @@ export const SingleTxDecoded = ({ tx, txData, actionTitle, variant, expanded, on
         <div className={css.summary}>
           <CodeIcon color="border" fontSize="small" />
           <Typography>{actionTitle}</Typography>
-          <Typography ml="8px">
-            {name ? name + ': ' : ''}
-            <b>{method}</b>
-          </Typography>
+          {transferInfos ? (
+            <InlineTransferTxInfo
+              value={transferInfos.transferValue}
+              tokenInfo={transferInfos.tokenInfo}
+              recipient={transferInfos.recipient}
+            />
+          ) : (
+            <Typography ml="8px">
+              {name ? name + ': ' : ''}
+              <b>{method}</b>
+            </Typography>
+          )}
         </div>
       </AccordionSummary>
 
