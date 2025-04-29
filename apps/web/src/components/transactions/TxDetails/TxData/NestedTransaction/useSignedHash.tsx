ffd2a320ef5ccbd2ec1f26d7aff880a0f878ed6f
Tim Schwarz <4171783+tmjssz@users.noreply.github.com>
2025-04-29 23:51:22 +0200

    fix(NestedTransaction): ensure nested transaction is approveHash call before calculating signedHash
    Render nested transaction link only if signedHash is defined

--- apps/web/src/components/transactions/TxDetails/TxData/NestedTransaction/useSignedHash.tsx
+++ apps/web/src/components/transactions/TxDetails/TxData/NestedTransaction/useSignedHash.tsx
@@ -1,11 +1,16 @@
 import { Safe__factory } from '@safe-global/utils/types/contracts'
 import { type TransactionData } from '@safe-global/safe-gateway-typescript-sdk'
 import { useMemo } from 'react'
+import { isOnChainConfirmationTxData } from '@/utils/transaction-guards'
 
 const safeInterface = Safe__factory.createInterface()
 
 export const useSignedHash = (txData?: TransactionData) => {
   const signedHash = useMemo(() => {
+    if (!isOnChainConfirmationTxData(txData)) {
+      return
+    }
+
     const params = txData?.hexData ? safeInterface.decodeFunctionData('approveHash', txData?.hexData) : undefined
     if (!params || params.length !== 1 || typeof params[0] !== 'string') {
       return
