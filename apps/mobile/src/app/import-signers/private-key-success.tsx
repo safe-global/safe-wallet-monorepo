35c9c5cdb67f3cf6cdd40886b485dbdf75ed81b8
Daniel Dimitrov <daniel.d@safe.global>
2025-04-30 11:46:04 +0200

    fix(mobile): private-key-success not respecting the safe area

--- apps/mobile/src/app/import-signers/private-key-success.tsx
+++ apps/mobile/src/app/import-signers/private-key-success.tsx
@@ -3,13 +3,14 @@ import { LinearGradient } from 'expo-linear-gradient'
 import { ImportSuccess } from '@/src/features/ImportPrivateKey/components/ImportSuccess'
 import React from 'react'
 import { useTheme, View } from 'tamagui'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
 
 export default function ImportPrivateKeySuccess() {
   const theme = useTheme()
   const colors: [string, string] = [theme.success.get(), 'transparent']
-
+  const { bottom } = useSafeAreaInsets()
   return (
-    <View style={{ flex: 1 }}>
+    <View style={{ flex: 1 }} paddingBottom={bottom}>
       <LinearGradient colors={colors} style={styles.background} />
 
       <ImportSuccess />
