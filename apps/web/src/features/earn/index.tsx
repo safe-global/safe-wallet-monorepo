e1e8c86d591a254220b7e8954c77f31c40e62e8e
Usame Algan <usame@safe.global>
2025-05-02 15:50:55 +0200

    feat: Add Earn info view

--- apps/web/src/features/earn/index.tsx
+++ apps/web/src/features/earn/index.tsx
@@ -1,18 +1,14 @@
-import { useRouter } from 'next/router'
 import { Stack } from '@mui/material'
 import Disclaimer from '@/components/common/Disclaimer'
 import WidgetDisclaimer from '@/components/common/WidgetDisclaimer'
 import BlockedAddress from '@/components/common/BlockedAddress'
 import useBlockedAddress from '@/hooks/useBlockedAddress'
-import EarnWidget from 'src/features/earn/components/EarnWidget'
 import useConsent from '@/hooks/useConsent'
 import { EARN_CONSENT_STORAGE_KEY } from '@/features/earn/constants'
+import EarnView from '@/features/earn/components/EarnView'
 
 const EarnPage = () => {
   const { isConsentAccepted, onAccept } = useConsent(EARN_CONSENT_STORAGE_KEY)
-  const router = useRouter()
-  const { asset } = router.query
-
   const blockedAddress = useBlockedAddress()
 
   if (blockedAddress) {
@@ -35,7 +31,7 @@ const EarnPage = () => {
   return (
     <>
       {isConsentAccepted ? (
-        <EarnWidget asset={String(asset)} />
+        <EarnView />
       ) : (
         <Stack
           direction="column"
