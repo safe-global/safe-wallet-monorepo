a6a329923f768700445c55a91e237750a5a1258e
Clóvis Neto <clovisdasilvaneto@gmail.com>
2025-05-07 10:09:34 -0300

    chore: move usage implementation to be inside the SafeImport container

--- apps/mobile/src/app/(import-accounts)/form.tsx
+++ apps/mobile/src/app/(import-accounts)/form.tsx
@@ -1,27 +1,11 @@
 import React from 'react'
 import { ImportAccountFormContainer } from '@/src/features/ImportReadOnly'
 import { View } from 'tamagui'
-import { useForm, FormProvider } from 'react-hook-form'
-import { zodResolver } from '@hookform/resolvers/zod'
-import { formSchema } from '@/src/features/ImportReadOnly/schema'
-import { useLocalSearchParams } from 'expo-router'
 
 function ImportAccountFormScreen() {
-  const params = useLocalSearchParams<{ safeAddress: string }>()
-  const methods = useForm({
-    resolver: zodResolver(formSchema),
-    mode: 'onChange',
-    defaultValues: {
-      name: '',
-      safeAddress: params.safeAddress || '',
-    },
-  })
-
   return (
     <View style={{ flex: 1 }}>
-      <FormProvider {...methods}>
-        <ImportAccountFormContainer />
-      </FormProvider>
+      <ImportAccountFormContainer />
     </View>
   )
 }
--- apps/mobile/src/features/ImportReadOnly/ImportAccountForm.container.tsx
+++ apps/mobile/src/features/ImportReadOnly/ImportAccountForm.container.tsx
@@ -1,30 +1,43 @@
-import { useRouter } from 'expo-router'
+import { useLocalSearchParams, useRouter } from 'expo-router'
 import React, { useCallback } from 'react'
 import { parsePrefixedAddress } from '@safe-global/utils/utils/addresses'
 import { ImportAccountFormView } from '@/src/features/ImportReadOnly/components/ImportAccountFormView'
-import { useFormContext } from 'react-hook-form'
+import { FormProvider, useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { formSchema } from './schema'
+import { FormValues } from './types'
 
 export const ImportAccountFormContainer = () => {
   const router = useRouter()
-  const { getFieldState, getValues } = useFormContext()
+  const params = useLocalSearchParams<{ safeAddress: string }>()
+  const methods = useForm<FormValues>({
+    resolver: zodResolver(formSchema),
+    mode: 'onChange',
+    defaultValues: {
+      name: '',
+      safeAddress: params.safeAddress || '',
+    },
+  })
 
-  const addressState = getFieldState('safeAddress')
+  const addressState = methods.getFieldState('safeAddress')
 
   const handleContinue = useCallback(() => {
-    const inputAddress = getValues('safeAddress')
-    const chainId = getValues('chainId')
-    const safeName = getValues('name')
+    const inputAddress = methods.getValues('safeAddress')
+    const chainId = methods.getValues('importedSafeResult.data.0.chainId')
+    const safeName = methods.getValues('name')
     const { address } = parsePrefixedAddress(inputAddress)
 
     router.push(
       `/(import-accounts)/signers?safeAddress=${address}&chainId=${chainId}&import_safe=true&safeName=${safeName}`,
     )
-  }, [router, getValues])
+  }, [router, methods.getValues])
 
   return (
-    <ImportAccountFormView
-      isEnteredAddressValid={addressState.isTouched && !addressState.invalid}
-      onContinue={handleContinue}
-    />
+    <FormProvider {...methods}>
+      <ImportAccountFormView
+        isEnteredAddressValid={addressState.isTouched && !addressState.invalid}
+        onContinue={handleContinue}
+      />
+    </FormProvider>
   )
 }
