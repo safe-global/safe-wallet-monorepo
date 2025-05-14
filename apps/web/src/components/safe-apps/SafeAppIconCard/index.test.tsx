5d4e9f65d9ff62ade5b2dc50cc4b6d7c353bb251
Aaron Cook <aaron@safe.global>
2025-05-14 17:38:12 +0200

    fix: revert svg rendering and use alternating icons

--- apps/web/src/components/safe-apps/SafeAppIconCard/index.test.tsx
+++ apps/web/src/components/safe-apps/SafeAppIconCard/index.test.tsx
@@ -1,5 +1,5 @@
-import { render, waitFor } from '@/tests/test-utils'
-import SafeAppIconCard, { _svgAttributesToProps } from '.'
+import { render } from '@/tests/test-utils'
+import SafeAppIconCard from '.'
 
 describe('SafeAppIconCard', () => {
   it('should render an icon', () => {
@@ -14,62 +14,5 @@ describe('SafeAppIconCard', () => {
     expect(img).toHaveAttribute('height', '100')
     expect(img).toHaveAttribute('width', '100')
     expect(img).not.toHaveAttribute('crossorigin')
-  })
-
-  it('should render an svg icon', async () => {
-    const className = 'test-class'
-    const viewBox = '0 0 100 100'
-
-    global.fetch = jest.fn(() =>
-      Promise.resolve({
-        ok: true,
-        status: 200,
-        text: () =>
-          Promise.resolve(
-            `<svg class="${className}" viewBox="${viewBox}" xmlns="http://www.w3.org/2000/svg"><circle cx="10" cy="10" r="10" /></svg>`,
-          ),
-      }),
-    ) as jest.Mock
-
-    const src = 'https://fake.url.com/image.svg'
-    const { container } = render(
-      <SafeAppIconCard src={src} fallback="/fallback.png" height={100} width={100} alt="test" />,
-    )
-
-    await waitFor(() => {
-      const svg = container.querySelector('svg')
-      expect(svg).toBeInTheDocument()
-      expect(svg).toHaveAttribute('class', className)
-      expect(svg).toHaveAttribute('viewBox', viewBox)
-      expect(svg).toHaveAttribute('height', '100')
-      expect(svg).toHaveAttribute('width', '100')
-    })
-  })
-})
-
-describe('svgAttributesToProps', () => {
-  it('should convert svg attributes to props', () => {
-    const el = document.createElement('svg')
-    el.setAttribute('class', 'test-class')
-    el.setAttribute('for', 'test-for')
-    el.setAttribute('viewBox', '0 0 100 100')
-    el.setAttribute('tabindex', '1')
-    el.setAttribute('readonly', 'true')
-    el.setAttribute('maxlength', '10')
-    el.setAttribute('contenteditable', 'true')
-    el.setAttribute('crossorigin', 'use-credentials')
-
-    const result = _svgAttributesToProps(el.attributes)
-
-    expect(result).toEqual({
-      className: 'test-class',
-      htmlFor: 'test-for',
-      viewBox: '0 0 100 100',
-      tabIndex: '1',
-      readOnly: 'true',
-      maxLength: '10',
-      contentEditable: 'true',
-      crossOrigin: 'use-credentials',
-    })
   })
 })
--- apps/web/src/components/safe-apps/SafeAppIconCard/index.tsx
+++ apps/web/src/components/safe-apps/SafeAppIconCard/index.tsx
@@ -1,6 +1,4 @@
 import ImageFallback from '@/components/common/ImageFallback'
-import useAsync from '@safe-global/utils/hooks/useAsync'
-import type { SVGAttributes } from 'react'
 
 const APP_LOGO_FALLBACK_IMAGE = `/images/apps/app-placeholder.svg`
 
@@ -17,50 +15,7 @@ const SafeAppIconCard = ({
   height?: number
   fallback?: string
 }) => {
-  const [svg] = useAsync<SVGSVGElement | null>(async () => {
-    if (!src || !src.endsWith('.svg')) {
-      return null
-    }
-
-    const res = await fetch(src)
-    const svg = await res.text()
-
-    const parser = new DOMParser()
-    const doc = parser.parseFromString(svg, 'image/svg+xml')
-    return doc.querySelector('svg')
-  }, [src])
-
-  if (svg) {
-    return (
-      <svg
-        {..._svgAttributesToProps(svg.attributes)}
-        width={width}
-        height={height}
-        dangerouslySetInnerHTML={{ __html: svg.innerHTML }}
-      />
-    )
-  }
-
   return <ImageFallback src={src} alt={alt} width={width} height={height} fallbackSrc={fallback} />
-}
-
-export function _svgAttributesToProps(attributes: NamedNodeMap): Partial<SVGAttributes<SVGSVGElement>> {
-  const exceptions: Record<string, string> = {
-    class: 'className',
-    for: 'htmlFor',
-    viewbox: 'viewBox',
-    tabindex: 'tabIndex',
-    readonly: 'readOnly',
-    maxlength: 'maxLength',
-    contenteditable: 'contentEditable',
-    crossorigin: 'crossOrigin',
-  }
-
-  return Array.from(attributes).reduce<Record<string, string>>((acc, attr) => {
-    const name = exceptions[attr.name] || attr.name
-    acc[name] = attr.value
-    return acc
-  }, {})
 }
 
 export default SafeAppIconCard
