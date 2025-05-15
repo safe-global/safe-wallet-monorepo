6410852570355ca15fc8d2113249a8d749921fc6
Usame Algan <usame@safe.global>
2025-05-15 17:47:41 +0200

    chore: Add prod widget url

--- apps/web/src/features/earn/constants.ts
+++ apps/web/src/features/earn/constants.ts
@@ -1,5 +1,6 @@
 export const EARN_TITLE = 'Earn'
 export const WIDGET_TESTNET_URL = 'https://safe.widget.testnet.kiln.fi/overview' // TODO: Change to prod widget url
+export const WIDGET_PRODUCTION_URL = 'https://safe-defi.widget.kiln.fi/overview'
 export const EARN_CONSENT_STORAGE_KEY = 'lendDisclaimerAcceptedV1'
 
 export const widgetAppData = {
