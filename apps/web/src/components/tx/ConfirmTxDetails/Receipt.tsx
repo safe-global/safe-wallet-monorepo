1805fcd9507f84e33b7cb0bd02fbc1b7c2c4674b
katspaugh <katspaugh@gmail.com>
2025-05-16 10:28:26 +0200

    Feat(Tx flow): restore JSON view + copy button

--- apps/web/src/components/tx/ConfirmTxDetails/Receipt.tsx
+++ apps/web/src/components/tx/ConfirmTxDetails/Receipt.tsx
@@ -14,6 +14,7 @@ import {
 import TxDetailsRow from './TxDetailsRow'
 import NameChip from './NameChip'
 import { isMultisigDetailedExecutionInfo } from '@/utils/transaction-guards'
+import { JsonView } from './JsonView'
 
 type TxDetailsProps = {
   safeTxData: SafeTransaction['data']
@@ -182,6 +183,14 @@ export const Receipt = ({ safeTxData, txData, txDetails, grid, withSignatures = 
                   </TxDetailsRow>
                 )}
               </Stack>
+            </ContentWrapper>
+          ),
+        },
+        {
+          title: 'JSON',
+          content: (
+            <ContentWrapper>
+              <JsonView data={safeTxData} />
             </ContentWrapper>
           ),
         },
