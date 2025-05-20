dc702403c2b8eb01b2639d28af3006099f4d3fce
Usame Algan <usame@safe.global>
2025-05-20 09:38:32 +0200

    fix: Add prod widget url and limit chain ids

--- apps/web/src/features/earn/hooks/useGetWidgetUrl.ts
+++ apps/web/src/features/earn/hooks/useGetWidgetUrl.ts
@@ -1,7 +1,18 @@
 import { useDarkMode } from '@/hooks/useDarkMode'
-import { WIDGET_TESTNET_URL } from '@/features/earn/constants'
+import { WIDGET_TESTNET_URL, WIDGET_PRODUCTION_URL } from '@/features/earn/constants'
+import useChains from '@/hooks/useChains'
+import { useMemo } from 'react'
+import useChainId from '@/hooks/useChainId'
 
 const useGetWidgetUrl = (asset?: string) => {
+  let url = WIDGET_PRODUCTION_URL
+  const currentChainId = useChainId()
+  const { configs } = useChains()
+  const testChains = useMemo(() => configs.filter((chain) => chain.isTestnet), [configs])
+  if (testChains.some((chain) => chain.chainId === currentChainId)) {
+    url = WIDGET_TESTNET_URL
+  }
+
   const params = new URLSearchParams()
   const isDarkMode = useDarkMode()
 
@@ -9,7 +20,7 @@ const useGetWidgetUrl = (asset?: string) => {
   if (asset) params.append('asset', asset)
 
   // TODO: Change this to the prod URL
-  return WIDGET_TESTNET_URL + '?' + params.toString()
+  return url + '?' + params.toString()
 }
 
 export default useGetWidgetUrl
