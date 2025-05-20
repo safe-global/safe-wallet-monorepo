1fd46911178fa85c6087efcd12a15c8afbf417cd
Usame Algan <usame@safe.global>
2025-05-20 09:38:32 +0200

    fix: Add prod widget url and limit chain ids

--- apps/web/src/features/lend/constants.ts
+++ apps/web/src/features/lend/constants.ts
@@ -1,10 +1,10 @@
 export const EARN_TITLE = 'Earn'
-export const WIDGET_TESTNET_URL = 'https://safe.widget.testnet.kiln.fi/overview' // TODO: Change to prod widget url
+export const WIDGET_TESTNET_URL = 'https://safe.widget.testnet.kiln.fi/overview'
 export const WIDGET_PRODUCTION_URL = 'https://safe-defi.widget.kiln.fi/overview'
 export const EARN_CONSENT_STORAGE_KEY = 'lendDisclaimerAcceptedV1'
 
 export const widgetAppData = {
   url: WIDGET_TESTNET_URL,
   name: EARN_TITLE,
-  chainIds: ['17000', '11155111', '1', '42161', '137', '56', '8453', '10'],
+  chainIds: ['1', '8453'],
 }
