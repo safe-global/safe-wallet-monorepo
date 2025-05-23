e28bc89a2ee75debc2ffbb7343596866cd24b4df
Daniel Dimitrov <daniel.d@safe.global>
2025-05-23 15:06:40 +0200

    refactor(mobile): getAccountType
    introduced a reusable helper to derive notification account type based on Safe owners and signers

--- apps/mobile/src/hooks/useNotificationGTWPermissions.ts
+++ apps/mobile/src/hooks/useNotificationGTWPermissions.ts
@@ -2,27 +2,18 @@ import { useCallback } from 'react'
 import { useAppSelector } from '../store/hooks'
 import { RootState } from '../store'
 import { selectSigners } from '../store/signersSlice'
-import { NOTIFICATION_ACCOUNT_TYPE } from '../store/constants'
 import { selectSafeInfo } from '../store/safesSlice'
+import { getAccountType } from '@/src/utils/notifications/accountType'
 
 export function useNotificationGTWPermissions(safeAddress: string) {
   const appSigners = useAppSelector(selectSigners)
 
   const activeSafeInfo = useAppSelector((state: RootState) => selectSafeInfo(state, safeAddress as `0x${string}`))
 
-  if (!activeSafeInfo) {
-    return {
-      getAccountType: () => ({ ownerFound: null, accountType: NOTIFICATION_ACCOUNT_TYPE.REGULAR }),
-    }
-  }
+  const getAccountTypeFn = useCallback(
+    () => getAccountType(activeSafeInfo?.SafeInfo, appSigners),
+    [activeSafeInfo?.SafeInfo, appSigners],
+  )
 
-  const owners = activeSafeInfo.SafeInfo.owners
-
-  const getAccountType = useCallback(() => {
-    const ownerFound = activeSafeInfo.SafeInfo.owners.find((owner) => appSigners[owner.value]) ?? null
-    const accountType = ownerFound ? NOTIFICATION_ACCOUNT_TYPE.OWNER : NOTIFICATION_ACCOUNT_TYPE.REGULAR
-    return { ownerFound, accountType }
-  }, [owners, appSigners])
-
-  return { getAccountType }
+  return { getAccountType: getAccountTypeFn }
 }
