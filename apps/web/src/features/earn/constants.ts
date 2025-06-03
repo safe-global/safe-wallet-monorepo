aa9f70e08b97c97d0c1649c404e26b42fb020d29
Usame Algan <usame@safe.global>
2025-06-03 08:45:49 +0200

    fix: Link to earn tab on kiln widget

--- apps/web/src/features/earn/constants.ts
+++ apps/web/src/features/earn/constants.ts
@@ -1,6 +1,6 @@
 export const EARN_TITLE = 'Earn'
-export const WIDGET_TESTNET_URL = 'https://safe.widget.testnet.kiln.fi/overview'
-export const WIDGET_PRODUCTION_URL = 'https://safe-defi.widget.kiln.fi/overview'
+export const WIDGET_TESTNET_URL = 'https://safe.widget.testnet.kiln.fi/earn'
+export const WIDGET_PRODUCTION_URL = 'https://safe-defi.widget.kiln.fi/earn'
 export const EARN_CONSENT_STORAGE_KEY = 'lendDisclaimerAcceptedV1'
 export const EARN_HELP_ARTICLE = 'https://help.safe.global/en/articles/322149-defi-lending-in-safe-wallet'
 
