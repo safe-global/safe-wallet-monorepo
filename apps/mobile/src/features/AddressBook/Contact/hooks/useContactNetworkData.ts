602f8f467e2117bac0bd177f238bd775b931d06c
Daniel Dimitrov <daniel.d@safe.global>
2025-06-06 17:33:18 +0200

    test(mobile): add tests for useContactNetworkData

--- apps/mobile/src/features/AddressBook/Contact/hooks/useContactNetworkData.ts
+++ apps/mobile/src/features/AddressBook/Contact/hooks/useContactNetworkData.ts
@@ -9,7 +9,7 @@ export const useContactNetworkData = (chainIds: string[]) => {
       return 'All Networks'
     }
     if (chainIds.length === 1) {
-      return selectedChains[0].chainName
+      return selectedChains[0]?.chainName || 'Unknown Network'
     }
     return `${chainIds.length} Networks`
   }
