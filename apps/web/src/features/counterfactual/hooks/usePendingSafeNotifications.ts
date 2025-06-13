07528505920bd856b3aeb202478ce526a408c2a5
dsh <sebastian0794@rambler.com>
2025-06-13 15:33:55 +0200

    invalidate tags + use currentData

--- apps/web/src/features/counterfactual/hooks/usePendingSafeNotifications.ts
+++ apps/web/src/features/counterfactual/hooks/usePendingSafeNotifications.ts
@@ -1,7 +1,5 @@
 import { SafeCreationEvent, safeCreationSubscribe } from '@/features/counterfactual/services/safeCreationEvents'
-import useWallet from '@/hooks/wallets/useWallet'
 import { getBlockExplorerLink } from '@safe-global/utils/utils/chains'
-import { skipToken } from '@reduxjs/toolkit/query'
 import { useEffect } from 'react'
 import { formatError } from '@safe-global/utils/utils/formatters'
 import { showNotification } from '@/store/notificationsSlice'
@@ -9,8 +7,7 @@ import { useAppDispatch } from '@/store'
 import { useCurrentChain } from '@/hooks/useChains'
 import useSafeAddress from '@/hooks/useSafeAddress'
 import { isWalletRejection } from '@/utils/wallets'
-import { useOwnersGetAllSafesByOwnerV2Query } from '@safe-global/store/gateway/AUTO_GENERATED/owners'
-import type { OwnersGetAllSafesByOwnerV2ApiArg } from '@safe-global/store/gateway/AUTO_GENERATED/owners'
+import { cgwApi } from '@safe-global/store/gateway/AUTO_GENERATED/owners'
 
 const SafeCreationNotifications = {
   [SafeCreationEvent.PROCESSING]: 'Validating...',
@@ -30,10 +27,6 @@ const usePendingSafeNotifications = (): void => {
   const dispatch = useAppDispatch()
   const chain = useCurrentChain()
   const safeAddress = useSafeAddress()
-  const { address = '' } = useWallet() || {}
-  const { refetch } = useOwnersGetAllSafesByOwnerV2Query(
-    address === '' ? (skipToken as unknown as OwnersGetAllSafesByOwnerV2ApiArg) : { ownerAddress: address }, //todo: can we do lazy here?
-  )
 
   useEffect(() => {
     if (!chain) return
@@ -53,7 +46,7 @@ const usePendingSafeNotifications = (): void => {
 
         // Fetch all owned safes after the Safe has been deployed
         if (isSuccess) {
-          refetch()
+          dispatch(cgwApi.util.invalidateTags(['owners']))
         }
 
         dispatch(
@@ -72,7 +65,7 @@ const usePendingSafeNotifications = (): void => {
     return () => {
       unsubFns.forEach((unsub) => unsub())
     }
-  }, [dispatch, safeAddress, chain, refetch])
+  }, [dispatch, safeAddress, chain])
 }
 
 export default usePendingSafeNotifications
--- apps/web/src/features/myAccounts/hooks/useAllOwnedSafes.ts
+++ apps/web/src/features/myAccounts/hooks/useAllOwnedSafes.ts
@@ -1,18 +1,15 @@
 import type { AsyncResult } from '@safe-global/utils/hooks/useAsync'
-import type {
-  OwnersGetAllSafesByOwnerV2ApiArg,
-  OwnersGetAllSafesByOwnerV2ApiResponse,
-} from '@safe-global/store/gateway/AUTO_GENERATED/owners'
+import type { OwnersGetAllSafesByOwnerV2ApiResponse } from '@safe-global/store/gateway/AUTO_GENERATED/owners'
 import { useOwnersGetAllSafesByOwnerV2Query } from '@safe-global/store/gateway/AUTO_GENERATED/owners'
 import { asError } from '@safe-global/utils/services/exceptions/utils'
-import { skipToken } from '@reduxjs/toolkit/query'
 
 const useAllOwnedSafes = (address: string): AsyncResult<OwnersGetAllSafesByOwnerV2ApiResponse> => {
-  const { data, error, isLoading } = useOwnersGetAllSafesByOwnerV2Query(
-    address === '' ? (skipToken as unknown as OwnersGetAllSafesByOwnerV2ApiArg) : { ownerAddress: address },
+  const { currentData, error, isLoading } = useOwnersGetAllSafesByOwnerV2Query(
+    { ownerAddress: address },
+    { skip: address === '' },
   )
 
-  return [address ? data : undefined, asError(error), isLoading]
+  return [address ? currentData : undefined, asError(error), isLoading]
 }
 
 export default useAllOwnedSafes
