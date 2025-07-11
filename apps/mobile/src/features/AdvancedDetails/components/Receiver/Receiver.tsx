7ce2ed373b8c1d41b3521d0d45103fed7be7f0fe
Tim Schwarz <4171783+tmjssz@users.noreply.github.com>
2025-07-11 11:09:44 +0200

    feat(mobile): enhance Receiver component to utilize address book for contact names

--- apps/mobile/src/features/AdvancedDetails/components/Receiver/Receiver.tsx
+++ apps/mobile/src/features/AdvancedDetails/components/Receiver/Receiver.tsx
@@ -3,13 +3,19 @@ import { View, Text } from 'tamagui'
 import { TransactionDetails } from '@safe-global/store/gateway/AUTO_GENERATED/transactions'
 import { Logo } from '@/src/components/Logo'
 import { SafeFontIcon } from '@/src/components/SafeFontIcon'
+import { useAppSelector } from '@/src/store/hooks'
+import { selectContactByAddress } from '@/src/store/addressBookSlice'
 
 interface ReceiverProps {
   txData: TransactionDetails['txData']
 }
 
 export function Receiver({ txData }: ReceiverProps) {
-  const content = txData?.to.name
+  const { to: { value = '', name, logoUri } = {} } = txData || {}
+
+  const contact = useAppSelector(selectContactByAddress(value))
+
+  const content = contact?.name || name
 
   if (!content) {
     return null
@@ -26,7 +32,7 @@ export function Receiver({ txData }: ReceiverProps) {
       gap="$2"
       alignSelf="flex-start"
     >
-      <Logo logoUri={txData?.to.logoUri} size="$4" />
+      <Logo logoUri={logoUri} size="$4" />
       <Text fontWeight={600}>{content}</Text>
       <SafeFontIcon name="check-oulined" color="$success" size={16} style={{ marginLeft: 'auto' }} />
     </View>
