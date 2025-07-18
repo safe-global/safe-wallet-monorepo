abef13bd3b9a7db38b16c2ef1b6636ba1aa72520
Usame Algan <usame@safe.global>
2025-07-18 12:16:21 +0200

    Fix(Mobile): Use SafeTab across app

--- apps/mobile/src/app/transaction-parameters/(tabs)/_layout.tsx
+++ apps/mobile/src/app/transaction-parameters/(tabs)/_layout.tsx
@@ -1,41 +1,19 @@
 import React from 'react'
+import TransactionParameters from '@/src/app/transaction-parameters/(tabs)/parameters'
+import TransactionData from '@/src/app/transaction-parameters/(tabs)/index'
+import { SafeTab } from '@/src/components/SafeTab'
 
-import {
-  MaterialTopTabNavigationEventMap,
-  MaterialTopTabNavigationOptions,
-  createMaterialTopTabNavigator,
-} from '@react-navigation/material-top-tabs'
-import { useLocalSearchParams, withLayoutContext } from 'expo-router'
-import { ParamListBase, TabNavigationState } from '@react-navigation/native'
-import { useTheme } from 'tamagui'
-import { getMaterialTopTabBarScreenOptions } from '@/src/theme/helpers/tabBarStyles'
-
-const { Navigator } = createMaterialTopTabNavigator()
-
-export const MaterialTopTabs = withLayoutContext<
-  MaterialTopTabNavigationOptions,
-  typeof Navigator,
-  TabNavigationState<ParamListBase>,
-  MaterialTopTabNavigationEventMap
->(Navigator)
+const tabItems = [
+  {
+    label: 'Data',
+    Component: TransactionData,
+  },
+  {
+    label: `Parameters`,
+    Component: TransactionParameters,
+  },
+]
 
 export default function TransactionsLayout() {
-  const theme = useTheme()
-  const { txId } = useLocalSearchParams<{ txId: string }>()
-
-  return (
-    <MaterialTopTabs
-      screenOptions={getMaterialTopTabBarScreenOptions({
-        theme,
-        tabBarItemWidth: 124,
-        tabBarIndicatorWidth: 94,
-        tabBarLabelFontSize: 16,
-        tabBarLabelFontWeight: '700',
-        tabBarItemLeft: 0,
-      })}
-    >
-      <MaterialTopTabs.Screen initialParams={{ txId }} name="index" options={{ title: 'Data' }} />
-      <MaterialTopTabs.Screen initialParams={{ txId }} name="parameters" options={{ title: 'Parameters' }} />
-    </MaterialTopTabs>
-  )
+  return <SafeTab items={tabItems} containerStyle={{ marginTop: 16 }} />
 }
--- apps/mobile/src/components/SafeTab/SafeTab.tsx
+++ apps/mobile/src/components/SafeTab/SafeTab.tsx
@@ -3,25 +3,37 @@ import { TabBarProps, Tabs } from 'react-native-collapsible-tab-view'
 import { safeTabItem } from './types'
 import { SafeTabBar } from './SafeTabBar'
 import { Theme } from 'tamagui'
+import { StyleProp, ViewStyle } from 'react-native'
 
 interface SafeTabProps<T> {
   renderHeader?: (props: TabBarProps<string>) => ReactElement
   headerHeight?: number
   items: safeTabItem<T>[]
   containerProps?: T
+  containerStyle?: StyleProp<ViewStyle>
+  onIndexChange?: (index: number) => void
 }
 
-export function SafeTab<T extends object>({ renderHeader, headerHeight, items, containerProps }: SafeTabProps<T>) {
+export function SafeTab<T extends object>({
+  renderHeader,
+  headerHeight,
+  items,
+  containerProps,
+  containerStyle,
+  onIndexChange,
+}: SafeTabProps<T>) {
   const [activeTab, setActiveTab] = useState(items[0].label)
 
   return (
     <Theme name={'tab'}>
       <Tabs.Container
+        containerStyle={containerStyle}
         renderHeader={renderHeader}
         headerContainerStyle={headerContainerStyle}
         headerHeight={headerHeight}
         renderTabBar={(props) => <SafeTabBar activeTab={activeTab} setActiveTab={setActiveTab} {...props} />}
         onTabChange={(event) => setActiveTab(event.tabName)}
+        onIndexChange={onIndexChange}
         initialTabName={items[0].label}
       >
         {items.map(({ label, Component }, index) => (
