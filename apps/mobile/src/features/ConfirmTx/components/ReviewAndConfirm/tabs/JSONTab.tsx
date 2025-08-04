c993e7a6f89b85a93c995e113ff4e96b89f909e3
Usame Algan <usame@safe.global>
2025-08-04 15:11:18 +0200

    Fix(Mobile): Show safe tx data in JSON view when confirming txs

--- apps/mobile/src/features/ConfirmTx/components/ReviewAndConfirm/tabs/JSONTab.tsx
+++ apps/mobile/src/features/ConfirmTx/components/ReviewAndConfirm/tabs/JSONTab.tsx
@@ -1,16 +1,21 @@
-import React from 'react'
 import { Text, View } from 'tamagui'
 import { Tabs } from 'react-native-collapsible-tab-view'
 import { TransactionDetails } from '@safe-global/store/gateway/AUTO_GENERATED/transactions'
 import { Container } from '@/src/components/Container'
 import { CopyButton } from '@/src/components/CopyButton'
+import useSafeTx from '@/src/hooks/useSafeTx'
 
 interface JSONTabProps {
   txDetails: TransactionDetails
 }
 
 export function JSONTab({ txDetails }: JSONTabProps) {
-  const jsonData = JSON.stringify(txDetails, null, 2)
+  const safeTx = useSafeTx(txDetails)
+  const jsonData = safeTx ? JSON.stringify(safeTx.data, null, 2) : undefined
+
+  if (!jsonData) {
+    return null
+  }
 
   return (
     <Tabs.ScrollView contentContainerStyle={{ padding: 16, marginTop: 16 }}>
