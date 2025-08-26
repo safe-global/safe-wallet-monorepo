982965083d2bba3a638027db610c25dd6fe15dca
Dasha Kobzeva <11198975+LucieFaire@users.noreply.github.com>
2025-08-26 16:53:45 +0200

    fix(test): trying spying on rtk-query calls for csv-export (#6251)
    * feat(analytics): add tests
    
    * fix(tests): Fix tests
    
    * fix(test): trying spying on rtk-query calls for csv-export
    
    Signed-off-by: dsh <11198975+LucieFaire@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: dsh <11198975+LucieFaire@users.noreply.github.com>
    Co-authored-by: valle-xyz <valentin.seehausen@gmail.com>

--- apps/web/src/components/transactions/CsvTxExportModal/CsvTxExportModal.test.tsx
+++ apps/web/src/components/transactions/CsvTxExportModal/CsvTxExportModal.test.tsx
@@ -1,13 +1,40 @@
 import React, { act } from 'react'
 import { render, screen, fireEvent, waitFor } from '@/tests/test-utils'
+import { trackEvent } from '@/services/analytics'
+import { TX_LIST_EVENTS } from '@/services/analytics/events/txList'
+import { MixPanelEventParams } from '@/services/analytics/mixpanel-events'
 import CsvTxExportModal from './index'
+import * as csvExportQueries from '@safe-global/store/gateway/AUTO_GENERATED/csv-export'
 
+jest.mock('@/services/analytics', () => ({
+  trackEvent: jest.fn(),
+  MixPanelEventParams: {
+    DATE_RANGE: 'Date Range',
+  },
+}))
+
+const mockLaunchFunction = jest.fn().mockImplementation(() => ({
+  unwrap: jest.fn().mockResolvedValue({ id: 'test-job-id', status: 'SUBMITTED' }),
+}))
+const mockTrackEvent = trackEvent as jest.MockedFunction<typeof trackEvent>
 const onClose = jest.fn()
 const onExport = jest.fn()
 
 describe('CsvTxExportModal', () => {
   const renderComponent = (hasActiveFilter: boolean = false) =>
     render(<CsvTxExportModal onClose={onClose} onExport={onExport} hasActiveFilter={hasActiveFilter} />)
+
+  beforeEach(() => {
+    jest.clearAllMocks()
+
+    jest.spyOn(csvExportQueries, 'useCsvExportLaunchExportV1Mutation').mockReturnValue([
+      mockLaunchFunction,
+      {
+        isLoading: false,
+        reset: jest.fn(),
+      },
+    ])
+  })
 
   it('renders modal with message and disabled export button', () => {
     renderComponent()
@@ -91,5 +118,21 @@ describe('CsvTxExportModal', () => {
     renderComponent(true)
 
     expect(screen.getByText("Transaction history filters won't apply here.")).toBeInTheDocument()
+  })
+
+  it('should track CSV_EXPORT_SUBMITTED event with DATE_RANGE parameter when form is submitted', async () => {
+    renderComponent()
+
+    act(() => fireEvent.mouseDown(screen.getByLabelText('Date range')))
+    act(() => fireEvent.click(screen.getByRole('option', { name: 'Last 30 days' })))
+
+    const exportBtn = screen.getByRole('button', { name: 'Export' })
+    await act(async () => fireEvent.click(exportBtn))
+
+    await waitFor(() => {
+      expect(mockTrackEvent).toHaveBeenCalledWith(TX_LIST_EVENTS.CSV_EXPORT_SUBMITTED, {
+        [MixPanelEventParams.DATE_RANGE]: 'Last 30 days',
+      })
+    })
   })
 })
