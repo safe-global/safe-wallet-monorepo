a39da75506447916c8fff721fe9916185bbc1253
valleXYZ <valentin.seehausen@gmail.com>
2025-08-27 15:00:11 +0200

    feat(positions): hide token and currency select at Positions Balance View

--- apps/web/src/pages/balances/positions.tsx
+++ apps/web/src/pages/balances/positions.tsx
@@ -2,10 +2,6 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 
 import AssetsHeader from '@/components/balances/AssetsHeader'
-import { useState } from 'react'
-import HiddenTokenButton from '@/components/balances/HiddenTokenButton'
-import CurrencySelect from '@/components/balances/CurrencySelect'
-import TokenListSelect from '@/components/balances/TokenListSelect'
 import { BRAND_NAME } from '@/config/constants'
 import DefiPositions from '@/features/positions'
 import { Box } from '@mui/material'
@@ -13,8 +9,6 @@ import TotalAssetValue from '@/components/balances/TotalAssetValue'
 import usePositionsFiatTotal from '@/features/positions/hooks/usePositionsFiatTotal'
 
 const Positions: NextPage = () => {
-  const [showHiddenAssets, setShowHiddenAssets] = useState(false)
-  const toggleShowHiddenAssets = () => setShowHiddenAssets((prev) => !prev)
   const fiatTotal = usePositionsFiatTotal()
 
   return (
@@ -23,11 +17,7 @@ const Positions: NextPage = () => {
         <title>{`${BRAND_NAME} – Assets`}</title>
       </Head>
 
-      <AssetsHeader>
-        <HiddenTokenButton showHiddenAssets={showHiddenAssets} toggleShowHiddenAssets={toggleShowHiddenAssets} />
-        <TokenListSelect />
-        <CurrencySelect />
-      </AssetsHeader>
+      <AssetsHeader />
 
       <main>
         <Box mb={2}>
