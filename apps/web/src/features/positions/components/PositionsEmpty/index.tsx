6319c85647831313fa7e83935fa4795aac50ec51
valleXYZ <valentin.seehausen@gmail.com>
2025-08-29 16:40:34 +0200

    feat(positions): hide earn button when earn is deactivated (#6275)
    * feat(positions): hide earn button when earn is deactivated
    
    * feat(positions): add test case

--- apps/web/src/features/positions/components/PositionsEmpty/index.tsx
+++ apps/web/src/features/positions/components/PositionsEmpty/index.tsx
@@ -7,6 +7,7 @@ import { AppRoutes } from '@/config/routes'
 import Track from '@/components/common/Track'
 import { POSITIONS_EVENTS } from '@/services/analytics/events/positions'
 import { MixPanelEventParams } from '@/services/analytics/mixpanel-events'
+import useIsEarnFeatureEnabled from '@/features/earn/hooks/useIsEarnFeatureEnabled'
 
 type PositionsEmptyProps = {
   entryPoint?: string
@@ -14,6 +15,7 @@ type PositionsEmptyProps = {
 
 const PositionsEmpty = ({ entryPoint = 'Dashboard' }: PositionsEmptyProps) => {
   const router = useRouter()
+  const isEarnFeatureEnabled = useIsEarnFeatureEnabled()
 
   return (
     <Paper elevation={0} sx={{ p: 3, textAlign: 'center' }}>
@@ -23,18 +25,20 @@ const PositionsEmpty = ({ entryPoint = 'Dashboard' }: PositionsEmptyProps) => {
         You have no active DeFi positions yet
       </Typography>
 
-      <Track
-        {...POSITIONS_EVENTS.EMPTY_POSITIONS_EXPLORE_CLICKED}
-        mixpanelParams={{
-          [MixPanelEventParams.ENTRY_POINT]: entryPoint,
-        }}
-      >
-        <Link href={AppRoutes.earn && { pathname: AppRoutes.earn, query: { safe: router.query.safe } }} passHref>
-          <Button size="small" sx={{ mt: 1 }}>
-            Explore Earn
-          </Button>
-        </Link>
-      </Track>
+      {isEarnFeatureEnabled && (
+        <Track
+          {...POSITIONS_EVENTS.EMPTY_POSITIONS_EXPLORE_CLICKED}
+          mixpanelParams={{
+            [MixPanelEventParams.ENTRY_POINT]: entryPoint,
+          }}
+        >
+          <Link href={{ pathname: AppRoutes.earn, query: { safe: router.query.safe } }} passHref>
+            <Button size="small" sx={{ mt: 1 }}>
+              Explore Earn
+            </Button>
+          </Link>
+        </Track>
+      )}
     </Paper>
   )
 }
