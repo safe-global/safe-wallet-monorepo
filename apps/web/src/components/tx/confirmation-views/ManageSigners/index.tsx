bb5579ebeb646f23f3c7b70689d5ca3f4347c7c9
dsh <11198975+LucieFaire@users.noreply.github.com>
2025-09-01 10:46:43 +0200

    fix(web): show signer name on confirmation step
    Signed-off-by: dsh <11198975+LucieFaire@users.noreply.github.com>
    
    fix(web): Display signer name on confirmation step
    
    Signed-off-by: dsh <11198975+LucieFaire@users.noreply.github.com>

--- apps/web/src/components/tx/confirmation-views/ManageSigners/index.tsx
+++ apps/web/src/components/tx/confirmation-views/ManageSigners/index.tsx
@@ -1,4 +1,4 @@
-import { useMemo } from 'react'
+import { useMemo, useContext } from 'react'
 import type { ReactElement } from 'react'
 
 import MinusIcon from '@/public/images/common/minus.svg'
@@ -11,6 +11,10 @@ import { getNewSafeSetup } from './get-new-safe-setup'
 import type { TransactionInfo, TransactionDetails } from '@safe-global/safe-gateway-typescript-sdk'
 import { ChangeSignerSetupWarning } from '@/features/multichain/components/SignerSetupWarning/ChangeSignerSetupWarning'
 import { OwnerList } from '@/components/tx-flow/common/OwnerList'
+import { TxFlowContext } from '@/components/tx-flow/TxFlowProvider'
+import type { ManageSignersForm } from '@/components/tx-flow/flows/ManagerSigners'
+import type { TxFlowContextType } from '@/components/tx-flow/TxFlowProvider'
+import type { AddressInfo } from '@safe-global/store/gateway/AUTO_GENERATED/safes'
 
 export function ManageSigners({
   txInfo,
@@ -20,13 +24,23 @@ export function ManageSigners({
   txData: TransactionDetails['txData']
 }): ReactElement {
   const { safe } = useSafeInfo()
+  // Extract signer names from context
+  const { data } = useContext<TxFlowContextType<ManageSignersForm>>(TxFlowContext)
+
+  const signerNames = useMemo(() => {
+    return data
+      ? Object.fromEntries(data.owners.filter((owner) => owner.name).map((owner) => [owner.address, owner.name]))
+      : {}
+  }, [data])
+
   const { newOwners, newThreshold } = useMemo(() => {
     return getNewSafeSetup({
       txInfo,
       txData,
       safe,
+      signerNames,
     })
-  }, [txInfo, txData, safe])
+  }, [txInfo, txData, safe, signerNames])
 
   return (
     <Stack display="flex" flexDirection="column" gap={3} sx={{ '& .MuiGrid-container': { alignItems: 'flex-start' } }}>
@@ -41,14 +55,17 @@ export function ManageSigners({
   )
 }
 
-function Actions({ newOwners }: { newOwners: Array<string> }): ReactElement | null {
+function Actions({ newOwners }: { newOwners: Array<AddressInfo> }): ReactElement | null {
   const { safe } = useSafeInfo()
 
   const addedOwners = newOwners
-    .filter((owner) => safe.owners.every(({ value }) => value !== owner))
-    .map((addedOwner) => ({ value: addedOwner }))
+    .filter((owner) => safe.owners.every(({ value }) => value !== owner.value))
+    .map((addedOwner) => ({
+      value: addedOwner.value,
+      name: addedOwner.name ?? undefined,
+    }))
   const removedOwners = safe.owners
-    .filter((owner) => !newOwners.includes(owner.value))
+    .filter((owner) => !newOwners.some((newOwner) => newOwner.value === owner.value))
     .map((removedOwner) => ({
       value: removedOwner.value,
       name: removedOwner.name ?? undefined,
@@ -74,19 +91,20 @@ function Actions({ newOwners }: { newOwners: Array<string> }): ReactElement | nu
   )
 }
 
-function Signers({ owners }: { owners: Array<string> }): ReactElement {
+function Signers({ owners }: { owners: Array<AddressInfo> }): ReactElement {
   return (
     <FieldsGrid title="Signers">
       <Box display="flex" flexDirection="column" gap={2} padding="var(--space-2)" fontSize="14px">
-        {owners.map((owner) => (
+        {owners.map(({ value, name }) => (
           <EthHashInfo
             avatarSize={32}
-            key={owner}
+            key={value}
             showName
-            address={owner}
+            address={value}
             shortAddress={false}
             showCopyButton
             hasExplorer
+            name={name}
           />
         ))}
       </Box>
@@ -94,7 +112,7 @@ function Signers({ owners }: { owners: Array<string> }): ReactElement {
   )
 }
 
-function Threshold({ owners, threshold }: { owners: Array<string>; threshold: number }): ReactElement {
+function Threshold({ owners, threshold }: { owners: Array<AddressInfo>; threshold: number }): ReactElement {
   return (
     <FieldsGrid title="Threshold">
       <Box
