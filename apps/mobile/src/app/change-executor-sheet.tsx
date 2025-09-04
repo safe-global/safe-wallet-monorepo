ac5c3db5f8fc1ba535b3c00941d04567660bf019
Usame Algan <usame@safe.global>
2025-09-04 13:33:43 +0200

    feat: Add select executor

---
+++ apps/mobile/src/app/change-executor-sheet.tsx
@@ -0,0 +1,7 @@
+import { ChangeExecutorSheetContainer } from '../features/ChangeExecutorSheet'
+
+export const ChangeExecutorSheetScreen = () => {
+  return <ChangeExecutorSheetContainer />
+}
+
+export default ChangeExecutorSheetScreen
