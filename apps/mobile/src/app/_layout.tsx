a4d42541ce77a5828f7d4b83f4d4f514b5ce3e8c
Usame Algan <usame@safe.global>
2025-09-04 13:38:02 +0200

    fix: Add change-executor-sheet to layout

--- apps/mobile/src/app/_layout.tsx
+++ apps/mobile/src/app/_layout.tsx
@@ -172,6 +172,14 @@ function RootLayout() {
                               }}
                             />
                             <Stack.Screen
+                              name="change-executor-sheet"
+                              options={{
+                                headerShown: false,
+                                presentation: 'transparentModal',
+                                animation: 'fade',
+                              }}
+                            />
+                            <Stack.Screen
                               name="notifications-opt-in"
                               options={{
                                 headerShown: false,
