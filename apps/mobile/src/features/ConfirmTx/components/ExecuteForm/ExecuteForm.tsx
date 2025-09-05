cc0cc90cc151bbb7b00ab62a1f7ec36d41fba364
Usame Algan <usame@safe.global>
2025-09-05 13:12:08 +0200

    fix: Disable continue button if not next

--- apps/mobile/src/features/ConfirmTx/components/ExecuteForm/ExecuteForm.tsx
+++ apps/mobile/src/features/ConfirmTx/components/ExecuteForm/ExecuteForm.tsx
@@ -1,14 +1,25 @@
 import { SafeButton } from '@/src/components/SafeButton'
 import React from 'react'
-import { View } from 'tamagui'
+import { View, Text, YStack } from 'tamagui'
 import { router } from 'expo-router'
 import { SafeAreaView } from 'react-native'
+import { useTransactionData } from '@/src/features/ConfirmTx/hooks/useTransactionData'
+import { isMultisigDetailedExecutionInfo } from '@/src/utils/transaction-guards'
+import useSafeInfo from '@/src/hooks/useSafeInfo'
 
 interface ExecuteFormProps {
   txId: string
 }
 
 export function ExecuteForm({ txId }: ExecuteFormProps) {
+  const { data: txData } = useTransactionData(txId)
+  const { safe } = useSafeInfo()
+  const txNonce = isMultisigDetailedExecutionInfo(txData?.detailedExecutionInfo)
+    ? txData?.detailedExecutionInfo.nonce
+    : undefined
+
+  const isNext = txNonce !== undefined && txNonce === safe.nonce
+
   const onExecutePress = () => {
     router.push({
       pathname: '/review-and-execute',
@@ -18,10 +29,24 @@ export function ExecuteForm({ txId }: ExecuteFormProps) {
 
   return (
     <SafeAreaView style={{ gap: 24 }}>
-      <View paddingHorizontal={'$3'} height={48} gap="$2" flexDirection="row">
-        <SafeButton flex={1} height="100%" onPress={onExecutePress}>
-          Continue
-        </SafeButton>
+      <View paddingHorizontal={'$3'} gap="$2" flexDirection="row">
+        <YStack justifyContent="center" gap="$2" width="100%">
+          {!isNext && (
+            <Text
+              fontSize="$4"
+              fontWeight={400}
+              width="70%"
+              alignSelf="center"
+              textAlign="center"
+              color="$textSecondaryLight"
+            >
+              You must execute the transaction with the lowest nonce first.
+            </Text>
+          )}
+          <SafeButton onPress={onExecutePress} disabled={!isNext}>
+            Continue
+          </SafeButton>
+        </YStack>
       </View>
     </SafeAreaView>
   )
