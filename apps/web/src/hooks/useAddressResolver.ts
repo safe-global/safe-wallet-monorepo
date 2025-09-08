3cb7261d3ad867c8f2d2f99de46ec45afeef2dc0
katspaugh <katspaugh@gmail.com>
2025-09-08 11:28:27 +0200

    refactor(web): cache reverse ENS lookups

--- apps/web/src/hooks/useAddressResolver.ts
+++ apps/web/src/hooks/useAddressResolver.ts
@@ -1,11 +1,14 @@
 import useAddressBook from '@/hooks/useAddressBook'
 import { useWeb3ReadOnly } from '@/hooks/wallets/web3'
 import { lookupAddress } from '@/services/ens'
-import { useMemo } from 'react'
+import { useEffect, useMemo } from 'react'
 import useAsync from '@safe-global/utils/hooks/useAsync'
 import useDebounce from './useDebounce'
 import { useHasFeature } from './useChains'
 import { FEATURES } from '@safe-global/utils/utils/chains'
+import useChainId from './useChainId'
+
+const cache: Record<string, Record<string, string>> = {}
 
 export const useAddressResolver = (address?: string) => {
   const addressBook = useAddressBook()
@@ -14,13 +17,25 @@ export const useAddressResolver = (address?: string) => {
   const addressBookName = address && addressBook[address]
   const isDomainLookupEnabled = useHasFeature(FEATURES.DOMAIN_LOOKUP)
   const shouldResolve = address && !addressBookName && isDomainLookupEnabled && !!ethersProvider && !!debouncedValue
+  const chainId = useChainId()
 
   const [ens, _, isResolving] = useAsync<string | undefined>(() => {
     if (!shouldResolve) return
+    if (chainId && debouncedValue && cache[chainId][debouncedValue]) {
+      return Promise.resolve(cache[chainId][debouncedValue])
+    }
     return lookupAddress(ethersProvider, debouncedValue)
-  }, [ethersProvider, debouncedValue, shouldResolve])
+  }, [chainId, ethersProvider, debouncedValue, shouldResolve])
 
   const resolving = (shouldResolve && isResolving) || false
+
+  // Cache resolved ENS names per chain
+  useEffect(() => {
+    if (chainId && ens && debouncedValue) {
+      cache[chainId] = cache[chainId] || {}
+      cache[chainId][debouncedValue] = ens
+    }
+  }, [chainId, debouncedValue, ens])
 
   return useMemo(
     () => ({
