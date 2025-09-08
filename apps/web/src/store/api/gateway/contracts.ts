933785509b7d35771e2c936bfd4cf289d7607b6d
katspaugh <381895+katspaugh@users.noreply.github.com>
2025-09-08 14:12:06 +0200

    refactor(web): use new gateway sdk for contract query

--- apps/web/src/store/api/gateway/contracts.ts
+++ apps/web/src/store/api/gateway/contracts.ts
@@ -1,17 +1,15 @@
 import { buildQueryFn } from '@/store/api/gateway'
 import type { EndpointBuilder } from '@reduxjs/toolkit/query/react'
 import type { fakeBaseQuery } from '@reduxjs/toolkit/query/react'
-import {
-  getContract as fetchContract,
-  type getContract as ContractResponse,
-} from '@safe-global/safe-client-gateway-sdk'
+import { getContract as fetchContract } from '@safe-global/safe-gateway-typescript-sdk'
+import type { Contract } from '@safe-global/safe-gateway-typescript-sdk/dist/types/contracts'
 
 export const contractEndpoints = (
   builder: EndpointBuilder<ReturnType<typeof fakeBaseQuery<Error>>, 'OwnedSafes' | 'Submissions', 'gatewayApi'>,
 ) => ({
-  getContract: builder.query<ContractResponse, { chainId: string; contractAddress: string }>({
+  getContract: builder.query<Contract, { chainId: string; contractAddress: string }>({
     queryFn({ chainId, contractAddress }) {
-      return buildQueryFn(() => fetchContract({ params: { path: { chainId, contractAddress } } }))
+      return buildQueryFn(() => fetchContract(chainId, contractAddress))
     },
   }),
 })
