f55f173e7bc715fb9aca5344cc47bf7b14429ff4
valleXYZ <valentin.seehausen@gmail.com>
2025-09-15 11:47:27 +0200

    fix(tests): token e2e tests (#6349)
    * fix: token e2e tests
    
    * fix: remove only

--- apps/web/cypress/e2e/regression/tokens.cy.js
+++ apps/web/cypress/e2e/regression/tokens.cy.js
@@ -5,8 +5,6 @@ import { getSafes, CATEGORIES } from '../../support/safes/safesHandler.js'
 import * as ls from '../../support/localstorage_data.js'
 
 const ASSET_NAME_COLUMN = 0
-const TOKEN_AMOUNT_COLUMN = 1
-const FIAT_AMOUNT_COLUMN = 2
 
 let staticSafes = []
 
@@ -27,59 +25,23 @@ describe('Tokens tests', () => {
   // Added to prod
   it('Verify that non-native tokens are present and have balance', () => {
     assets.selectTokenList(assets.tokenListOptions.allTokens)
-    assets.verifyBalance(assets.currencyDaiCap, TOKEN_AMOUNT_COLUMN, assets.currencyDaiAlttext)
-    assets.verifyTokenBalanceFormat(
-      assets.currencyDaiCap,
-      assets.currencyDaiFormat_2,
-      TOKEN_AMOUNT_COLUMN,
-      FIAT_AMOUNT_COLUMN,
-      value,
-    )
+    assets.verifyBalance(assets.currencyDaiCap, assets.currencyDaiAlttext)
+    assets.verifyTokenBalanceFormat(assets.currencyDaiCap, assets.currencyDaiFormat_2, value)
 
-    assets.verifyBalance(assets.currencyAave, TOKEN_AMOUNT_COLUMN, assets.currencyAaveAlttext)
-    assets.verifyTokenBalanceFormat(
-      assets.currencyAave,
-      assets.currentcyAaveFormat,
-      TOKEN_AMOUNT_COLUMN,
-      FIAT_AMOUNT_COLUMN,
-      value,
-    )
+    assets.verifyBalance(assets.currencyAave, assets.currencyAaveAlttext)
+    assets.verifyTokenBalanceFormat(assets.currencyAave, assets.currentcyAaveFormat, value)
 
-    assets.verifyBalance(assets.currencyLink, TOKEN_AMOUNT_COLUMN, assets.currencyLinkAlttext)
-    assets.verifyTokenBalanceFormat(
-      assets.currencyLink,
-      assets.currentcyLinkFormat,
-      TOKEN_AMOUNT_COLUMN,
-      FIAT_AMOUNT_COLUMN,
-      value,
-    )
+    assets.verifyBalance(assets.currencyLink, assets.currencyLinkAlttext)
+    assets.verifyTokenBalanceFormat(assets.currencyLink, assets.currentcyLinkFormat, value)
 
-    assets.verifyBalance(assets.currencyTestTokenA, TOKEN_AMOUNT_COLUMN, assets.currencyTestTokenAAlttext)
-    assets.verifyTokenBalanceFormat(
-      assets.currencyTestTokenA,
-      assets.currentcyTestTokenAFormat,
-      TOKEN_AMOUNT_COLUMN,
-      FIAT_AMOUNT_COLUMN,
-      value,
-    )
+    assets.verifyBalance(assets.currencyTestTokenA, assets.currencyTestTokenAAlttext)
+    assets.verifyTokenBalanceFormat(assets.currencyTestTokenA, assets.currentcyTestTokenAFormat, value)
 
-    assets.verifyBalance(assets.currencyTestTokenB, TOKEN_AMOUNT_COLUMN, assets.currencyTestTokenBAlttext)
-    assets.verifyTokenBalanceFormat(
-      assets.currencyTestTokenB,
-      assets.currentcyTestTokenBFormat,
-      TOKEN_AMOUNT_COLUMN,
-      FIAT_AMOUNT_COLUMN,
-      value,
-    )
+    assets.verifyBalance(assets.currencyTestTokenB, assets.currencyTestTokenBAlttext)
+    assets.verifyTokenBalanceFormat(assets.currencyTestTokenB, assets.currentcyTestTokenBFormat, value)
 
-    assets.verifyBalance(assets.currencyUSDC, TOKEN_AMOUNT_COLUMN, assets.currencyTestUSDCAlttext)
-    assets.verifyTokenBalanceFormat(
-      assets.currencyUSDC,
-      assets.currentcyTestUSDCFormat,
-      TOKEN_AMOUNT_COLUMN,
-      FIAT_AMOUNT_COLUMN,
-      value,
-    )
+    assets.verifyBalance(assets.currencyUSDC, assets.currencyTestUSDCAlttext)
+    assets.verifyTokenBalanceFormat(assets.currencyUSDC, assets.currentcyTestUSDCFormat, value)
   })
 
   it('Verify that every token except the native token has a "go to blockexplorer link"', () => {
@@ -95,12 +57,12 @@ describe('Tokens tests', () => {
 
   it('Verify the default Fiat currency and the effects after changing it', () => {
     assets.selectTokenList(assets.tokenListOptions.allTokens)
-    assets.verifyFirstRowDoesNotContainCurrency(assets.currencyEUR, FIAT_AMOUNT_COLUMN)
-    assets.verifyFirstRowContainsCurrency(assets.currency$, FIAT_AMOUNT_COLUMN)
+    assets.verifyFirstRowDoesNotContainCurrency(assets.currencyEUR)
+    assets.verifyFirstRowContainsCurrency(assets.currency$)
     assets.clickOnCurrencyDropdown()
     assets.selectCurrency(assets.currencyOptionEUR)
-    assets.verifyFirstRowDoesNotContainCurrency(assets.currency$, FIAT_AMOUNT_COLUMN)
-    assets.verifyFirstRowContainsCurrency(assets.currencyEUR, FIAT_AMOUNT_COLUMN)
+    assets.verifyFirstRowDoesNotContainCurrency(assets.currency$)
+    assets.verifyFirstRowContainsCurrency(assets.currencyEUR)
   })
 
   it('Verify that checking the checkboxes increases the token selected counter', () => {
