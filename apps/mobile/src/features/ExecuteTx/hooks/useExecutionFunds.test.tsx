86df0eb0af95ff2e83f718c408b95eba66d63e0e
Clovis da Silva Neto <clovisdasilvaneto@gmail.com>
2025-10-13 15:25:14 +0200

    refactor(mobile): remove the chain mock declaration

--- apps/mobile/src/features/ExecuteTx/hooks/useExecutionFunds.test.tsx
+++ apps/mobile/src/features/ExecuteTx/hooks/useExecutionFunds.test.tsx
@@ -15,56 +15,9 @@ import { makeStore } from '@/src/store'
 import { useExecutionFunds } from './useExecutionFunds'
 import { ExecutionMethod } from '@/src/features/HowToExecuteSheet/types'
 import type { Chain } from '@safe-global/store/gateway/AUTO_GENERATED/chains'
+import { mockedChains } from '@/src/store/constants'
 
-const mockChain: Chain = {
-  chainId: '1',
-  chainName: 'Ethereum',
-  description: 'Ethereum Mainnet',
-  l2: false,
-  isTestnet: false,
-  zk: false,
-  nativeCurrency: {
-    name: 'Ether',
-    symbol: 'ETH',
-    decimals: 18,
-    logoUri: '',
-  },
-  transactionService: 'https://safe-transaction-mainnet.safe.global',
-  blockExplorerUriTemplate: {
-    address: 'https://etherscan.io/address/{{address}}',
-    txHash: 'https://etherscan.io/tx/{{txHash}}',
-    api: 'https://api.etherscan.io/api?module={{module}}&action={{action}}&address={{address}}&apiKey={{apiKey}}',
-  },
-  beaconChainExplorerUriTemplate: {},
-  disabledWallets: [],
-  ensRegistryAddress: null,
-  balancesProvider: {
-    chainName: 'mainnet',
-    enabled: true,
-  },
-  contractAddresses: {
-    safeSingletonAddress: '0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552',
-    safeProxyFactoryAddress: '0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2',
-    multiSendAddress: '0xA238CBeb142c10Ef7Ad8442C6D1f9E89e07e7761',
-    multiSendCallOnlyAddress: '0x40A2aCCbd92BCA938b02010E17A5b8929b49130D',
-    fallbackHandlerAddress: '0xf48f2B2d2a534e402487b3ee7C18c33Aec0Fe5e4',
-    signMessageLibAddress: '0xA65387F16B013cf2Af4605Ad8aA5ec25a2cbA3a2',
-    createCallAddress: '0x7cbB62EaA69F79e6873cD1ecB2392971036cFAa4',
-    simulateTxAccessorAddress: '0x59AD6735bCd8152B84860Cb256dD9e96b85F69Da',
-    safeWebAuthnSignerFactoryAddress: null,
-  },
-  features: [],
-  gasPrice: [],
-  publicRpcUri: { authentication: 'NO_AUTHENTICATION', value: 'https://ethereum.publicnode.com' },
-  rpcUri: { authentication: 'NO_AUTHENTICATION', value: 'https://ethereum.publicnode.com' },
-  safeAppsRpcUri: { authentication: 'NO_AUTHENTICATION', value: 'https://ethereum.publicnode.com' },
-  shortName: 'eth',
-  theme: {
-    backgroundColor: '#E8E7E6',
-    textColor: '#001428',
-  },
-  recommendedMasterCopyVersion: '1.3.0',
-}
+const mockChain = mockedChains[0] as unknown as Chain
 
 describe('useExecutionFunds', () => {
   const mockSignerAddress = faker.finance.ethereumAddress() as `0x${string}`
@@ -76,7 +29,7 @@ describe('useExecutionFunds', () => {
     store = makeStore()
   })
 
-  const wrapper = ({ children }: { children: React.ReactNode }) => <Provider store={store}>{children}</Provider>
+  const wrapper = ({ children }: { children: React.ReactNode }) => React.createElement(Provider, { store }, children)
 
   describe('WITH_RELAY execution method', () => {
     it('should return hasSufficientFunds as true when using relay', () => {
