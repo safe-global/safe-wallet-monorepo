e49417c02735df67833923f7c1f5f5f9cea09418
Clovis da Silva Neto <clovisdasilvaneto@gmail.com>
2025-10-20 16:39:26 +0200

    fix(web): remove console.log

--- packages/utils/src/features/safe-shield/utils/getOverallStatus.ts
+++ packages/utils/src/features/safe-shield/utils/getOverallStatus.ts
@@ -46,7 +46,6 @@ export const getOverallStatus = (
       for (const addressResults of Object.values(data)) {
         for (const groupResults of Object.values(addressResults)) {
           if (groupResults) {
-            console.log('groupResults', groupResults)
             allResults.push(...groupResults)
           }
         }
