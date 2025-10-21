02d4906936339fcca6c07bf2d6c95357fe842b80
valens-carpentier <valenscarpentier@gmail.com>
2025-10-21 16:23:13 +0200

    feat: integrate No Fee November banner into dashboard

--- apps/web/src/components/dashboard/index.tsx
+++ apps/web/src/components/dashboard/index.tsx
@@ -21,8 +21,13 @@ import useIsStakingBannerVisible from '@/components/dashboard/StakingBanner/useI
 import EarnBanner, { earnBannerID } from '@/components/dashboard/NewsCarousel/banners/EarnBanner'
 import SpacesBanner, { spacesBannerID } from '@/components/dashboard/NewsCarousel/banners/SpacesBanner'
 import StakeBanner, { stakeBannerID } from '@/components/dashboard/NewsCarousel/banners/StakeBanner'
+import NoFeeNovemberBanner, {
+  noFeeNovemberBannerID,
+} from '@/components/dashboard/NewsCarousel/banners/NoFeeNovemberBanner'
 import AddFundsToGetStarted from '@/components/dashboard/AddFundsBanner'
 import useIsPositionsFeatureEnabled from '@/features/positions/hooks/useIsPositionsFeatureEnabled'
+import useIsNoFeeNovemberBannerVisible from '@/hooks/useIsNoFeeNovemberBannerVisible'
+import useNoFeeNovemberEligibility from '@/hooks/useNoFeeNovemberEligibility'
 
 const RecoveryHeader = dynamic(() => import('@/features/recovery/components/RecoveryHeader'))
 const PositionsWidget = dynamic(() => import('@/features/positions/components/PositionsWidget'))
@@ -41,8 +46,15 @@ const Dashboard = (): ReactElement => {
   const isSpacesFeatureEnabled = useHasFeature(FEATURES.SPACES)
   const isStakingBannerVisible = useIsStakingBannerVisible()
   const isPositionsFeatureEnabled = useIsPositionsFeatureEnabled()
+  const isNoFeeNovemberVisible = useIsNoFeeNovemberBannerVisible()
+  const { isEligible } = useNoFeeNovemberEligibility()
 
   const banners = [
+    isNoFeeNovemberVisible && {
+      id: noFeeNovemberBannerID,
+      element: NoFeeNovemberBanner,
+      eligibilityState: isEligible,
+    },
     isEarnPromoEnabled && { id: earnBannerID, element: EarnBanner },
     isSpacesFeatureEnabled && { id: spacesBannerID, element: SpacesBanner },
     isStakingBannerVisible && { id: stakeBannerID, element: StakeBanner },
