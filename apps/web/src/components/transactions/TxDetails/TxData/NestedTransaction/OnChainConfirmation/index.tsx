4e269de759928ad0beda777142aff3170e5883b6
valleXYZ <valentin.seehausen@gmail.com>
2025-10-21 14:57:29 +0200

    feat(Safe Shield): move Tenderly simulation to Safe Shield widget (#6490)
    * feat(Safe Shield): wip move tenderly widget to safe shield side bar
    
    * fix: resolve type checks
    
    * feat(Safe Shield): add TenderlySimulation component to Safe Shield
    
    * fix: address comments
    
    * fix: add nested and normal tx check
    
    * fix: change colors and show tooltip conditional
    
    * fix: prettier format
    
    * move nested tx logic into hook
    
    * fix: resolve type errors
    
    * fix: Reset tx only when it changed
    
    * fix: run prettier
    
    * fix: remove comments
    
    * fix: remove tx check
    
    * fix: right align view button

--- apps/web/src/components/transactions/TxDetails/TxData/NestedTransaction/OnChainConfirmation/index.tsx
+++ apps/web/src/components/transactions/TxDetails/TxData/NestedTransaction/OnChainConfirmation/index.tsx
@@ -1,16 +1,11 @@
 import type { TransactionData } from '@safe-global/store/gateway/AUTO_GENERATED/transactions'
 import useChainId from '@/hooks/useChainId'
-import { Skeleton, Stack } from '@mui/material'
+import { Skeleton } from '@mui/material'
 import ErrorMessage from '@/components/tx/ErrorMessage'
 import { useTransactionsGetTransactionByIdV1Query } from '@safe-global/store/gateway/AUTO_GENERATED/transactions'
-import { useMemo } from 'react'
 import { NestedTransaction } from '../NestedTransaction'
 import TxData from '../..'
-import { TxSimulation, TxSimulationMessage } from '@/components/tx/security/tenderly'
-import type { SafeTransaction } from '@safe-global/types-kit'
-import extractTxInfo from '@/services/tx/extractTxInfo'
 import { useSignedHash } from '../useSignedHash'
-import useSafeAddress from '@/hooks/useSafeAddress'
 
 export const OnChainConfirmation = ({
   data,
@@ -21,52 +16,22 @@ export const OnChainConfirmation = ({
 }) => {
   const chainId = useChainId()
   const signedHash = useSignedHash(data)
-  const safeAddress = useSafeAddress()
 
   const { data: nestedTxDetails, error: txDetailsError } = useTransactionsGetTransactionByIdV1Query(
     { chainId: chainId || '', id: signedHash || '' },
     { skip: !signedHash || !chainId },
   )
 
-  const nestedTx = useMemo<SafeTransaction | undefined>(
-    () =>
-      nestedTxDetails
-        ? {
-            addSignature: () => {},
-            encodedSignatures: () => '',
-            getSignature: () => undefined,
-            data: extractTxInfo(nestedTxDetails).txParams,
-            signatures: new Map(),
-          }
-        : undefined,
-    [nestedTxDetails],
-  )
-
   return (
     <NestedTransaction txData={data} isConfirmationView={isConfirmationView}>
       {nestedTxDetails ? (
-        <>
-          <TxData
-            txData={nestedTxDetails.txData}
-            txInfo={nestedTxDetails.txInfo}
-            txDetails={nestedTxDetails}
-            trusted
-            imitation={false}
-          />
-
-          {isConfirmationView && (
-            <Stack spacing={2}>
-              <TxSimulation
-                disabled={false}
-                transactions={nestedTx}
-                title="Simulate nested transaction"
-                executionOwner={safeAddress}
-                nestedSafe={nestedTxDetails.safeAddress}
-              />
-              <TxSimulationMessage isNested />
-            </Stack>
-          )}
-        </>
+        <TxData
+          txData={nestedTxDetails.txData}
+          txInfo={nestedTxDetails.txInfo}
+          txDetails={nestedTxDetails}
+          trusted
+          imitation={false}
+        />
       ) : txDetailsError ? (
         <ErrorMessage>Could not load details on hash to approve.</ErrorMessage>
       ) : (
