2c62681073ff9e3021c153acce14163af3f74cd0
Daniel Dimitrov <daniel.d@safe.global>
2025-10-24 15:43:14 +0200

    refactor(web): don’t use getSafesByOwner from safe-client-gateway-sdk
    .

--- apps/web/src/store/api/gateway/index.ts
+++ apps/web/src/store/api/gateway/index.ts
@@ -2,7 +2,7 @@ import { createApi, fakeBaseQuery } from '@reduxjs/toolkit/query/react'
 
 import { asError } from '@safe-global/utils/services/exceptions/utils'
 import { safeOverviewEndpoints } from './safeOverviews'
-import { createSubmission, getSafesByOwner, getSubmission } from '@safe-global/safe-client-gateway-sdk'
+import { createSubmission, getSubmission } from '@safe-global/safe-client-gateway-sdk'
 
 export async function buildQueryFn<T>(fn: () => Promise<T>) {
   try {
@@ -19,16 +19,8 @@ export function makeSafeTag(chainId: string, address: string): `${number}:0x${st
 export const gatewayApi = createApi({
   reducerPath: 'gatewayApi',
   baseQuery: fakeBaseQuery<Error>(),
-  tagTypes: ['OwnedSafes', 'Submissions'],
+  tagTypes: ['Submissions'],
   endpoints: (builder) => ({
-    getOwnedSafes: builder.query<getSafesByOwner, { chainId: string; ownerAddress: string }>({
-      queryFn({ chainId, ownerAddress }) {
-        return buildQueryFn(() => getSafesByOwner({ params: { path: { chainId, ownerAddress } } }))
-      },
-      providesTags: (_res, _err, { chainId, ownerAddress }) => {
-        return [{ type: 'OwnedSafes', id: makeSafeTag(chainId, ownerAddress) }]
-      },
-    }),
     getSubmission: builder.query<
       getSubmission,
       { outreachId: number; chainId: string; safeAddress: string; signerAddress: string }
@@ -65,5 +57,4 @@ export const {
   useCreateSubmissionMutation,
   useGetSafeOverviewQuery,
   useGetMultipleSafeOverviewsQuery,
-  useGetOwnedSafesQuery,
 } = gatewayApi
