3f1094245f4d2e2bb45748945afc05add476c591
Daniel Dimitrov <daniel.d@safe.global>
2025-10-24 15:58:01 +0200

    refactor(web): don’t use reateSubmission, getSubmission from @safe-global/safe-client-gateway-sdk

--- apps/web/src/store/api/gateway/index.ts
+++ apps/web/src/store/api/gateway/index.ts
@@ -2,7 +2,6 @@ import { createApi, fakeBaseQuery } from '@reduxjs/toolkit/query/react'
 
 import { asError } from '@safe-global/utils/services/exceptions/utils'
 import { safeOverviewEndpoints } from './safeOverviews'
-import { createSubmission, getSubmission } from '@safe-global/safe-client-gateway-sdk'
 
 export async function buildQueryFn<T>(fn: () => Promise<T>) {
   try {
@@ -21,40 +20,8 @@ export const gatewayApi = createApi({
   baseQuery: fakeBaseQuery<Error>(),
   tagTypes: ['Submissions'],
   endpoints: (builder) => ({
-    getSubmission: builder.query<
-      getSubmission,
-      { outreachId: number; chainId: string; safeAddress: string; signerAddress: string }
-    >({
-      queryFn({ outreachId, chainId, safeAddress, signerAddress }) {
-        return buildQueryFn(() =>
-          getSubmission({ params: { path: { outreachId, chainId, safeAddress, signerAddress } } }),
-        )
-      },
-      providesTags: ['Submissions'],
-    }),
-    createSubmission: builder.mutation<
-      createSubmission,
-      { outreachId: number; chainId: string; safeAddress: string; signerAddress: string }
-    >({
-      queryFn({ outreachId, chainId, safeAddress, signerAddress }) {
-        return buildQueryFn(() =>
-          createSubmission({
-            params: {
-              path: { outreachId, chainId, safeAddress, signerAddress },
-            },
-            body: { completed: true },
-          }),
-        )
-      },
-      invalidatesTags: ['Submissions'],
-    }),
     ...safeOverviewEndpoints(builder),
   }),
 })
 
-export const {
-  useGetSubmissionQuery,
-  useCreateSubmissionMutation,
-  useGetSafeOverviewQuery,
-  useGetMultipleSafeOverviewsQuery,
-} = gatewayApi
+export const { useGetSafeOverviewQuery, useGetMultipleSafeOverviewsQuery } = gatewayApi
