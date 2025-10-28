8d64c73094462791b000dc4a7c1a32f2066ad38f
Daniel Dimitrov <daniel.d@safe.global>
2025-10-28 17:59:49 +0100

    fix(mobile): failing end-to-end tests (#6541)
    tests(mobile): add second safe test
    
    .
    
    .
    
    .

--- apps/mobile/index.js
+++ apps/mobile/index.js
@@ -1,4 +1,19 @@
 // Initialize all background notification handlers FIRST - must be self-contained, no app dependencies
 import '@/src/services/notifications/backgroundHandlers'
 
-import 'expo-router/entry'
+// changed to the below syntax, because on my machine I was failing to build
+// the release version
+// https://github.com/expo/expo/issues/27299#issuecomment-2138722853
+
+import { registerRootComponent } from 'expo'
+
+// import 'expo-router/entry'
+import { ExpoRoot } from 'expo-router'
+
+// Must be exported or Fast Refresh won't update the context
+export function App() {
+  const ctx = require.context('./src/app')
+  return <ExpoRoot context={ctx} />
+}
+
+registerRootComponent(App)
