3e82e63e82f5afed87dca0c5cfede2de02348e4c
Clovis da Silva Neto <clovisdasilvaneto@gmail.com>
2025-10-31 13:21:48 +0100

    fix(web): add tenderly warn in allResults array

--- packages/utils/src/features/safe-shield/utils/getOverallStatus.ts
+++ packages/utils/src/features/safe-shield/utils/getOverallStatus.ts
@@ -38,10 +38,6 @@ export const getOverallStatus = (
     return undefined
   }
 
-  if (hasSimulationError) {
-    return { severity: Severity.WARN, title: SEVERITY_TO_TITLE[Severity.WARN] }
-  }
-
   // Flatten all AnalysisResult objects from contract, recipient, and threat into one array
   const allResults: AnalysisResult[] = []
 
@@ -68,6 +64,15 @@ export const getOverallStatus = (
         }
       }
     }
+  }
+
+  if (hasSimulationError) {
+    allResults.push({
+      severity: Severity.WARN,
+      title: SEVERITY_TO_TITLE[Severity.WARN],
+      type: CommonSharedStatus.FAILED,
+      description: 'Tenderly simulation failed',
+    })
   }
 
   const primaryResult = getPrimaryResult(allResults)
