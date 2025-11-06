39826b317cf51c25d7ae65f19dd28603d3e28029
Clovis da Silva Neto <clovisdasilvaneto@gmail.com>
2025-11-06 11:59:12 +0100

    fix(web): use tenderly status on the useHighlightedSeverity hook

---
+++ apps/web/src/features/safe-shield/hooks/useCheckSimulation.ts
@@ -0,0 +1,18 @@
+import { useCurrentChain } from '@/hooks/useChains'
+import { useContext } from 'react'
+import { TxInfoContext } from '@/components/tx-flow/TxInfoProvider'
+import { useNestedTransaction } from '../components/useNestedTransaction'
+import { isTxSimulationEnabled } from '@safe-global/utils/components/tx/security/tenderly/utils'
+import { isSimulationError } from '@safe-global/utils/components/tx/security/tenderly/utils'
+import { SafeTransaction } from '@safe-global/types-kit'
+
+export const useCheckSimulation = (safeTx?: SafeTransaction) => {
+  const chain = useCurrentChain()
+  const { status: simulationStatus, nestedTx } = useContext(TxInfoContext)
+  const { isNested } = useNestedTransaction(safeTx, chain)
+  const showSimulation = isTxSimulationEnabled(chain) && safeTx
+
+  const hasSimulationError = showSimulation && isSimulationError(simulationStatus, nestedTx, isNested)
+
+  return { hasSimulationError }
+}
