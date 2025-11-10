0e3e48b112677e2f3c990a494c6a9f2758f7ccc4
vmosharova <varya.mosharova@gmail.com>
2025-11-10 10:29:03 +0100

    Chore(Hypernative): formatting

--- apps/web/src/features/hypernative/components/HnSecurityReportBtn/HnSecurityReportBtn.tsx
+++ apps/web/src/features/hypernative/components/HnSecurityReportBtn/HnSecurityReportBtn.tsx
@@ -13,8 +13,11 @@ export const HnSecurityReportBtn = (): ReactElement => {
       <Stack direction="row" alignItems="center" justifyContent="center" spacing={1} className={css.content}>
         <SvgIcon component={HypernativeIcon} inheritViewBox className={css.hypernativeIcon} />
         <Typography className={css.text}>{text}</Typography>
-        <ExternalLink href={typeof href === 'string' ? href : href.href || '#'} className={css.link} noIcon>
-        </ExternalLink>
+        <ExternalLink
+          href={typeof href === 'string' ? href : href.href || '#'}
+          className={css.link}
+          noIcon
+        ></ExternalLink>
       </Stack>
     </Card>
   )
