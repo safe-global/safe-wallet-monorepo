ae66235e796a93893db2d6214ee05568008ec55c
vmosharova <varya.mosharova@gmail.com>
2025-11-10 18:11:38 +0100

    Feat(Hypernative): Request form opens on click (Dashboard banner)

--- apps/web/src/features/hypernative/components/DashboardBanner/DashboardBanner.tsx
+++ apps/web/src/features/hypernative/components/DashboardBanner/DashboardBanner.tsx
@@ -1,42 +1,54 @@
 import { Box, Button, Card, Typography } from '@mui/material'
 import Image from 'next/image'
-import Link from 'next/link'
+import { useState } from 'react'
 import css from './styles.module.css'
 import { dashboardBannerConfig } from './config'
+import { HnSignupFlow } from '../HnSignupFlow'
 
 export const DashboardBanner = () => {
-  const { title, description, ctaLabel, href, badgeSrc, badgeAlt, tagLabel } = dashboardBannerConfig
+  const { title, description, ctaLabel, badgeSrc, badgeAlt, tagLabel } = dashboardBannerConfig
+  const [isSignupFlowOpen, setIsSignupFlowOpen] = useState(false)
+
+  const handleCtaClick = () => {
+    setIsSignupFlowOpen(true)
+  }
+
+  const handleCloseSignupFlow = () => {
+    setIsSignupFlowOpen(false)
+  }
 
   return (
-    <Card className={css.banner}>
-      <Box className={css.tag}>
-        <Typography variant="body2" className={css.tagText}>
-          {tagLabel}
-        </Typography>
-      </Box>
-
-      <Box className={css.content}>
-        <Box className={css.badgeContainer}>
-          <Image src={badgeSrc} alt={badgeAlt} width={54} height={54} className={css.badge} />
+    <>
+      <Card className={css.banner}>
+        <Box className={css.tag}>
+          <Typography variant="body2" className={css.tagText}>
+            {tagLabel}
+          </Typography>
         </Box>
 
-        <Box className={css.textContent}>
-          <Typography variant="h6" className={css.title}>
-            {title}
-          </Typography>
+        <Box className={css.content}>
+          <Box className={css.badgeContainer}>
+            <Image src={badgeSrc} alt={badgeAlt} width={54} height={54} className={css.badge} />
+          </Box>
 
-          <Typography variant="body2" className={css.description}>
-            {description}
-          </Typography>
+          <Box className={css.textContent}>
+            <Typography variant="h6" className={css.title}>
+              {title}
+            </Typography>
 
-          <Link href={href} passHref legacyBehavior>
-            <Button variant="outlined" size="small" className={css.ctaButton}>
+            <Typography variant="body2" className={css.description}>
+              {description}
+            </Typography>
+
+            <Button variant="outlined" size="small" className={css.ctaButton} onClick={handleCtaClick}>
               {ctaLabel}
             </Button>
-          </Link>
+          </Box>
         </Box>
-      </Box>
-    </Card>
+      </Card>
+
+      <HnSignupFlow open={isSignupFlowOpen} onClose={handleCloseSignupFlow} />
+    </>
   )
 }
 
