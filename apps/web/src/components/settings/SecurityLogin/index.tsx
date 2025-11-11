0c2216e5ff393262caea4068bad41977104ac895
vmosharova <varya.mosharova@gmail.com>
2025-11-11 12:12:17 +0100

    Feat(Hypernative): conditional HoC wrapper for the HnBanner in Settings

--- apps/web/src/components/settings/SecurityLogin/index.tsx
+++ apps/web/src/components/settings/SecurityLogin/index.tsx
@@ -3,6 +3,8 @@ import dynamic from 'next/dynamic'
 import { useIsRecoverySupported } from '@/features/recovery/hooks/useIsRecoverySupported'
 import SecuritySettings from '../SecuritySettings'
 import { useRouter } from 'next/router'
+import { useBannerVisibility } from '@/features/hypernative/hooks'
+import { BannerType } from '@/features/hypernative/hooks/useBannerStorage'
 import HnBanner from '@/features/hypernative/components/HnBanner'
 
 const RecoverySettings = dynamic(() => import('@/features/recovery/components/RecoverySettings'))
@@ -10,10 +12,11 @@ const RecoverySettings = dynamic(() => import('@/features/recovery/components/Re
 const SecurityLogin = () => {
   const isRecoverySupported = useIsRecoverySupported()
   const router = useRouter()
+  const { showBanner: showHnBanner, loading: hnLoading } = useBannerVisibility(BannerType.Promo)
 
   return (
     <Box display="flex" flexDirection="column" gap={2}>
-      <HnBanner isDismissable={false} />
+      {(showHnBanner || hnLoading) && <HnBanner />}
 
       {isRecoverySupported && router.query.safe ? <RecoverySettings /> : null}
 
