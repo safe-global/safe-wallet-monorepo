9679e5bafe796e70863fa6541af6e26ce540d382
vmosharova <varya.mosharova@gmail.com>
2025-11-11 18:16:07 +0100

    Chore(Hypernative): formatting

--- apps/web/src/features/hypernative/components/HnBanner/HnBannerWithLocalStorage.tsx
+++ apps/web/src/features/hypernative/components/HnBanner/HnBannerWithLocalStorage.tsx
@@ -5,16 +5,14 @@ import type { WithHnSignupFlowProps } from '../withHnSignupFlow'
 import { HnBanner } from './HnBanner'
 import { HN_BANNER_LS_KEY } from './constants'
 
-export interface HnBannerWithLocalStorageProps extends WithHnSignupFlowProps { }
+export interface HnBannerWithLocalStorageProps extends WithHnSignupFlowProps {}
 
 /**
  * Wrapper component for HnBanner that doesn't require SafeInfo.
  * Uses localStorage for visibility control and dismissal instead of Redux.
  * Only renders if the localStorage value is not false.
  */
-export const HnBannerWithLocalStorage = ({
-  onHnSignupClick,
-}: HnBannerWithLocalStorageProps): ReactElement | null => {
+export const HnBannerWithLocalStorage = ({ onHnSignupClick }: HnBannerWithLocalStorageProps): ReactElement | null => {
   const [value, setBannerVisible] = useLocalStorage<boolean>(HN_BANNER_LS_KEY)
 
   // Only render if value is not false (default behavior: show if undefined or true)
