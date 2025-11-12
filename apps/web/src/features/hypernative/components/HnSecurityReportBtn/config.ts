49d03f51bb941edaf23d78c612a2a88068090484
Varya <72924940+vmosharova@users.noreply.github.com>
2025-11-12 15:12:01 +0100

    Feat(Hypernative): New tx report link (#6639)
    * Feat(Hypernative): New HN link for tx security check
    
    * Feat(Hypernative): New wording for the HN description

--- apps/web/src/features/hypernative/components/HnSecurityReportBtn/config.ts
+++ apps/web/src/features/hypernative/components/HnSecurityReportBtn/config.ts
@@ -3,6 +3,6 @@ import { IS_PRODUCTION } from '@/config/constants'
 export const hnSecurityReportBtnConfig = {
   text: 'Review security report',
   baseUrl: IS_PRODUCTION
-    ? 'https://app.hypernative.xyz/guardian/transaction-risks'
-    : 'https://stage.app.hypernative.xyz/guardian/transaction-risks',
+    ? 'https://app.hypernative.xyz/guardian/alerts'
+    : 'https://stage.app.hypernative.xyz/guardian/alerts',
 } as const
