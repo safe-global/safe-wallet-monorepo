183a5067b424c00dff708351f4a4d55c5f2991ae
valleXYZ <valentin.seehausen@gmail.com>
2025-11-12 16:19:59 +0100

    feat(portfolio): add new portfolio endpoint to usePositions and useLoadBalances

--- packages/store/src/gateway/selectors/portfolioSelectors.ts
+++
@@ -1,6 +0,0 @@
-import { cgwApi } from '../AUTO_GENERATED/portfolios'
-
-export const selectPortfolioPositions = (state: any, address: string) => {
-  const portfolioCache = cgwApi.endpoints.portfolioGetPortfolioV1.select({ address })(state)
-  return portfolioCache?.data?.positionBalances
-}
