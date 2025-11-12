ff1056e8238d623198f7883cd4a4bb73a4d41a9d
vmosharova <varya.mosharova@gmail.com>
2025-11-12 15:19:18 +0100

    Fix(Hypernative): new tx security link + `referrer=safe` query parameter

--- apps/web/src/features/hypernative/components/HnSecurityReportBtn/config.ts
+++ apps/web/src/features/hypernative/components/HnSecurityReportBtn/config.ts
@@ -3,6 +3,6 @@ import { IS_PRODUCTION } from '@/config/constants'
 export const hnSecurityReportBtnConfig = {
   text: 'Review security report',
   baseUrl: IS_PRODUCTION
-    ? 'https://app.hypernative.xyz/guardian/alerts'
-    : 'https://stage.app.hypernative.xyz/guardian/alerts',
+    ? 'https://app.hypernative.xyz/guardian/alert'
+    : 'https://stage.app.hypernative.xyz/guardian/alert',
 } as const
