ae10eacc304af1c24a413159e468b61c088872ca
Daniel Dimitrov <daniel.d@safe.global>
2025-11-28 15:42:03 +0100

    fix(mobile): split components and add tests

--- apps/mobile/src/features/TxHistory/components/TxHistoryList/components/LoadingComponents.tsx
+++ apps/mobile/src/features/TxHistory/components/TxHistoryList/components/LoadingComponents.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
-import { View, Text } from 'tamagui'
+import { View } from 'tamagui'
 import { TransactionSkeleton, TransactionSkeletonItem } from '@/src/components/TransactionSkeleton'
-import { SafeFontIcon } from '@/src/components/SafeFontIcon'
 
 const EmptyComponentBase = () => (
   <View flex={1} alignItems="flex-start" justifyContent="flex-start" paddingTop="$4" testID="tx-history-initial-loader">
@@ -29,25 +28,3 @@ const FooterComponentBase = () => (
 
 export const FooterComponent = React.memo(FooterComponentBase)
 FooterComponent.displayName = 'FooterComponent'
-
-const ErrorComponentBase = () => (
-  <View
-    flex={1}
-    alignItems="center"
-    justifyContent="center"
-    paddingTop="$8"
-    paddingHorizontal="$4"
-    testID="tx-history-error"
-  >
-    <SafeFontIcon name="info" size={48} color="$colorSecondary" />
-    <Text fontSize="$5" fontWeight="600" marginTop="$4" textAlign="center">
-      Error fetching transactions
-    </Text>
-    <Text fontSize="$3" color="$colorSecondary" marginTop="$2" textAlign="center">
-      Swipe down to retry
-    </Text>
-  </View>
-)
-
-export const ErrorComponent = React.memo(ErrorComponentBase)
-ErrorComponent.displayName = 'ErrorComponent'
