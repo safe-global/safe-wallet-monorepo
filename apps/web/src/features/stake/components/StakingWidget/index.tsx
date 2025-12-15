883992071696613861db6b61d1f3ca6a45dd2b4e
valleXYZ <valentin.seehausen@gmail.com>
2025-12-15 11:36:51 +0100

    feature(portfolio): exchange tx icons

--- apps/web/src/features/stake/components/StakingWidget/index.tsx
+++ apps/web/src/features/stake/components/StakingWidget/index.tsx
@@ -3,20 +3,18 @@ import AppFrame from '@/components/safe-apps/AppFrame'
 import { getEmptySafeApp } from '@/components/safe-apps/utils'
 import { useGetStakeWidgetUrl } from '@/features/stake/hooks/useGetStakeWidgetUrl'
 import { widgetAppData } from '@/features/stake/constants'
-import { useDarkMode } from '@/hooks/useDarkMode'
 
 const StakingWidget = ({ asset }: { asset?: string }) => {
   const url = useGetStakeWidgetUrl(asset)
-  const isDarkMode = useDarkMode()
 
   const appData = useMemo(
     () => ({
       ...getEmptySafeApp(),
       ...widgetAppData,
-      iconUrl: isDarkMode ? '/images/common/stake-light.svg' : '/images/common/stake.svg',
+      iconUrl: '/images/common/stake.svg',
       url,
     }),
-    [url, isDarkMode],
+    [url],
   )
 
   return (
