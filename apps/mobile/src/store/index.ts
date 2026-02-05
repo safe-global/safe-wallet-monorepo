3a4a441ecf8e3a0315e0ef0aeae465fda5121704
Clovis da Silva Neto <clovisdasilvaneto@gmail.com>
2026-02-05 15:20:23 +0100

    feat(mobile): introduce a new slice for transaction simulation management, including state handling for simulation status, error messages, and call traces.

--- apps/mobile/src/store/index.ts
+++ apps/mobile/src/store/index.ts
@@ -17,6 +17,7 @@ import {
   REGISTER,
 } from 'redux-persist'
 import { reduxStorage } from './storage'
+import txSimulation from './txSimulationSlice'
 import txHistory from './txHistorySlice'
 import activeSafe from './activeSafeSlice'
 import activeSigner from './activeSignerSlice'
@@ -96,6 +97,7 @@ export const persistBlacklist = [
   'signingState',
   'signerImportFlow',
   'executingState',
+  'txSimulation',
 ]
 
 export const persistTransforms = [cgwClientFilter, sanitizePendingQueriesTransform]
@@ -109,6 +111,7 @@ const persistConfig = {
 }
 
 export const rootReducer = combineReducers({
+  txSimulation,
   txHistory,
   safes,
   activeSigner,
---
+++ apps/mobile/src/store/txSimulationSlice.ts
@@ -0,0 +1,39 @@
+import { createSelector, createSlice, PayloadAction, Slice } from '@reduxjs/toolkit'
+import { RootState } from '.'
+import { FETCH_STATUS, TenderlySimulation } from '@safe-global/utils/components/tx/security/tenderly/types'
+
+type simulationState = {
+  status: FETCH_STATUS
+  link?: string
+  error?: string
+  dataStatus: true
+  callTrace: TenderlySimulation['transaction']['call_trace']
+}
+
+export type txSimulationState = Record<string, simulationState>;
+
+const settingsSlice: Slice<txSimulationState> = createSlice({
+  name: 'txSimulation',
+  initialState: {},
+  reducers: {
+    setTxSimulation: (
+      state,
+      action: PayloadAction<
+        {
+          txId: string
+        } & simulationState
+      >,
+    ) => {
+      const { txId, status, link, error, dataStatus, callTrace } = action.payload
+      state[txId] = { status, link, error, dataStatus, callTrace }
+    },
+  },
+})
+
+const selectCachedSimulationState = (state: RootState) => state.txSimulation
+
+export const selectCachedSimulation = (txId?: string) =>
+  createSelector(selectCachedSimulationState, (txSimulation) => (txId ? txSimulation[txId] : undefined))
+
+export const { setTxSimulation } = settingsSlice.actions
+export default settingsSlice.reducer
