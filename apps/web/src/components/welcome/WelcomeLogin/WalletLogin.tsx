2308ce647e435184628e94540de442a22a363088
Clovis da Silva Neto <clovisdasilvaneto@gmail.com>
2026-02-12 15:36:53 +0100

    feat(spaces): setup SIWE on the home page

--- apps/web/src/components/welcome/WelcomeLogin/WalletLogin.tsx
+++ apps/web/src/components/welcome/WelcomeLogin/WalletLogin.tsx
@@ -1,26 +1,34 @@
 import useConnectWallet from '@/components/common/ConnectWallet/useConnectWallet'
 import useWallet from '@/hooks/wallets/useWallet'
-import { Box, Button, Typography } from '@mui/material'
+import { Box, Button, CircularProgress, Typography } from '@mui/material'
 import EthHashInfo from '@/components/common/EthHashInfo'
 import WalletIcon from '@/components/common/WalletIcon'
+import { useEffect, useState } from 'react'
 
-const WalletLogin = ({
-  onLogin,
-  onContinue,
-  buttonText,
-  fullWidth,
-}: {
+interface WalletLoginProps {
   onLogin: () => void
   onContinue: () => void
   buttonText?: string
   fullWidth?: boolean
-}) => {
+  isLoading?: boolean
+}
+
+const WalletLogin = ({ onLogin, onContinue, buttonText, fullWidth, isLoading }: WalletLoginProps) => {
   const wallet = useWallet()
   const connectWallet = useConnectWallet()
+  const [hasConnectedWallet, setHasConnectedWallet] = useState(false)
 
-  const onConnectWallet = () => {
-    connectWallet()
-    onLogin()
+  useEffect(() => {
+    if (hasConnectedWallet) {
+      onLogin()
+      setHasConnectedWallet(false)
+    }
+  }, [hasConnectedWallet])
+
+  const onConnectWallet = async () => {
+    const wallets = await connectWallet()
+
+    setHasConnectedWallet(!!wallets?.length)
   }
 
   if (wallet !== null) {
@@ -32,18 +40,29 @@ const WalletLogin = ({
         fullWidth={fullWidth}
         style={{ color: '#fff', background: '#121312' }}
         data-testid="continue-with-wallet-btn"
+        disabled={isLoading}
       >
-        <Box justifyContent="space-between" display="flex" flexDirection="row" alignItems="center" gap={1}>
-          <Box display="flex" flexDirection="column" alignItems="flex-start">
-            <Typography variant="subtitle2" fontWeight={700}>
-              {buttonText || 'Continue with'} {wallet.label}
-            </Typography>
-            {wallet.address && (
-              <EthHashInfo address={wallet.address} shortAddress avatarSize={16} showName={false} copyAddress={false} />
-            )}
+        {isLoading ? (
+          <CircularProgress size={20} />
+        ) : (
+          <Box justifyContent="space-between" display="flex" flexDirection="row" alignItems="center" gap={1}>
+            <Box display="flex" flexDirection="column" alignItems="flex-start">
+              <Typography variant="subtitle2" fontWeight={700}>
+                {buttonText || 'Continue with'} {wallet.label}
+              </Typography>
+              {wallet.address && (
+                <EthHashInfo
+                  address={wallet.address}
+                  shortAddress
+                  avatarSize={16}
+                  showName={false}
+                  copyAddress={false}
+                />
+              )}
+            </Box>
+            {wallet.icon && <WalletIcon icon={wallet.icon} provider={wallet.label} width={24} height={24} />}
           </Box>
-          {wallet.icon && <WalletIcon icon={wallet.icon} provider={wallet.label} width={24} height={24} />}
-        </Box>
+        )}
       </Button>
     )
   }
