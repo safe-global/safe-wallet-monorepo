ab9250b7f91b6da493e75a1d822872612a5e94c8
Clovis da Silva Neto <clovisdasilvaneto@gmail.com>
2026-02-12 17:43:35 +0100

    feat(web): create onboarding flow

---
+++ apps/web/src/hooks/useRouterGuard/index.ts
@@ -0,0 +1,38 @@
+import { AppRoutes } from '@/config/routes'
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
+
+export type ActivationGuard = () => Promise<{ success: boolean; redirectTo?: string }>
+export type UseGuard = () => {
+  activationGuard: ActivationGuard
+}
+
+interface useRouterGuardProps {
+  useGuard: UseGuard
+}
+
+export const useRouterGuard = ({ useGuard }: useRouterGuardProps) => {
+  const router = useRouter()
+  const [isCheckingAccess, setIsCheckingAccess] = useState(true)
+  const { activationGuard } = useGuard()
+
+  useEffect(() => {
+    const checkAccess = async () => {
+      setIsCheckingAccess(true)
+
+      const { success, redirectTo } = await activationGuard()
+
+      if (success) {
+        setIsCheckingAccess(false)
+      } else {
+        // we do not want to set isCheckingAccess to false here because we want
+        // the checking access to be reseted only after the redirect is done
+        router.replace(redirectTo ?? AppRoutes.welcome.index)
+      }
+    }
+
+    checkAccess()
+  }, [activationGuard, router])
+
+  return { isCheckingAccess }
+}
