ae519f6beb0f377f70961d965c81defa568cb35a
vmosharova <varya.mosharova@gmail.com>
2026-02-19 17:51:52 +0100

    fix: UI fixes for minimized sidebar and the Help section

--- apps/web/src/features/spaces/components/Sidebar/SidebarCommonFooter.tsx
+++ apps/web/src/features/spaces/components/Sidebar/SidebarCommonFooter.tsx
@@ -10,16 +10,22 @@ import { cn } from '@/utils/cn'
 import { icons } from './config'
 import css from './styles.module.css'
 
+const HELP_URL = 'https://help.safe.global/en/'
+
 export const SidebarCommonFooter = (): ReactElement => {
   return (
     <SidebarFooter>
       <SidebarMenu>
         <SidebarMenuItem>
-          <SidebarMenuButton size="lg" tooltip="Help" className={cn(css.sidebarInteractive, css.footerHelp)}>
+          <SidebarMenuButton
+            size="lg"
+            className={cn(css.sidebarInteractive, css.footerHelp)}
+            render={<a href={HELP_URL} target="_blank" rel="noopener noreferrer" />}
+          >
             <icons.CircleHelp />
             <span>Help</span>
           </SidebarMenuButton>
-          <SidebarMenuAction className={css.sidebarInteractive}>
+          <SidebarMenuAction className={cn(css.sidebarInteractive, css.footerMenuAction)}>
             <icons.EllipsisVertical />
             <span className="sr-only">More options</span>
           </SidebarMenuAction>
