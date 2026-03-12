16348c776cd96cac500336d125d38d8f02f04ff8
vmosharova <varya.mosharova@gmail.com>
2026-03-12 21:36:43 +0100

    fix: layout shift fixes which were introduced after design system upgrade

--- apps/web/src/features/spaces/components/SafeAccounts/EmptySafeAccounts.tsx
+++ apps/web/src/features/spaces/components/SafeAccounts/EmptySafeAccounts.tsx
@@ -1,11 +1,13 @@
-import { Card, Typography } from '@mui/material'
+import { Box, Card, Typography } from '@mui/material'
 import SafeAccountsIcon from '@/public/images/spaces/safe-accounts.svg'
 
 const EmptySafeAccounts = () => {
   return (
     <>
       <Card sx={{ p: 5, textAlign: 'center' }}>
-        <SafeAccountsIcon />
+        <Box display="flex" justifyContent="center">
+          <SafeAccountsIcon />
+        </Box>
 
         <Typography color="text.secondary" mb={2}>
           Add existing Safe Accounts in your space to see them here.
