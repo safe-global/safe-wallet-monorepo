92cd5753ad01f3fd16917ca98b34c78ccea0bc4d
vmosharova <varya.mosharova@gmail.com>
2026-03-16 16:23:38 +0100

    fix: revert back the address fix to avoid conflicts

--- apps/web/src/features/myAccounts/hooks/useSpaceAccountsData.ts
+++ apps/web/src/features/myAccounts/hooks/useSpaceAccountsData.ts
@@ -53,7 +53,7 @@ const formatMultichainAccount = (
   return {
     id: safe.address,
     name,
-    address: safe.address,
+    address: shortenAddress(safe.address),
     href: getSafeHref(firstChain, safe.address),
     safes: safe.safes,
     fiatTotal: safeOverviews.length > 0 ? totalFiat.toString() : undefined,
