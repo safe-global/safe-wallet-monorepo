6767fa59282246d3a7ab73159dad6fc53d7857ee
Zalens <168520191+Zalens@users.noreply.github.com>
2026-03-26 10:52:02 +0100

    Update apps/web/src/features/gtf/hooks/useIsGtfSlotVisible.ts
    Co-authored-by: Daniel <25051234+dasanra@users.noreply.github.com>

--- apps/web/src/features/gtf/hooks/useIsGtfSlotVisible.ts
+++ apps/web/src/features/gtf/hooks/useIsGtfSlotVisible.ts
@@ -1,9 +1,10 @@
 import { useContext } from 'react'
 import { TxFlowContext } from '@/components/tx-flow/TxFlowProvider'
-import { GTFFeature } from '@/features/gtf'
+import { useHasFeature } from '@/hooks/useChains'
+import { FEATURES } from '@safe-global/utils/utils/chains'
 
 export const useIsGtfSlotVisible = (): boolean => {
   const { isRejection } = useContext(TxFlowContext)
-  const isGtfEnabled = GTFFeature.useIsEnabled()
+  const isGtfEnabled = useHasFeature(FEATURES.GTF)
   return !isRejection && !!isGtfEnabled
 }
