2aa68ef6025d50028549c75344c47b585a3ae96d
Tim Schwarz <4171783+tmjssz@users.noreply.github.com>
2026-03-27 12:01:03 +0100

    fix:  revert removal of `@react-native-async-storage/async-storage` dependency
    It's needed by appkit

--- apps/mobile/.storybook/index.ts
+++ apps/mobile/.storybook/index.ts
@@ -1,22 +1,10 @@
-import { createMMKV } from 'react-native-mmkv'
-import { safeJsonParse, safeJsonStringify } from '@walletconnect/safe-json';
+import AsyncStorage from '@react-native-async-storage/async-storage'
 import { view } from './storybook.requires'
-
-const mmkv = createMMKV({ id: 'storybook' })
 
 const StorybookUIRoot = view.getStorybookUI({
   storage: {
-    getItem: async <T = any>(key: string): Promise<T | undefined> => {
-      const item = mmkv.getString(key);
-      if (typeof item === 'undefined' || item === null) {
-        return undefined;
-      }
-
-      return safeJsonParse(item) as T;
-    },
-    setItem: async <T = any>(key: string, value: T) => {
-      return mmkv.set(key, safeJsonStringify(value));
-    },
+    getItem: AsyncStorage.getItem,
+    setItem: AsyncStorage.setItem,
   },
 })
 
