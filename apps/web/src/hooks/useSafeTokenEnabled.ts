b17c58f2250b673e3b5a6e6530d50ba8ec08f982
Greg Jeanmart <greg@jeanmart.me>
2026-04-02 09:26:13 +0200

    fix: put feature flag back

--- apps/web/src/hooks/useSafeTokenEnabled.ts
+++ apps/web/src/hooks/useSafeTokenEnabled.ts
@@ -1,13 +1,13 @@
 import { useContext } from 'react'
 import { GeoblockingContext } from '@/components/common/GeoblockingProvider'
 import useSafeInfo from './useSafeInfo'
-// import { useHasFeature } from './useChains'
-// import { FEATURES } from '@safe-global/utils/utils/chains'
+import { useHasFeature } from './useChains'
+import { FEATURES } from '@safe-global/utils/utils/chains'
 import { getSafeTokenAddress } from '@/components/common/SafenetStakingWidget'
 
 export function useSafeTokenEnabled(): boolean {
   const isBlockedCountry = useContext(GeoblockingContext)
   const { safe, safeLoaded } = useSafeInfo()
-  const hasSafeTokenFeature = true //useHasFeature(FEATURES.SAFE_STAKING)
+  const hasSafeTokenFeature = useHasFeature(FEATURES.SAFE_STAKING)
   return !isBlockedCountry && safeLoaded && !!hasSafeTokenFeature && !!getSafeTokenAddress(safe.chainId)
 }
