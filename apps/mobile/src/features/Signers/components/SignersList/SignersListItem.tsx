48aa6727a5cb6ad02f20836d5728fe890f2efe85
Tim Schwarz <4171783+tmjssz@users.noreply.github.com>
2026-04-02 11:34:47 +0200

    fix(mobile): revert to absolute positioning of menu button in SignerListItem

--- apps/mobile/src/features/Signers/components/SignersList/SignersListItem.tsx
+++ apps/mobile/src/features/Signers/components/SignersList/SignersListItem.tsx
@@ -7,7 +7,7 @@ import { WalletConnectBadge } from '@/src/features/WalletConnect/components/Wall
 import { AddressInfo } from '@safe-global/store/gateway/AUTO_GENERATED/transactions'
 import { SignerSection } from './SignersList'
 import { View } from 'tamagui'
-import { TouchableOpacity, View as RNView } from 'react-native'
+import { TouchableOpacity } from 'react-native'
 import { useTheme } from '@/src/theme/hooks/useTheme'
 import { useAppSelector } from '@/src/store/hooks'
 import { selectContactByAddress } from '@/src/store/addressBookSlice'
@@ -77,7 +77,7 @@ function SignersListItem({ item, signersGroup }: SignersListItemProps) {
   }
 
   return (
-    <View marginBottom="$2">
+    <View position="relative" marginBottom="$2">
       <TouchableOpacity onPress={handleItemPress} testID={`signer-${item.value}`}>
         <View backgroundColor={isDark ? '$backgroundPaper' : '$background'} borderRadius="$2" collapsable={false}>
           <SignersCard
@@ -91,21 +91,39 @@ function SignersListItem({ item, signersGroup }: SignersListItemProps) {
                   <WalletConnectBadge address={item.value} testID={`wc-badge-${item.value}`} skipStatus />
                 )}
 
-                <RNView onStartShouldSetResponder={() => true}>
-                  <MenuView
-                    onPressAction={onPressMenuAction}
-                    actions={actions}
-                    style={{ padding: 4 }}
-                    testID="signer-menu"
-                  >
-                    <SafeFontIcon name="options-horizontal" />
-                  </MenuView>
-                </RNView>
+                {/* Invisible spacer matching menu width so card content doesn't overlap */}
+                <View width={32} />
               </View>
             }
           />
         </View>
       </TouchableOpacity>
+
+      <View
+        position="absolute"
+        right={0}
+        top={0}
+        height={'100%'}
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        flexDirection="row"
+      >
+        <MenuView
+          onPressAction={onPressMenuAction}
+          actions={actions}
+          style={{
+            height: '100%',
+            justifyContent: 'center',
+            alignItems: 'center',
+            paddingRight: 16,
+            paddingLeft: 16,
+          }}
+          testID="signer-menu"
+        >
+          <SafeFontIcon name="options-horizontal" />
+        </MenuView>
+      </View>
     </View>
   )
 }
