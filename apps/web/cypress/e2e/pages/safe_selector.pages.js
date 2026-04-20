0dbc9c4c9d7d178ab4c585aff4f033c2b69dd4ed
vmosharova <varya.mosharova@gmail.com>
2026-04-20 17:25:04 +0200

    fix: add smoke tests for the Safe Selector instead of deprecated Sidebar Accounts

--- apps/web/cypress/e2e/pages/safe_selector.pages.js
+++ apps/web/cypress/e2e/pages/safe_selector.pages.js
@@ -1,6 +1,8 @@
 const safeSelectorBlock = '[data-testid="space-safes-navigation-block"]'
 const openSafesIcon = '[data-testid="open-safes-icon"]'
 const safeSelectorTriggerName = '[data-testid="safe-selector-trigger-name"]'
+const dropdownContent = '[data-slot="select-content"]'
+const chainLogo = '[data-testid="chain-logo"]'
 
 export function openSelector() {
   cy.get(safeSelectorBlock).should('be.visible')
@@ -13,4 +15,16 @@ export function verifyItemExistsInSelector(name) {
 
 export function clickOnSafe(name) {
   cy.contains(name).click()
+}
+
+export function verifyDropdownContainsSafe(address) {
+  cy.get(dropdownContent).should('be.visible').and('contain', address)
+}
+
+export function verifyMultichainSafeChainLogos(address, expectedCount) {
+  cy.get(dropdownContent)
+    .contains(address)
+    .parents('[role="option"]')
+    .find(chainLogo)
+    .should('have.length', expectedCount)
 }
--- apps/web/cypress/e2e/smoke/safe_selector.cy.js
+++ apps/web/cypress/e2e/smoke/safe_selector.cy.js
@@ -1,19 +1,21 @@
 import * as constants from '../../support/constants.js'
 import * as main from '../pages/main.page.js'
-import * as sideBar from '../pages/sidebar.pages.js'
+import * as safeSelector from '../pages/safe_selector.pages.js'
 import * as ls from '../../support/localstorage_data.js'
 import { getSafes, CATEGORIES } from '../../support/safes/safesHandler.js'
 
 let staticSafes = []
 
-describe('[SMOKE] Sidebar tests', { defaultCommandTimeout: 60000, ...constants.VISUAL_VIEWPORT }, () => {
+const multichainSafeShortAddress = '0xC96e'
+
+describe('[SMOKE] Safe selector tests', { defaultCommandTimeout: 60000, ...constants.VISUAL_VIEWPORT }, () => {
   before(async () => {
     staticSafes = await getSafes(CATEGORIES.static)
   })
 
-  it.skip('[SMOKE] Verify the sidebar with multichain safes is displayed', () => {
+  it('[SMOKE] Verify the safe selector dropdown displays multichain safes', () => {
     cy.visit(constants.BALANCE_URL + staticSafes.SEP_STATIC_SAFE_9)
-    // Add multichain safe data (safe3 on Sepolia + Ethereum) and undeployed safe for the group
+    // Add multichain safe data (safe3 on Sepolia + Ethereum)
     main.addToAppLocalStorage(
       constants.localStorageKeys.SAFE_v2__addedSafes,
       ls.addedSafes.sidebarTrustedSafe3TwoChains,
@@ -21,10 +23,9 @@ describe('[SMOKE] Sidebar tests', { defaultCommandTimeout: 60000, ...constants.V
     main.addToAppLocalStorage(constants.localStorageKeys.SAFE_v2__undeployedSafes, ls.undeployedSafe.safes2)
     cy.reload()
 
-    sideBar.openSidebar()
-    sideBar.searchSafe(sideBar.sideBarSafes.multichain_short_)
-    sideBar.expandGroupSafes(0)
-    sideBar.checkMultichainSubSafeExists([constants.networks.ethereum, constants.networks.sepolia])
+    safeSelector.openSelector()
+    safeSelector.verifyDropdownContainsSafe(multichainSafeShortAddress)
+    safeSelector.verifyMultichainSafeChainLogos(multichainSafeShortAddress, 2)
 
     // Wait for main content to fully load before the snapshot is captured
     cy.contains('Sepolia Ether', { timeout: 30000 }).should('be.visible')
