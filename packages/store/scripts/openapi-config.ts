975f05eb59e7c5d3ee705adf6eff0ec2dfebbeff
Clovis da Silva Neto <clovisdasilvaneto@gmail.com>
2026-05-19 16:14:48 +0200

    fix(web): regenerate CGW types

--- packages/store/scripts/openapi-config.ts
+++ packages/store/scripts/openapi-config.ts
@@ -71,7 +71,7 @@ const config: ConfigFile = {
       filterEndpoints: [/^users/],
     },
     '../src/gateway/AUTO_GENERATED/spaces.ts': {
-      filterEndpoints: [/^(spaces|members|spaceSafes|addressBook|userAddressBook)/],
+      filterEndpoints: [/^(spaces|members|spaceSafes|spaceTransactions|addressBook|userAddressBook)/],
     },
     '../src/gateway/AUTO_GENERATED/positions.ts': {
       filterEndpoints: [/^positions/],
