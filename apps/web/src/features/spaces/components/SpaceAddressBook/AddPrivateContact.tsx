79c793b2091969b58955175aa8f386e457358841
vmosharova <varya.mosharova@gmail.com>
2026-05-27 15:23:19 +0200

    feat: new UI for Add Private Contact btn

--- apps/web/src/features/spaces/components/SpaceAddressBook/AddPrivateContact.tsx
+++ apps/web/src/features/spaces/components/SpaceAddressBook/AddPrivateContact.tsx
@@ -1,7 +1,7 @@
 import { Alert, DialogActions, Button, DialogContent } from '@mui/material'
 import { Button as ShadcnButton } from '@/components/ui/button'
 import { Spinner } from '@/components/ui/spinner'
-import PlusIcon from '@/public/images/common/plus.svg'
+import { Plus } from 'lucide-react'
 import { Controller, FormProvider, useForm } from 'react-hook-form'
 import ModalDialog from '@/components/common/ModalDialog'
 import { useState } from 'react'
@@ -96,8 +96,8 @@ const AddPrivateContact = () => {
 
   return (
     <>
-      <ShadcnButton size="sm" variant="outline" onClick={handleOpen}>
-        <PlusIcon />
+      <ShadcnButton size="lg" className="font-bold px-4 py-0" onClick={handleOpen}>
+        <Plus className="size-4 mr-1 text-green-500" />
         Add private contact
       </ShadcnButton>
       <ModalDialog open={open} onClose={handleClose} dialogTitle="Add private contact" hideChainIndicator>
