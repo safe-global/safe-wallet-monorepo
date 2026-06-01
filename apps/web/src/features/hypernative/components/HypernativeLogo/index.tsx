cdb8657266aa18b3200d79d89879dc1873ad0632
Tim Schwarz <4171783+tmjssz@users.noreply.github.com>
2026-06-01 15:25:34 +0200

    fix(hypernative): logo SVG

--- apps/web/src/features/hypernative/components/HypernativeLogo/index.tsx
+++ apps/web/src/features/hypernative/components/HypernativeLogo/index.tsx
@@ -1,4 +1,3 @@
-import { useId, useRef, useEffect } from 'react'
 import { SvgIcon, type SvgIconProps } from '@mui/material'
 import HypernativeLogoSvg from '@/public/images/hypernative/hypernative-logo.svg'
 
@@ -11,48 +10,12 @@ interface HypernativeLogoProps extends Omit<SvgIconProps, 'component'> {
  * when rendered multiple times. Uses React's useId hook to generate unique IDs.
  */
 const HypernativeLogo = (props: HypernativeLogoProps) => {
-  const uniqueId = useId()
-  const filterId = `invert-${uniqueId}`
-  const maskId = `logoMask-${uniqueId}`
-  const containerRef = useRef<HTMLDivElement>(null)
+  const sx = {
+    ...props.sx,
+    ...(props.fill ? { '.hypernative-logo-fill': { fill: props.fill } } : {}),
+  }
 
-  useEffect(() => {
-    if (!containerRef.current) return
-
-    // Find the SVG element inside the SvgIcon wrapper
-    const svg = containerRef.current.querySelector('svg')
-    if (!svg) return
-
-    // Find and update filter ID
-    const filter = svg.querySelector('#invert')
-    if (filter) {
-      filter.setAttribute('id', filterId)
-    }
-
-    // Find and update mask ID
-    const mask = svg.querySelector('#logoMask')
-    if (mask) {
-      mask.setAttribute('id', maskId)
-    }
-
-    // Update image filter reference
-    const image = svg.querySelector('mask image')
-    if (image) {
-      image.setAttribute('filter', `url(#${filterId})`)
-    }
-
-    // Update rect mask reference
-    const rect = svg.querySelector('rect')
-    if (rect) {
-      rect.setAttribute('mask', `url(#${maskId})`)
-    }
-  }, [filterId, maskId])
-
-  return (
-    <div ref={containerRef} style={{ display: 'inline-flex' }}>
-      <SvgIcon {...props} component={HypernativeLogoSvg} inheritViewBox />
-    </div>
-  )
+  return <SvgIcon {...props} sx={sx} component={HypernativeLogoSvg} inheritViewBox />
 }
 
 export default HypernativeLogo
