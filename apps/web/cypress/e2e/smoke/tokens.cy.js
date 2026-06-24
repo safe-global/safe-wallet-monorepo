8f9cb26af31e604e688ed879066d80fde53a6a78
vmosharova <varya.mosharova@gmail.com>
2026-06-24 15:01:20 +0200

    fix: smoke tests for tokens

--- apps/web/cypress/e2e/smoke/tokens.cy.js
+++ apps/web/cypress/e2e/smoke/tokens.cy.js
@@ -3,8 +3,11 @@ import * as main from '../pages/main.page'
 import * as assets from '../pages/assets.pages'
 import { getSafes, CATEGORIES } from '../../support/safes/safesHandler.js'
 import * as ls from '../../support/localstorage_data.js'
+import * as space from '../pages/spaces.page.js'
 
 let staticSafes = []
+const walletCredentials = JSON.parse(Cypress.env('CYPRESS_WALLET_CREDENTIALS'))
+const signer = walletCredentials.OWNER_4_PRIVATE_KEY
 
 describe('[SMOKE] Tokens tests', () => {
   before(async () => {
@@ -15,11 +18,12 @@ describe('[SMOKE] Tokens tests', () => {
       constants.localStorageKeys.SAFE_v2__tokenlist_onboarding,
       ls.cookies.acceptedTokenListOnboarding,
     )
-    cy.visit(constants.BALANCE_URL + staticSafes.SEP_STATIC_SAFE_2)
+    space.signInAndVisit(constants.BALANCE_URL + staticSafes.SEP_STATIC_SAFE_2, signer)
   })
 
   // Added to prod
   it('Verify that when owner is disconnected, Send button is disabled', () => {
+    space.disconnectFromSpaceLevel()
     assets.toggleShowAllTokens(true)
     assets.toggleHideDust(false)
     assets.showSendBtn(0)
