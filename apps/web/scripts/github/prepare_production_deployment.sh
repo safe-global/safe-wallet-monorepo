94c9c166049f6789916c705ed9260f37d2aeb6bc
Luqman Bello <luqman@safe.global>
2026-07-10 12:25:21 +0200

    chore: replace GitLab deploy hook with GitHub dispatch

--- apps/web/scripts/github/prepare_production_deployment.sh
+++ apps/web/scripts/github/prepare_production_deployment.sh
@@ -4,14 +4,12 @@ set -ev
 
 # Only:
 # - Tagged commits
-# - Security env variables are available.
-if [ -n "$VERSION_TAG" ] && [ -n "$PROD_DEPLOYMENT_HOOK_TOKEN" ] && [ -n "$PROD_DEPLOYMENT_HOOK_URL" ]
+# - GH_TOKEN is available.
+if [ -n "$VERSION_TAG" ] && [ -n "$GH_TOKEN" ]
 then
-  curl --silent --output /dev/null --write-out "%{http_code}" -X POST \
-     -F token="$PROD_DEPLOYMENT_HOOK_TOKEN" \
-     -F ref=master \
-     -F "variables[TRIGGER_RELEASE_COMMIT_TAG]=$VERSION_TAG" \
-      $PROD_DEPLOYMENT_HOOK_URL
+  gh workflow run web-core-production.yml \
+    --repo safe-global/safe-production-promotions \
+    -f "tag=$VERSION_TAG"
 else
   echo "⚠︎ Production deployment could not be prepared"
 fi
