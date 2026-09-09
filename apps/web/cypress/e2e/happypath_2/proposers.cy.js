497e6802c318bdc48031612d5b903df1ca5dc9ee
dsh <11198975+LucieFaire@users.noreply.github.com>
2026-09-09 14:40:21 +0200

    fix(e2e): proposer tests (#8681)

--- apps/web/cypress/e2e/happypath_2/proposers.cy.js
+++ apps/web/cypress/e2e/happypath_2/proposers.cy.js
@@ -21,20 +21,20 @@ describe('Happy path Proposers tests', { defaultCommandTimeout: 30000 }, () => {
     staticSafes = await getSafes(CATEGORIES.static)
   })
 
-  it('Verify that editing a proposer is only possible for the proposer created by the creator', () => {
+  it('Verify a proposer can be renamed in the local address book by any signer', () => {
     wallet.connectSignerViaStorage(signer3, constants.setupUrl + staticSafes.SEP_STATIC_SAFE_31)
     cy.contains(owner.safeAccountNonceStr, { timeout: 10000 })
-    proposer.verifyEditProposerBtnDisabled(proposerAddress)
+    proposer.verifyEditProposerBtnEnabled(proposerAddress)
 
     proposer.clickOnEditProposerBtn(proposerAddress2)
     proposer.enterProposerName(changedProposerName)
-    proposer.clickOnSubmitProposerBtn()
+    proposer.saveProposerName()
     cy.reload()
     proposer.checkProposerData([changedProposerName])
 
     proposer.clickOnEditProposerBtn(proposerAddress2)
     proposer.enterProposerName(proposerName2)
-    proposer.clickOnSubmitProposerBtn()
+    proposer.saveProposerName()
     cy.reload()
     proposer.checkProposerData([proposerName2])
   })
