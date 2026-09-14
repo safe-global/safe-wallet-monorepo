02423599352a04a3ed2f9a6773972b410fc333ef
vmosharova <varya.mosharova@gmail.com>
2026-09-14 13:38:00 +0200

    fix: failing smoke assertions for dashboard

--- apps/web/cypress/e2e/pages/dashboard.pages.js
+++ apps/web/cypress/e2e/pages/dashboard.pages.js
@@ -159,10 +159,7 @@ export function verifyTxQueueWidget() {
 
     cy.contains(`a[href^="/transactions/tx?id=multisig_0x"]`, '1/1').should('exist')
 
-    cy.contains(
-      `a[data-testid="view-all-link"][href="${constants.transactionQueueUrl}${encodeURIComponent(staticSafes.SEP_STATIC_SAFE_2)}"]`,
-      viewAllStr,
-    )
+    cy.get(viewAllLink).should('exist').and('contain.text', viewAllStr)
   })
 }
 
